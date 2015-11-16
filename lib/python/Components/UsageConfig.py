9c39a7938204fb43e81de790e7c6bdb0df2350a0
littlesat <littlesat99@yahoo.com>
2015-11-16 07:40:25 +0100

    Only show timer conflict ignore warning screen once
    It will not re-occur when you restart enigma2

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -196,8 +196,9 @@ def InitUsageConfig():
 		("5", "DVB-T/-S/-C"),
 		("127", _("No priority")) ])
 
-	config.usage.remote_fallback_enabled = ConfigYesNo(default = False);
-	config.usage.remote_fallback = ConfigText(default = "", fixed_size = False);
+	config.usage.remote_fallback_enabled = ConfigYesNo(default = False)
+	config.usage.remote_fallback = ConfigText(default = "", fixed_size = False)
+	config.usage.show_timer_conflict_warning = ConfigYesNo(default = True)
 
 	dvbs_nims = [("-2", _("Disabled"))]
 	dvbt_nims = [("-2", _("Disabled"))]
