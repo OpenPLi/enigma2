88f4fbba7aaac5f5be07a2b265755c41698f6417
Athanasios Oikonomou <athoik@gmail.com>
2016-01-04 18:51:32 +0200

    FBC: Use CFile to setProcFBCID
    (cherry picked from commit 39be60e4e4aa703980aeb19aafa116516f013c35)
    Signed-off-by: Erik Slagter <erik@openpli.org>

--- lib/dvb/fbc.cpp
+++ lib/dvb/fbc.cpp
@@ -1,6 +1,7 @@
 #include <lib/dvb/fbc.h>
 #include <lib/dvb/dvb.h>
 #include <lib/dvb/sec.h>
+#include <lib/base/cfile.h>
 #include <lib/base/object.h>
 
 #include <unistd.h>
@@ -107,22 +108,13 @@ int eFBCTunerManager::getFBCTunerNum()
 
 int eFBCTunerManager::setProcFBCID(int fe_id, int fbc_id)
 {
-	char tmp[128];
-	int fd;
-
+	char tmp[64];
 	snprintf(tmp, sizeof(tmp), "/proc/stb/frontend/%d/fbc_id", fe_id);
-
-	if((fd = open(tmp, O_RDWR)) < 0)
-		return(-1);
 
 	if(isLinkedByIndex(fe_id))
 		fbc_id += 0x10; // 0x10 mask: linked, 0x0f (?) mask: fbc_id // FIXME: shouldn't this be |=?
 
-	snprintf(tmp, sizeof(tmp), "%x", fbc_id);
-	write(fd, tmp, strlen(tmp));
-	close(fd);
-
-	return(0);
+	return CFile::writeIntHex(tmp, fbc_id);
 }
 
 bool eFBCTunerManager::isRootFeSlot(int fe_slot_id)
