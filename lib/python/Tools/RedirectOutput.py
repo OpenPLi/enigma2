465b91e05de3f72d9407412ad04318d4035201c8
Mirakels <mirakels@openpli.org>
2016-01-16 21:16:10 +0100

    debugging: timestamp python output

--- lib/python/Tools/RedirectOutput.py
+++ lib/python/Tools/RedirectOutput.py
@@ -2,10 +2,16 @@ import sys
 from enigma import ePythonOutput
 
 class EnigmaOutput:
+	def __init__(self):
+		self.line = ''
+
 	def write(self, data):
 		if isinstance(data, unicode):
 			data = data.encode("UTF-8")
-		ePythonOutput(data)
+		self.line += data
+		if '\n' in data:
+			ePythonOutput(self.line)
+			self.line = ''
 
 	def flush(self):
 		pass
