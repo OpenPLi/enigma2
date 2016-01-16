621baa377ba761e8049aa80a888c9ebe0e1c69c1
Erik Slagter <erik@openpli.org>
2016-01-16 16:49:50 +0100

    Revert "Remove referral to FBC tuner manager that's not in this tree."
    This reverts commit 530bef6e8829ebfebe94f12798f7acd6f4550abf.
    
    It IS in this tree...

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -97,6 +97,7 @@ eDVBResourceManager::eDVBResourceManager()
 		adapter->scanDevices();
 	}
 
+	m_fbcmng = new eFBCTunerManager(instance);
 	m_sec = new eDVBSatelliteEquipmentControl(m_frontend, m_simulate_frontend);
 
 	if (adapter)
