65250a287a6aa860fcc47266fef5283ee93eaaa3
littlesat <littlesat99@yahoo.com>
2016-02-15 19:26:19 +0100

    Enabled/disabled fallback tuner for recordings seperately
    As discussed here: http://forums.openpli.org/topic/40376-fallbacktuner-and-timerconflict-dectection/page-5

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -197,6 +197,7 @@ def InitUsageConfig():
 		("127", _("No priority")) ])
 
 	config.usage.remote_fallback_enabled = ConfigYesNo(default = False)
+	config.usage.remote_fallback_recording_enabled = ConfigYesNo(default = False)
 	config.usage.remote_fallback = ConfigText(default = "", fixed_size = False)
 	config.usage.show_timer_conflict_warning = ConfigYesNo(default = True)
 
--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -2291,7 +2291,7 @@ ePyObject eDVBServicePlay::getRassInteractiveMask()
 	Py_RETURN_NONE;
 }
 
-bool eDVBServiceBase::tryFallbackTuner(eServiceReferenceDVB &service, bool &is_stream, bool is_pvr, bool simulate)
+bool eDVBServiceBase::tryFallbackTuner(eServiceReferenceDVB &service, bool &is_stream, bool is_pvr, bool simulate, bool is_recording)
 {
 	ePtr<eDVBResourceManager> res_mgr;
 	std::ostringstream remote_service_ref;
@@ -2299,7 +2299,8 @@ bool eDVBServiceBase::tryFallbackTuner(eServiceReferenceDVB &service, bool &is_s
 	int system;
 	size_t index;
 
-	bool remote_fallback_enabled = eConfigManager::getConfigBoolValue("config.usage.remote_fallback_enabled", false);
+	bool remote_fallback_enabled = is_recording ? eConfigManager::getConfigBoolValue("config.usage.remote_fallback_recording_enabled", false) :
+		eConfigManager::getConfigBoolValue("config.usage.remote_fallback_enabled", false);
 	std::string remote_fallback_url = eConfigManager::getConfigValue("config.usage.remote_fallback");
 
 	if(is_stream || is_pvr || simulate ||
