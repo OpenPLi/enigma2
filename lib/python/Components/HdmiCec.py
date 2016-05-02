db5bf0f963dcfd2e61c51c4bc1c2d7402f9eb107
littlesat <littlesat99@yahoo.com>
2016-05-02 19:41:45 +0200

    Add repeat wakeup messages for HDMI CEC
    In some cases when e.g. a surround set is connected via HDMI between the
    receiver and the TV it could be the TV is in cascade turned on via the
    surround send, then it might happen the TV is missing some HDMI-CEC
    commands. While repeating the wakeup comments when the TV is turned on we
    ensure the TV will receive these commands

--- lib/python/Components/HdmiCec.py
+++ lib/python/Components/HdmiCec.py
@@ -32,6 +32,10 @@ choicelist = []
 for i in (10, 50, 100, 150, 250):
 	choicelist.append(("%d" % i, "%d ms" % i))
 config.hdmicec.minimum_send_interval = ConfigSelection(default = "0", choices = [("0", _("Disabled"))] + choicelist)
+choicelist = []
+for i in range(5, 65, 5):
+	choicelist.append(("%d" % i, "%d sec" % i))
+config.hdmicec.repeat_wakeup_timer = ConfigSelection(default = "0", choices = [("0", _("Disabled"))] + choicelist)
 
 class HdmiCec:
 
@@ -41,6 +45,8 @@ class HdmiCec:
 
 		self.wait = eTimer()
 		self.wait.timeout.get().append(self.sendCmd)
+		self.repeat = eTimer()
+		self.repeat.timeout.get().append(self.wakeupMessages)
 		self.queue = []
 
 		eHdmiCEC.getInstance().messageReceived.get().append(self.messageReceived)
@@ -55,7 +61,7 @@ class HdmiCec:
 		config.hdmicec.enabled.addNotifier(self.configVolumeForwarding)
 		if config.hdmicec.handle_deepstandby_events.value:
 			if not getFPWasTimerWakeup():
-				self.wakeupMessages()
+				self.onLeaveStandby()
 
 	def getPhysicalAddress(self):
 		physicaladdress = eHdmiCEC.getInstance().getPhysicalAddress()
@@ -177,10 +183,13 @@ class HdmiCec:
 
 	def onLeaveStandby(self):
 		self.wakeupMessages()
+		if config.hdmicec.repeat_wakeup_timer.value:
+			self.repeat.startLongTimer(int(config.hdmicec.repeat_wakeup_timer.value))
 
 	def onEnterStandby(self, configElement):
 		from Screens.Standby import inStandby
 		inStandby.onClose.append(self.onLeaveStandby)
+		self.repeat.stop()
 		self.standbyMessages()
 
 	def onEnterDeepStandby(self, configElement):
