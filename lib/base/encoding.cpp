5762cf8972c4c6d81c2222fef67daf44c65eefff
nx111 <gd.zhangdz@gmail.com>
2016-06-21 21:25:33 +0800

    simplified code in eDVBTextEncodingHandler, revert change in configure.ac

--- lib/base/encoding.cpp
+++ lib/base/encoding.cpp
@@ -43,59 +43,48 @@ eDVBTextEncodingHandler::eDVBTextEncodingHandler()
 					line[i]=0;
 					break;
 				}
-				if(line[start]>0 && line[start] <= ' ' ){
+				if(line[start] > 0 && line[start] <= ' ' ){
 					start++;
-					continue;
+					continue;	//skip blank char, 'start' to find the offset of the first printable char
 				}
 				
 				if(line[i] >= 'A' && line[i] <= 'Z')
-					line[i]=line[i]+0x20;
+					line[i]=line[i] + 0x20;		//lower case the char
 			}
 			if( i == start )
-				continue;
+				continue;	//no printable char ,skip this line
 
 			int tsid, onid, encoding;
 
-			if ( (sscanf( line, "0x%x 0x%x iso8859-%d", &tsid, &onid, &encoding ) == 3 )
-				||(sscanf( line, "%d %d iso8859-%d", &tsid, &onid, &encoding ) == 3 ) )
-				m_TransponderDefaultMapping[(tsid<<16)|onid]=encoding;
-			else if ( (sscanf( line, "0x%x 0x%x iso%d", &tsid, &onid, &encoding ) == 3 && encoding == 6937 )
-					||(sscanf( line, "%d %d iso%d", &tsid, &onid, &encoding ) == 3 && encoding == 6937 ) )
+			if ( sscanf( line, "0x%x 0x%x iso8859-%d", &tsid, &onid, &encoding ) == 3
+				  || sscanf( line, "%d %d iso8859-%d", &tsid, &onid, &encoding ) == 3 )
+				m_TransponderDefaultMapping[(tsid<<16)|onid] = encoding;
+			else if ((sscanf( line, "0x%x 0x%x iso%d", &tsid, &onid, &encoding ) == 3 && encoding == 6937 )
+				  || (sscanf( line, "%d %d iso%d", &tsid, &onid, &encoding ) == 3 && encoding == 6937 ))
 				m_TransponderDefaultMapping[(tsid<<16)|onid]=0;
-			else if ( (sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3 
-					&& ( strcmp(s_table,"gb2312") == 0 || strcmp(s_table,"gbk") == 0
-					   	|| strcmp(s_table,"gb18030") == 0 || strcmp(s_table,"cp936") == 0  ) )
-			        ||(sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 
-					&& ( strcmp(s_table,"gb2312") == 0 || strcmp(s_table,"gbk") == 0
-					   	|| strcmp(s_table,"gb18030") == 0 || strcmp(s_table,"cp936") == 0  ) )
-				 )
-				m_TransponderDefaultMapping[(tsid<<16)|onid]=GB18030_ENCODING;
-			else if ( (sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3 
-					&& ( strcmp(s_table,"big5") == 0 || strcmp(s_table,"cp950") == 0 ) )
-			        ||(sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 
-					&& ( strcmp(s_table,"big5") == 0 || strcmp(s_table,"cp950") == 0 ) )
-				 )
-				m_TransponderDefaultMapping[(tsid<<16)|onid]=BIG5_ENCODING;
-			else if ( ((sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3 )
-						&& (strcmp(s_table, "utf8")==0 || strcmp(s_table, "utf-8")==0 ) )
-					||((sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 )
-						&&  (strcmp(s_table, "utf8")==0 || strcmp(s_table, "utf-8")==0 ) )
-				 )
-				m_TransponderDefaultMapping[(tsid<<16)|onid]=UTF8_ENCODING;
-			else if ( ((sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3 )
-						&& ( strcmp(s_table, "unicode")==0 || strcmp(s_table, "utf16be")==0 ))
-					||((sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 )
-						&& ( strcmp(s_table, "unicode")==0 || strcmp(s_table, "utf16be")==0 ))
-				 )
-				m_TransponderDefaultMapping[(tsid<<16)|onid]=UTF16BE_ENCODING;
-			else if ( ((sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3 )
-						&& strcmp(s_table, "utf16le")==0)
-					||((sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 )
-						&& strcmp(s_table, "utf16le")==0)
-				 )
-				m_TransponderDefaultMapping[(tsid<<16)|onid]=UTF16LE_ENCODING;
-			else if ( (sscanf( line, "0x%x 0x%x", &tsid, &onid ) == 2 )
-					||(sscanf( line, "%d %d", &tsid, &onid ) == 2 ) )
+			else if (( sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3
+				  || sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 )
+				&& ( strcmp(s_table,"gb2312") == 0 || strcmp(s_table,"gbk") == 0
+				  || strcmp(s_table,"gb18030") == 0 || strcmp(s_table,"cp936") == 0  ))
+				m_TransponderDefaultMapping[(tsid<<16)|onid] = GB18030_ENCODING;
+			else if (( sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3
+			          || sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 )
+				&& ( strcmp(s_table,"big5") == 0 || strcmp(s_table,"cp950") == 0 ))
+				m_TransponderDefaultMapping[(tsid<<16)|onid] = BIG5_ENCODING;
+			else if ((sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3
+				  || sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 )
+				&& (strcmp(s_table, "utf8") == 0 || strcmp(s_table, "utf-8") == 0 ))
+				m_TransponderDefaultMapping[(tsid<<16)|onid] = UTF8_ENCODING;
+			else if (( sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3
+				  || sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 )
+				&& ( strcmp(s_table, "unicode") == 0 || strcmp(s_table, "utf16be") == 0 ))
+				m_TransponderDefaultMapping[(tsid<<16)|onid] = UTF16BE_ENCODING;
+			else if (( sscanf( line, "0x%x 0x%x %s", &tsid, &onid, s_table ) == 3
+				  || sscanf( line, "%d %d %s", &tsid, &onid, s_table ) == 3 )
+				&& strcmp(s_table, "utf16le") == 0 )
+				m_TransponderDefaultMapping[(tsid<<16)|onid] = UTF16LE_ENCODING;
+			else if ( sscanf( line, "0x%x 0x%x", &tsid, &onid ) == 2
+					|| sscanf( line, "%d %d", &tsid, &onid ) == 2 )
 				m_TransponderUseTwoCharMapping.insert((tsid<<16)|onid);
 			else if ( sscanf( line, "%s iso8859-%d", countrycode, &encoding ) == 2)
 			{
@@ -103,72 +92,68 @@ eDVBTextEncodingHandler::eDVBTextEncodingHandler()
 				countrycode[0]=toupper(countrycode[0]);
 				countrycode[1]=toupper(countrycode[1]);
 				countrycode[2]=toupper(countrycode[2]);
-				m_CountryCodeDefaultMapping[countrycode]=encoding;
+				m_CountryCodeDefaultMapping[countrycode] = encoding;
 			   }
-                           else
+			   else
 				defaultEncodingTable=encoding;
 			}
 			else if ( sscanf( line, "%s %s", countrycode, s_table ) == 2 
-				  &&( strcmp(s_table,"gb2312") == 0 || strcmp(s_table,"gbk") == 0 
-				      || strcmp(s_table,"gb18030") == 0 || strcmp(s_table,"cp936") == 0  
-				    )
-				)
+				&&( strcmp(s_table,"gb2312") == 0 || strcmp(s_table,"gbk") == 0
+				  || strcmp(s_table,"gb18030") == 0 || strcmp(s_table,"cp936") == 0 ))
 			{
 			   if ( countrycode[0] != '*' ){
 				countrycode[0]=toupper(countrycode[0]);
 				countrycode[1]=toupper(countrycode[1]);
 				countrycode[2]=toupper(countrycode[2]);
-				m_CountryCodeDefaultMapping[countrycode]=GB18030_ENCODING;
+				m_CountryCodeDefaultMapping[countrycode] = GB18030_ENCODING;
 			   }
                            else
 				defaultEncodingTable=GB18030_ENCODING;
 			}
-			else if ( sscanf( line, "%s %s", countrycode, s_table ) == 2
-				 && ( strcmp(s_table,"big5") == 0 || strcmp(s_table,"cp950") == 0 )
-				)
+			else if ( sscanf( line, "%s %s", countrycode, s_table ) == 2 &&
+				  ( strcmp(s_table,"big5") == 0 || strcmp(s_table,"cp950") == 0 ))
 			{
 			   if ( countrycode[0] != '*' ){
 				countrycode[0]=toupper(countrycode[0]);
 				countrycode[1]=toupper(countrycode[1]);
 				countrycode[2]=toupper(countrycode[2]);
-				m_CountryCodeDefaultMapping[countrycode]=BIG5_ENCODING;
+				m_CountryCodeDefaultMapping[countrycode] = BIG5_ENCODING;
 			   }
                            else
 				defaultEncodingTable=BIG5_ENCODING;
 			}
 			else if ( sscanf( line, "%s %s", countrycode, s_table ) == 2 &&
-				  (strcmp(s_table, "utf8") == 0 || strcmp(s_table,"utf-8") == 0 )
-				)
+				  (strcmp(s_table, "utf8") == 0 || strcmp(s_table,"utf-8") == 0 ))
 			{
 			   if ( countrycode[0] != '*' ){
 				countrycode[0]=toupper(countrycode[0]);
 				countrycode[1]=toupper(countrycode[1]);
 				countrycode[2]=toupper(countrycode[2]);
-				m_CountryCodeDefaultMapping[countrycode]=UTF8_ENCODING;
+				m_CountryCodeDefaultMapping[countrycode] = UTF8_ENCODING;
 			   }
                            else
 				defaultEncodingTable=UTF8_ENCODING;
 			}
 			else if ( sscanf( line, "%s %s", countrycode, s_table ) == 2 &&
-				  (strcmp(s_table, "unicode")==0 || strcasecmp(s_table, "utf16be")==0) )
+				  (strcmp(s_table, "unicode") == 0 || strcasecmp(s_table, "utf16be") == 0 ))
 			{
 			   if ( countrycode[0] != '*' ){
 				countrycode[0]=toupper(countrycode[0]);
 				countrycode[1]=toupper(countrycode[1]);
 				countrycode[2]=toupper(countrycode[2]);
-				m_CountryCodeDefaultMapping[countrycode]=UTF16BE_ENCODING;
+				m_CountryCodeDefaultMapping[countrycode] = UTF16BE_ENCODING;
 			   }
                            else
 				defaultEncodingTable=UTF16BE_ENCODING;
 			}
 			else if ( sscanf( line, "%s %s", countrycode, s_table ) == 2 &&
-				  strcasecmp(s_table, "utf16le")==0)
+				  strcasecmp(s_table, "utf16le") == 0)
 			{
 			   if ( countrycode[0] != '*' ){
 				countrycode[0]=toupper(countrycode[0]);
 				countrycode[1]=toupper(countrycode[1]);
 				countrycode[2]=toupper(countrycode[2]);
-				m_CountryCodeDefaultMapping[countrycode]=UTF16LE_ENCODING;
+				m_CountryCodeDefaultMapping[countrycode] = UTF16LE_ENCODING;
 			   }
                            else
 				defaultEncodingTable=UTF16LE_ENCODING;
@@ -179,10 +164,10 @@ eDVBTextEncodingHandler::eDVBTextEncodingHandler()
 				countrycode[0]=toupper(countrycode[0]);
 				countrycode[1]=toupper(countrycode[1]);
 				countrycode[2]=toupper(countrycode[2]);
-				m_CountryCodeDefaultMapping[countrycode]=0;
+				m_CountryCodeDefaultMapping[countrycode] = 0;
 			   }
-                           else
-				defaultEncodingTable=0;
+			   else
+				defaultEncodingTable = 0;
 			}
 			else
 				eDebug("[eDVBTextEncodingHandler] encoding.conf: couldn't parse %s", line);
