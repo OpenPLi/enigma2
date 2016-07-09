434c62b1616c6c61325cc023d1e10b77cf03a800
littlesat <littlesat99@yahoo.com>
2016-07-09 22:10:37 +0200

    LanguageSelection: Adapt for menu_path

--- lib/python/Screens/LanguageSelection.py
+++ lib/python/Screens/LanguageSelection.py
@@ -32,6 +32,8 @@ class LanguageSelection(Screen):
 
 		self.updateList()
 		self.onLayoutFinish.append(self.selectActiveLanguage)
+		from Screens.Menu import setmenu_path
+		setmenu_path(self, _("Language selection"))
 
 		self["actions"] = ActionMap(["OkCancelActions"],
 		{
@@ -40,7 +42,6 @@ class LanguageSelection(Screen):
 		}, -1)
 
 	def selectActiveLanguage(self):
-		self.setTitle(_("Language selection"))
 		pos = 0
 		for pos, x in enumerate(self.list):
 			if x[0] == self.oldActiveLanguage:
