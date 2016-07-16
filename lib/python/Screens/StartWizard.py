0b099506e4cd12c0e9684a6b2c8a60c46f72ac78
littlesat <littlesat99@yahoo.com>
2016-07-16 11:39:11 +0200

    Arrange settings restore and auto install via StartWizard
    Now the user can select if he wants to restore the settings and auto install after he reflashed an image. When the user does not respond for 10
    seconds it automatically restore the settings and auto install.

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,4 +1,5 @@
 from Wizard import wizardManager
+from Screens.MessageBox import MessageBox
 from Screens.WizardLanguage import WizardLanguage
 from Screens.Rc import Rc
 from Tools.HardwareInfo import HardwareInfo
@@ -34,6 +35,24 @@ class StartWizard(WizardLanguage, Rc):
 		config.misc.firstrun.save()
 		configfile.save()
 
+def checkForAvailableAutoBackup():
+	import os
+	for dir in [name for name in os.listdir("/media/") if os.path.isdir(os.path.join("/media/", name))]:
+		if os.path.isfile("/%s/backup/PLi-AutoBackup.tar.gz" % dir):
+			return True
+	return False
+
+class AutoRestoreWizard(MessageBox):
+	def __init__(self, session):
+		MessageBox.__init__(self, session, _("Do you want to autorestore settings?"), type=MessageBox.TYPE_YESNO, timeout=10, default=True, simple=True)
+
+	def close(self, value):
+		if value:
+			MessageBox.close(self, 43)
+		else:
+			MessageBox.close(self)
+
+wizardManager.registerWizard(AutoRestoreWizard, config.misc.firstrun.value and checkForAvailableAutoBackup(), priority = -10)
 wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.value, priority = 5)
 if OverscanWizard:
 	wizardManager.registerWizard(OverscanWizard, config.misc.do_overscanwizard.value, priority = 10)
