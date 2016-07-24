c6d93f2f3f3f6da4055f11e448a3b11894a8539d
littlesat <littlesat99@yahoo.com>
2016-07-24 13:11:23 +0200

    Solve commitlog issue for OpenPli5.0
    See
    http://stackoverflow.com/questions/27835619/ssl-certificate-verify-failed-error
    for the full clarification

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -197,7 +197,13 @@ class CommitInfo(Screen):
 			commitlog += 80 * '-' + '\n'
 			commitlog += url.split('/')[-2] + '\n'
 			commitlog += 80 * '-' + '\n'
-			for c in loads(urlopen(url, timeout=5).read()):
+			try:
+				# OpenPli 5.0 uses python 2.7.11 and here we need to bypass the certificate check
+				from ssl import _create_unverified_context
+				log = loads(urlopen(url, timeout=5, context=_create_unverified_context()).read())
+			except:
+				log = loads(urlopen(url, timeout=5).read())
+			for c in log:
 				creator = c['commit']['author']['name']
 				title = c['commit']['message']
 				date = datetime.strptime(c['commit']['committer']['date'], '%Y-%m-%dT%H:%M:%SZ').strftime('%x %X')
