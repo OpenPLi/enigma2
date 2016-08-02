825bc96d603c2c3dc8309b76a5277d7e4d68084e
littlesat <littlesat99@yahoo.com>
2016-08-02 19:19:28 +0200

    Show list of countries in WizardLanguage
    As we have that manu languages with only stepping forward you need manu
    presses to find the correct language. Now we redirect to the 'flags' list
    which makes more sense and is faster

--- lib/python/Screens/WizardLanguage.py
+++ lib/python/Screens/WizardLanguage.py
@@ -1,31 +1,18 @@
 from Wizard import Wizard
 from Components.Label import Label
 from Components.Language import language
+from LanguageSelection import LanguageWizard
 
 class WizardLanguage(Wizard):
 	def __init__(self, session, showSteps = True, showStepSlider = True, showList = True, showConfig = True):
 		Wizard.__init__(self, session, showSteps, showStepSlider, showList, showConfig)
 		self["languagetext"] = Label()
-		self.updateLanguageDescription()
+		self.LanguageWizardCallback()
 
 	def red(self):
-		self.resetCounter()
-		self.languageSelect()
+		self.session.openWithCallback(self.LanguageWizardCallback, LanguageWizard)
 
-	def languageSelect(self):
-		print "languageSelect"
-		newlanguage = language.getActiveLanguageIndex() + 1
-		if newlanguage >= len(language.getLanguageList()):
-			newlanguage = 0
-		language.activateLanguageIndex(newlanguage)
-		self.updateTexts()
-
-	def updateLanguageDescription(self):
-		print language.getLanguageList()[language.getActiveLanguageIndex()]
+	def LanguageWizardCallback(self, key="1234"):
+		print "languageSelect", key
 		self["languagetext"].setText(self.getTranslation(language.getLanguageList()[language.getActiveLanguageIndex()][1][0]))
-
-	def updateTexts(self):
-		print "updateTexts"
-		self.updateText(firstset = True)
-		self.updateValues()
-		self.updateLanguageDescription()
+		self.resetCounter()
