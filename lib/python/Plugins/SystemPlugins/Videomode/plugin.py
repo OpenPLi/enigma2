edd5c0d10688a0697792d9f7785a2e57f81c6f20
littlesat <littlesat99@yahoo.com>
2016-08-02 18:14:30 +0200

    Change HaveColorSpace to HasColorSpace

--- lib/python/Plugins/SystemPlugins/Videomode/plugin.py
+++ lib/python/Plugins/SystemPlugins/Videomode/plugin.py
@@ -117,7 +117,7 @@ class VideoSetup(Screen, ConfigListScreen):
 			self.list.append(getConfigListEntry(_("Scaler sharpness"), config.av.scaler_sharpness, _("Configure the sharpness of the video scaling.")))
 		if SystemInfo["HasBypassEdidChecking"]:
 			self.list.append(getConfigListEntry(_("Bypass HDMI EDID checking"), config.av.bypassEdidChecking, _("Configure if the HDMI EDID checking should be bypassed as this might solve issue with some TVs.")))
-		if SystemInfo["HaveColorspace"]:
+		if SystemInfo["HasColorspace"]:
 			self.list.append(getConfigListEntry(_("HDMI Colorspace"), config.av.hdmicolorspace, _("This option allows you can config the Colorspace from Auto to RGB")))
 
 		self["config"].list = self.list
