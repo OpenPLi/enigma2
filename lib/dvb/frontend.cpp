bb95925a69a2c9e55c9b462d13f67403f2c41c9a
Athanasios Oikonomou <athoik@gmail.com>
2016-09-16 20:43:48 +0300

    Fix GCC6 abs wrap around on unsigned integers (#436)
    * Fix GCC6 abs wrap around on unsigned integers
    
    GCC6 used the behaviour on ::abs as in std::abs.
    
    https://gcc.gnu.org/gcc-6/porting_to.html
    
    Calling abs using GCC6 on unsigned values will produce results like
    abs(1-2) = 4.29497e+09 (UINT_MAX), so we are getting the desired
    wrap around as defined by the standard.
    
    Introduce the absdiff that will return the absolute difference without
    wraping around unsigned integers.
    
    * move absdiff to a separate file instead of defining function twice
    
    * sec: change tuner frequency to unsigned int to prevent warnings
    
    ../../git/lib/dvb/sec.cpp:227:21: warning: comparison between signed and unsigned integer expressions [-Wsign-compare]
          if (tuner_freq < fe_info.frequency_min || tuner_freq > fe_info.frequency_max)
              ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~
    ../../git/lib/dvb/sec.cpp:227:59: warning: comparison between signed and unsigned integer expressions [-Wsign-compare]
          if (tuner_freq < fe_info.frequency_min || tuner_freq > fe_info.frequency_max)
                                                    ~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~
    
    * fb/fblcd: silence sign-compare warnings
    
    ../../git/lib/gdi/fblcd.cpp: In member function 'int eFbLCD::setMode(int, int, int)':
    ../../git/lib/gdi/fblcd.cpp:147:25: warning: comparison between signed and unsigned integer expressions [-Wsign-compare]
      if ((m_screeninfo.xres != nxRes) && (m_screeninfo.yres != nyRes) && (m_screeninfo.bits_per_pixel != nbpp))
           ~~~~~~~~~~~~~~~~~~^~~~~~~~
    
    ../../git/lib/gdi/fb.cpp: In member function 'int fbClass::SetMode(int, int, int)':
    ../../git/lib/gdi/fb.cpp:154:22: warning: comparison between signed and unsigned integer expressions [-Wsign-compare]
      if ((screeninfo.xres!=nxRes) || (screeninfo.yres!=nyRes) || (screeninfo.bits_per_pixel!=nbpp))
           ~~~~~~~~~~~~~~~^~~~~~~

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -12,6 +12,8 @@
 
 #include <sstream>
 
+#include "absdiff.h"
+
 #ifndef I2C_SLAVE_FORCE
 #define I2C_SLAVE_FORCE	0x0706
 #endif
@@ -343,8 +345,8 @@ RESULT eDVBFrontendParameters::calculateDifference(const iDVBFrontendParameters 
 				diff = 1<<27;
 			else
 			{
-				diff = abs(sat.frequency - osat.frequency);
-				diff += abs(sat.symbol_rate - osat.symbol_rate);
+				diff = absdiff(sat.frequency, osat.frequency);
+				diff += absdiff(sat.symbol_rate, osat.symbol_rate);
 			}
 			return 0;
 		}
@@ -362,8 +364,8 @@ RESULT eDVBFrontendParameters::calculateDifference(const iDVBFrontendParameters 
 				diff = 1 << 27;
 			else
 			{
-				diff = abs(cable.frequency - ocable.frequency);
-				diff += abs(cable.symbol_rate - ocable.symbol_rate);
+				diff = absdiff(cable.frequency, ocable.frequency);
+				diff += absdiff(cable.symbol_rate, ocable.symbol_rate);
 			}
 			return 0;
 		}
@@ -405,7 +407,7 @@ RESULT eDVBFrontendParameters::calculateDifference(const iDVBFrontendParameters 
 			else if (oterrestrial.system != terrestrial.system)
 				diff = 1 << 30;
 			else
-				diff = abs(terrestrial.frequency - oterrestrial.frequency) / 1000;
+				diff = absdiff(terrestrial.frequency, oterrestrial.frequency) / 1000;
 			return 0;
 		}
 		case iDVBFrontend::feATSC:
@@ -420,7 +422,7 @@ RESULT eDVBFrontendParameters::calculateDifference(const iDVBFrontendParameters 
 				diff = 1 << 29;
 			else
 			{
-				diff = abs(atsc.frequency - oatsc.frequency);
+				diff = absdiff(atsc.frequency, oatsc.frequency);
 			}
 			return 0;
 		}
--- lib/dvb/sec.cpp
+++ lib/dvb/sec.cpp
@@ -7,6 +7,8 @@
 
 #include <lib/base/eerror.h>
 
+#include "absdiff.h"
+
 #define SEC_DEBUG
 
 #ifdef SEC_DEBUG
@@ -221,7 +223,7 @@ int eDVBSatelliteEquipmentControl::canTune(const eDVBFrontendParametersSatellite
 				{
 					int lof = sat.frequency > lnb_param.m_lof_threshold ?
 						lnb_param.m_lof_hi : lnb_param.m_lof_lo;
-					int tuner_freq = abs(sat.frequency - lof);
+					unsigned int tuner_freq = absdiff(sat.frequency, lof);
 					if (tuner_freq < fe_info.frequency_min || tuner_freq > fe_info.frequency_max)
 						ret = 0;
 				}
@@ -392,8 +394,7 @@ RESULT eDVBSatelliteEquipmentControl::prepare(iDVBFrontend &frontend, const eDVB
 			if(!is_unicable)
 			{
 				// calc Frequency
-				int local= abs(sat.frequency
-					- lof);
+				int local = absdiff(sat.frequency, lof);
 				frequency = ((((local * 2) / 125) + 1) / 2) * 125;
 				frontend.setData(eDVBFrontend::FREQ_OFFSET, sat.frequency - frequency);
 
@@ -417,8 +418,7 @@ RESULT eDVBSatelliteEquipmentControl::prepare(iDVBFrontend &frontend, const eDVB
 			}
 			else
 			{
-				int tmp1 = abs(sat.frequency
-						-lof)
+				int tmp1 = absdiff(sat.frequency, lof)
 						+ lnb_param.SatCRvco
 						- 1400000
 						+ lnb_param.guard_offset;
