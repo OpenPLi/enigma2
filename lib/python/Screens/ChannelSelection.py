7a96f741176970840bd2c0b069344e3bf959b970
Dimitrij <Dima-73@inbox.lv>
2016-09-20 09:41:05 +0300

    [ChannelSelection] title - add "User - bouquets (TV)" to lokalize (#448)
    * [ChannelSelection] title - add "User - bouquets (TV)" to lokalize
    
    * [ChannelSelection]Simplify search string
    
    * Update ChannelSelection.py

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -1398,7 +1398,7 @@ class ChannelSelectionBase(Screen):
 
 	def getServiceName(self, ref):
 		str = self.removeModeStr(ServiceReference(ref).getServiceName())
-		if str in ("Bouquets (Radio)" , "Bouquets (TV)", "User - bouquets"):
+		if 'bouquets' in str.lower():
 			return _("User - bouquets")
 		if not str:
 			pathstr = ref.getPath()
