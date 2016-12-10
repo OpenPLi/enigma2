4ec828411ed6ba9865a280e75627d2a9010e09fc
Littlesat <littlesat99@yahoo.com>
2016-12-10 12:40:11 +0100

    HdmiCec: Change wakeup repeat timer default to 3 seconds
    It seems some TVs need such delay and therefore always enable this timer

--- lib/python/Components/HdmiCec.py
+++ lib/python/Components/HdmiCec.py
@@ -33,9 +33,9 @@ for i in (10, 50, 100, 150, 250):
 	choicelist.append(("%d" % i, "%d ms" % i))
 config.hdmicec.minimum_send_interval = ConfigSelection(default = "0", choices = [("0", _("Disabled"))] + choicelist)
 choicelist = []
-for i in range(5, 65, 5):
+for i in [3] + range(5, 65, 5):
 	choicelist.append(("%d" % i, _("%d sec") % i))
-config.hdmicec.repeat_wakeup_timer = ConfigSelection(default = "0", choices = [("0", _("Disabled"))] + choicelist)
+config.hdmicec.repeat_wakeup_timer = ConfigSelection(default = "3", choices = [("0", _("Disabled"))] + choicelist)
 
 class HdmiCec:
 
