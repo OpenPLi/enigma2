12010998bb38ddcc06f293378342a865cc14f106
Alex Maystrenko <alexeytech@gmail.com>
2016-12-28 01:23:56 +0100

    fix RGB structure size on 64bit

--- lib/gdi/gpixmap.h
+++ lib/gdi/gpixmap.h
@@ -15,14 +15,14 @@ struct gRGB
 	union {
 #if BYTE_ORDER == LITTLE_ENDIAN
 		struct {
-			unsigned char b, g, r, a;
+			uint8_t b, g, r, a;
 		};
 #else
 		struct {
-			unsigned char a, r, g, b;
+			uint8_t a, r, g, b;
 		};
 #endif
-		unsigned int value;
+		uint32_t value;
 	};
 	gRGB(int r, int g, int b, int a=0): b(b), g(g), r(r), a(a)
 	{
