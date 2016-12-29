4a4d8a2a4c8d93e591b2da94540f967d81d74c8f
Mike Looijmans <milo@openpli.org>
2016-12-29 15:20:26 +0100

    Publish Enigma2 stream service using Avahi
    Tell other boxes that we have something to offer (e.g. a fallback tuner).
    This implements the service part, the "browse" code is yet to be done.

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -9,6 +9,7 @@
 #include <lib/actions/action.h>
 #include <lib/driver/rc.h>
 #include <lib/base/ioprio.h>
+#include <lib/base/e2avahi.h>
 #include <lib/base/ebase.h>
 #include <lib/base/eenv.h>
 #include <lib/base/eerror.h>
@@ -108,6 +109,7 @@ class eMain: public eApplication, public Object
 public:
 	eMain()
 	{
+		e2avahi_init(this);
 		init.setRunlevel(eAutoInitNumbers::main);
 		/* TODO: put into init */
 		m_dvbdb = new eDVBDB();
@@ -121,6 +123,7 @@ public:
 	{
 		m_dvbdb->saveServicelist();
 		m_mgr->releaseCachedChannel();
+		e2avahi_close();
 	}
 };
 
