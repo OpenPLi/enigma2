c77265bda3da06352eaa46407e53bf4814227a30
Taapat <taapat@gmail.com>
2017-01-07 00:16:54 +0200

    Converter VfdDisplay: use lower for argumets, thx for proposal littlesat
    This allow use an independent case for arguments.
    
    Set self.num in one string.
    Such long lines work slowly and do not like PEP8, but like littlesat ;)

--- lib/python/Components/Converter/VfdDisplay.py
+++ lib/python/Components/Converter/VfdDisplay.py
@@ -14,13 +14,13 @@ class VfdDisplay(Poll, Converter, object):
 		self.showclock = 0
 		self.delay = 5000
 		self.loop = -1
-		self.type = type.split(';')
-		if 'Number' in self.type and 'Clock' not in self.type:  # Only channel number
+		self.type = type.lower().split(';')
+		if 'number' in self.type and 'clock' not in self.type:  # Only channel number
 			self.delay = 0
 			self.poll_enabled = False
 		else:
 			self.poll_enabled = True
-			if 'Clock' in self.type and 'Number' not in self.type:  # Only clock
+			if 'clock' in self.type and 'number' not in self.type:  # Only clock
 				self.showclock = 1
 				self.delay = -1
 			else:
@@ -28,13 +28,13 @@ class VfdDisplay(Poll, Converter, object):
 					if x.isdigit():
 						self.delay = int(x) * 1000
 						break
-				if 'Loop' in self.type and self.delay:
+				if 'loop' in self.type and self.delay:
 					self.loop = self.delay
-			if 'Nozero' in self.type:
+			if 'nozero' in self.type:
 				self.hour = '%'
 			else:
 				self.hour = '%02'
-			if '12h' in self.type or '12H' in self.type:
+			if '12h' in self.type:
 				self.hour = self.hour + 'I'
 			else:
 				self.hour = self.hour + 'H'
@@ -49,7 +49,7 @@ class VfdDisplay(Poll, Converter, object):
 				return self.num
 		else:
 			if self.showclock == 1:
-				if 'Noblink' in self.type:
+				if 'noblink' in self.type:
 					self.poll_interval = self.delay
 				else:
 					self.poll_interval = 1000
@@ -76,13 +76,7 @@ class VfdDisplay(Poll, Converter, object):
 			if self.loop != -1:
 				self.loop = self.delay
 			service = self.source.serviceref
-			if service:
-				if 'Nozero' in self.type:
-					self.num = '%d' % service.getChannelNum()
-				else:
-					self.num = '%04d' % service.getChannelNum()
-			else:
-				self.num = None
+			self.num = service and ('%d' if 'nozero' in self.type else '%04d') % service.getChannelNum() or None
 			Converter.changed(self, what)
 		elif what[0] is self.CHANGED_POLL:
 			Converter.changed(self, what)
