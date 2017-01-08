2d0291fc3da3b2b51bba77cec88d746b8a6d31a6
littlesat <littlesat99@yahoo.com>
2017-01-08 10:05:58 +0100

    Revert "Support NUL chars in console output"
    This reverts commit 7883901f94102316ec21ca82f7d4c9531ede62f5.
    
    As it has side effects on HDMI-CEC
    
    See: https://forums.openpli.org/topic/47172-clean-flash-et900-gsod/#entry645558

--- lib/python/connections.h
+++ lib/python/connections.h
@@ -19,17 +19,14 @@ public:
 	PyObject *get();
 };
 
-inline PyObject *PyFrom(int v, int s = 0)
+inline PyObject *PyFrom(int v)
 {
 	return PyInt_FromLong(v);
 }
 
-inline PyObject *PyFrom(const char *c, int s = -1)
+inline PyObject *PyFrom(const char *c)
 {
-	if (s == -1)
-		return PyString_FromString(c);
-	else
-		return PyString_FromStringAndSize(c, s);
+	return PyString_FromString(c);
 }
 
 template <class R>
@@ -52,12 +49,12 @@ template <class R, class V0>
 class PSignal1: public PSignal, public Signal1<R,V0>
 {
 public:
-	R operator()(V0 a0, int a1=int(-1))
+	R operator()(V0 a0)
 	{
 		if (m_list)
 		{
 			PyObject *pArgs = PyTuple_New(1);
-			PyTuple_SET_ITEM(pArgs, 0, PyFrom(a0, a1));
+			PyTuple_SET_ITEM(pArgs, 0, PyFrom(a0));
 			callPython(pArgs);
 			Org_Py_DECREF(pArgs);
 		}
