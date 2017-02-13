OpenPLi/enigma2

commit 1adb2eef0c86f7f1dd7bf5d621b77ada77380363

Integrate SoftcamSetup into Enigma2
Move SoftcamSetup from PLi-Plugins into E2 directly.

lib/python/Screens/SoftcamSetup.py
@@ -1,0 +1,171 @@
+from Screens.Screen import Screen
+from Screens.MessageBox import MessageBox
+from Components.FileList import FileEntryComponent, FileList
+from Components.ConfigList import ConfigListScreen
+from Components.ActionMap import ActionMap, NumberActionMap
+from Components.Button import Button
+from Components.Label import Label
+from Components.config import config, ConfigElement, ConfigSubsection, ConfigSelection, ConfigSubList, getConfigListEntry, KEY_LEFT, KEY_RIGHT, KEY_OK
+from Components.ConfigList import ConfigList
+from Components.Pixmap import Pixmap
+from Components.ScrollLabel import ScrollLabel
+from Tools.GetEcmInfo import GetEcmInfo
+
+import os
+from Tools.camcontrol import CamControl
+from enigma import eTimer, eDVBCI_UI, eListboxPythonStringContent, eListboxPythonConfigContent
+
+class ConfigAction(ConfigElement):
+	def __init__(self, action, *args):
+		ConfigElement.__init__(self)
+		self.value = "(OK)"
+		self.action = action
+		self.actionargs = args
+	def handleKey(self, key):
+		if (key == KEY_OK):
+			self.action(*self.actionargs)
+	def getMulti(self, dummy):
+		pass
+
+class SoftcamSetup(Screen, ConfigListScreen):
+	skin = """
+	<screen name="SoftcamSetup" position="center,center" size="560,450" >
+		<widget name="config" position="5,10" size="550,90" />
+		<widget name="info" position="5,100" size="550,300" font="Fixed;18" />
+		<ePixmap name="red" position="0,410" zPosition="1" size="140,40" pixmap="skin_default/buttons/red.png" transparent="1" alphatest="on" />
+		<ePixmap name="green" position="140,410" zPosition="1" size="140,40" pixmap="skin_default/buttons/green.png" transparent="1" alphatest="on" />
+		<ePixmap name="blue" position="420,410" zPosition="1" size="140,40" pixmap="skin_default/buttons/blue.png" transparent="1" alphatest="on" />
+		<widget name="key_red" position="0,410" zPosition="2" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" />
+		<widget name="key_green" position="140,410" zPosition="2" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" />
+		<widget name="key_blue" position="420,410" zPosition="2" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" />
+	</screen>"""
+	def __init__(self, session):
+		Screen.__init__(self, session)
+
+		self.setup_title = _("Softcam setup")
+
+		self["actions"] = ActionMap(["OkCancelActions", "ColorActions", "CiSelectionActions"],
+			{
+				"cancel": self.cancel,
+				"green": self.save,
+				"red": self.cancel,
+				"blue": self.ppanelShortcut,
+			},-1)
+
+		self.list = [ ]
+		ConfigListScreen.__init__(self, self.list, session = session)
+
+		self.softcam = CamControl('softcam')
+		self.cardserver = CamControl('cardserver')
+
+		self.ecminfo = GetEcmInfo()
+		(newEcmFound, ecmInfo) = self.ecminfo.getEcm()
+		self["info"] = ScrollLabel("".join(ecmInfo))
+		self.EcmInfoPollTimer = eTimer()
+		self.EcmInfoPollTimer.callback.append(self.setEcmInfo)
+		self.EcmInfoPollTimer.start(1000)
+
+		softcams = self.softcam.getList()
+		cardservers = self.cardserver.getList()
+
+		self.softcams = ConfigSelection(choices = softcams)
+		self.softcams.value = self.softcam.current()
+
+		self.list.append(getConfigListEntry(_("Select Softcam"), self.softcams))
+		if cardservers:
+			self.cardservers = ConfigSelection(choices = cardservers)
+			self.cardservers.value = self.cardserver.current()
+			self.list.append(getConfigListEntry(_("Select Card Server"), self.cardservers))
+
+		self.list.append(getConfigListEntry(_("Restart softcam"), ConfigAction(self.restart, "s")))
+		if cardservers:
+			self.list.append(getConfigListEntry(_("Restart cardserver"), ConfigAction(self.restart, "c")))
+			self.list.append(getConfigListEntry(_("Restart both"), ConfigAction(self.restart, "sc")))
+
+		self["key_red"] = Label(_("Cancel"))
+		self["key_green"] = Label(_("OK"))
+		self["key_blue"] = Label(_("Info"))
+
+		self.onLayoutFinish.append(self.layoutFinished)
+
+	def setEcmInfo(self):
+		(newEcmFound, ecmInfo) = self.ecminfo.getEcm()
+		if newEcmFound:
+			self["info"].setText("".join(ecmInfo))
+
+	def layoutFinished(self):
+		self.setTitle(self.setup_title)
+
+	def ppanelShortcut(self):
+		ppanelFileName = '/etc/ppanels/' + self.softcams.value + '.xml'
+		if os.path.isfile(ppanelFileName) and os.path.isdir('/usr/lib/enigma2/python/Plugins/Extensions/PPanel'):
+			from Plugins.Extensions.PPanel.ppanel import PPanel
+			self.session.open(PPanel, name = self.softcams.value + ' PPanel', node = None, filename = ppanelFileName, deletenode = None)
+		else:
+			return 0
+
+	def restart(self, what):
+		self.what = what
+		if "s" in what:
+			if "c" in what:
+				msg = _("Please wait, restarting softcam and cardserver.")
+			else:
+				msg  = _("Please wait, restarting softcam.")
+		elif "c" in what:
+			msg = _("Please wait, restarting cardserver.")
+		self.mbox = self.session.open(MessageBox, msg, MessageBox.TYPE_INFO)
+		self.activityTimer = eTimer()
+		self.activityTimer.timeout.get().append(self.doStop)
+		self.activityTimer.start(100, False)
+
+	def doStop(self):
+		self.activityTimer.stop()
+		if "c" in self.what:
+			self.cardserver.command('stop')
+		if "s" in self.what:
+			self.softcam.command('stop')
+		self.oldref = self.session.nav.getCurrentlyPlayingServiceReference()
+		self.session.nav.stopService()
+		# Delay a second to give 'em a chance to stop
+		self.activityTimer = eTimer()
+		self.activityTimer.timeout.get().append(self.doStart)
+		self.activityTimer.start(1000, False)
+
+	def doStart(self):
+		self.activityTimer.stop()
+		del self.activityTimer
+		if "c" in self.what:
+			self.cardserver.select(self.cardservers.value)
+			self.cardserver.command('start')
+		if "s" in self.what:
+			self.softcam.select(self.softcams.value)
+			self.softcam.command('start')
+		if self.mbox:
+			self.mbox.close()
+		self.close()
+		self.session.nav.playService(self.oldref)
+		del self.oldref
+
+	def restartCardServer(self):
+		if hasattr(self, 'cardservers'):
+			self.restart("c")
+
+	def restartSoftcam(self):
+		self.restart("s")
+
+	def save(self):
+		what = ''
+		if hasattr(self, 'cardservers') and (self.cardservers.value != self.cardserver.current()):
+			what = 'sc'
+		elif self.softcams.value != self.softcam.current():
+			what = 's'
+		if what:
+			self.restart(what)
+		else:
+			from Components.PluginComponent import plugins
+			plugins.reloadPlugins()
+			config.misc.softcam_setup.extension_menu.save()
+			self.close()
+
+	def cancel(self):
+		self.close()

lib/python/Tools/camcontrol.py
@@ -1,0 +1,52 @@
+import os
+import enigma
+
+class CamControl:
+	'''CAM convention is that a softlink named /etc/init.c/softcam.* points
+	to the start/stop script.'''
+	def __init__(self, name):
+		self.name = name
+		self.link = '/etc/init.d/' + name
+		if not os.path.exists(self.link):
+			print "[CamControl] No softcam link?", self.link
+
+	def getList(self):
+		result = []
+		prefix = self.name + '.'
+		for f in os.listdir("/etc/init.d"):
+			if f.startswith(prefix):
+				result.append(f[len(prefix):])
+		return result
+
+	def current(self):
+		try:
+			l = os.readlink(self.link)
+			return os.path.split(l)[1].split('.', 2)[1]
+		except:
+			pass
+		return None
+
+	def command(self, cmd):
+		if os.path.exists(self.link):
+			print "Executing", self.link + ' ' + cmd
+			enigma.eConsoleAppContainer().execute(self.link + ' ' + cmd)
+
+	def select(self, which):
+		print "Selecting CAM:", which
+		if not which:
+			which = "None"
+		dst = self.name + '.' + which
+		if not os.path.exists('/etc/init.d/' + dst):
+			print "[CamControl] init script does not exist:", dst
+			return 
+		try:
+			os.unlink(self.link)
+		except:
+			pass
+		try:
+			os.symlink(dst, self.link);
+		except:
+			print "Failed to create symlink for softcam:", dst
+			import sys
+			print sys.exc_info()[:2]
+

data/menu.xml
@@ -60,6 +60,7 @@
 			</menu>
 			<menu weight="10" text="SoftCam / CI" entryID="cam_setup">
 				<id val="cam"/>
+				<item weight="10" level="1" text="Softcam setup..." entryID="softcam_setup"><screen module="SoftcamSetup" screen="SoftcamSetup"/></item>
 				<item weight="10" level="1" text="Common Interface" entryID="ci_setup" requires="CommonInterface"><screen module="Ci" screen="CiSelection"/></item>
 			</menu>
 			<item weight="15" level="0" text="Parental control" entryID="parental_setup"><screen module="ParentalControlSetup" screen="ParentalControlSetup"/></item>

lib/python/Screens/Makefile.am
@@ -16,4 +16,4 @@ install_PYTHON = \
 	SessionGlobals.py LocationBox.py WizardLanguage.py TaskView.py Rc.py VirtualKeyBoard.py \
 	TextBox.py FactoryReset.py RecordPaths.py UnhandledKey.py ServiceStopScreen.py \
 	InputDeviceSetup.py DVD.py SoftwareUpdate.py AutoDiseqc.py InstallWizard.py ScreenSaver.py \
-	Hotkey.py StreamingClientsInfo.py
+	Hotkey.py StreamingClientsInfo.py SoftcamSetup.py

lib/python/Tools/Makefile.am
@@ -5,4 +5,5 @@ install_PYTHON = \
 	KeyBindings.py BoundFunction.py ISO639.py Notifications.py __init__.py \
 	RedirectOutput.py StbHardware.py Import.py Event.py CList.py \
 	LoadPixmap.py Profile.py HardwareInfo.py Transponder.py ASCIItranslit.py \
-	Downloader.py Trashcan.py GetEcmInfo.py Alternatives.py TextBoundary.py
+	Downloader.py Trashcan.py GetEcmInfo.py Alternatives.py TextBoundary.py \
+	camcontrol.py

commit 99b59ad27257866e142e71adaf75a0b7ceb604e6

SoftcamSetup: Integrate missing commits from github
Apparently got the old sourceforge version. Merged the github changes
to fix a "." split problem and some oscam support.

Fixes: 1adb2eef0c86f7f1dd7bf5d621b77ada77380363

lib/python/Screens/SoftcamSetup.py
@@ -43,6 +43,7 @@ class SoftcamSetup(Screen, ConfigListScreen):
 		Screen.__init__(self, session)
 
 		self.setup_title = _("Softcam setup")
+		self.setTitle(self.setup_title)
 
 		self["actions"] = ActionMap(["OkCancelActions", "ColorActions", "CiSelectionActions"],
 			{
@@ -86,19 +87,17 @@ class SoftcamSetup(Screen, ConfigListScreen):
 		self["key_green"] = Label(_("OK"))
 		self["key_blue"] = Label(_("Info"))
 
-		self.onLayoutFinish.append(self.layoutFinished)
-
 	def setEcmInfo(self):
 		(newEcmFound, ecmInfo) = self.ecminfo.getEcm()
 		if newEcmFound:
 			self["info"].setText("".join(ecmInfo))
 
-	def layoutFinished(self):
-		self.setTitle(self.setup_title)
-
 	def ppanelShortcut(self):
+		if "oscam" in self.softcams.value.lower() and os.path.isfile('/usr/lib/enigma2/python/Plugins/Extensions/OscamStatus/plugin.py'):
+			from Plugins.Extensions.OscamStatus.plugin import OscamStatus
+			self.session.open(OscamStatus)
 		ppanelFileName = '/etc/ppanels/' + self.softcams.value + '.xml'
-		if os.path.isfile(ppanelFileName) and os.path.isdir('/usr/lib/enigma2/python/Plugins/Extensions/PPanel'):
+		if os.path.isfile(ppanelFileName) and os.path.isfile('/usr/lib/enigma2/python/Plugins/Extensions/PPanel/plugin.py'):
 			from Plugins.Extensions.PPanel.ppanel import PPanel
 			self.session.open(PPanel, name = self.softcams.value + ' PPanel', node = None, filename = ppanelFileName, deletenode = None)
 		else:

lib/python/Tools/camcontrol.py
@@ -21,7 +21,8 @@ class CamControl:
 	def current(self):
 		try:
 			l = os.readlink(self.link)
-			return os.path.split(l)[1].split('.', 2)[1]
+			prefix = self.name + '.'
+			return os.path.split(l)[1].split(prefix, 2)[1]
 		except:
 			pass
 		return None