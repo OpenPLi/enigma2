24c85c60c20c49da73b0d482b686bf29b40e1ac1
Persian Professionals <persianpros@yahoo.com>
2017-02-15 15:25:17 +0330

    Cleanup EventName.py

--- lib/python/Components/Converter/EventName.py
+++ lib/python/Components/Converter/EventName.py
@@ -1,6 +1,6 @@
 from Components.Converter.Converter import Converter
 from Components.Element import cached
-from Components.Converter.genre import getGenreStringLong, getGenreStringSub
+from Components.Converter.genre import getGenreStringSub
 
 class EventName(Converter, object):
 	NAME = 0
