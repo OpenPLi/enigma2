549b2105b621787de15d0961f339af615a2f04a4
Persian Professionals <persianpros@yahoo.com>
2017-02-19 13:18:25 +0330

    Cleanup SetupDevices.py (#739)

--- lib/python/Components/SetupDevices.py
+++ lib/python/Components/SetupDevices.py
@@ -8,14 +8,14 @@ def InitSetupDevices():
 	def timezoneNotifier(configElement):
 		timezones.activateTimezone(configElement.index)
 
-	config.timezone = ConfigSubsection();
+	config.timezone = ConfigSubsection()
 	config.timezone.val = ConfigSelection(default = timezones.getDefaultTimezone(), choices = timezones.getTimezoneList())
 	config.timezone.val.addNotifier(timezoneNotifier)
 
 	def keyboardNotifier(configElement):
 		keyboard.activateKeyboardMap(configElement.index)
 
-	config.keyboard = ConfigSubsection();
+	config.keyboard = ConfigSubsection()
 	config.keyboard.keymap = ConfigSelection(default = keyboard.getDefaultKeyboardMap(), choices = keyboard.getKeyboardMaplist())
 	config.keyboard.keymap.addNotifier(keyboardNotifier)
 
@@ -23,14 +23,14 @@ def InitSetupDevices():
 		language.activateLanguage(configElement.value)
 
 	config.osd = ConfigSubsection()
-	config.osd.language = ConfigText(default = "en_EN");
+	config.osd.language = ConfigText(default = "en_EN")
 	config.osd.language.addNotifier(languageNotifier)
 
-	config.parental = ConfigSubsection();
+	config.parental = ConfigSubsection()
 	config.parental.lock = ConfigOnOff(default = False)
 	config.parental.setuplock = ConfigOnOff(default = False)
 
-	config.expert = ConfigSubsection();
+	config.expert = ConfigSubsection()
 	config.expert.satpos = ConfigOnOff(default = True)
 	config.expert.fastzap = ConfigOnOff(default = True)
 	config.expert.skipconfirm = ConfigOnOff(default = False)
