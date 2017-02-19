f4ed3f205f4838ea2fbb31420d4f58a9c3cd5866
Persian Professionals <persianpros@yahoo.com>
2017-02-19 10:36:28 +0330

    Cleanup GetEcmInfo.py

--- lib/python/Tools/GetEcmInfo.py
+++ lib/python/Tools/GetEcmInfo.py
@@ -92,7 +92,7 @@ class GetEcmInfo:
 					if ecm[1].startswith('SysID'):
 						info['prov'] = ecm[1].strip()[6:]
 					if info['response'] and 'CaID 0x' in ecm[0] and 'pid 0x' in ecm[0]:
-						self.textvalue = self.textvalue + " (0.%ss)" % info['response']
+						self.textvalue += " (0.%ss)" % info['response']
 						info['caid'] = ecm[0][ecm[0].find('CaID 0x')+7:ecm[0].find(',')]
 						info['pid'] = ecm[0][ecm[0].find('pid 0x')+6:ecm[0].find(' =')]
 						info['provid'] = info.get('prov', '0')[:4]
