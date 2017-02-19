e3d8e3495c42d2e03396c8e91728c464152745fa
littlesat <littlesat99@yahoo.com>
2017-02-19 13:26:18 +0100

    Instead of current language use "Choose Language" at red button in Wizard
    Previously a while ago you toggle through the languages with the red
    button in the wizard so it was logical that the current language was shown
    there, but since you get to the list of languages and quickly select one I
    think it is better to rename this lable "Choose Language".

--- lib/python/Screens/WizardLanguage.py
+++ lib/python/Screens/WizardLanguage.py
@@ -6,13 +6,7 @@ from LanguageSelection import LanguageWizard
 class WizardLanguage(Wizard):
 	def __init__(self, session, showSteps = True, showStepSlider = True, showList = True, showConfig = True):
 		Wizard.__init__(self, session, showSteps, showStepSlider, showList, showConfig)
-		self["languagetext"] = Label()
-		self.LanguageWizardCallback()
+		self["languagetext"] = Label(_("Change Language"))
 
 	def red(self):
-		self.session.openWithCallback(self.LanguageWizardCallback, LanguageWizard)
-
-	def LanguageWizardCallback(self, key="1234"):
-		print "languageSelect", key
-		self["languagetext"].setText(self.getTranslation(language.getLanguageList()[language.getActiveLanguageIndex()][1][0]))
-		self.resetCounter()
+		self.session.open(LanguageWizard)
