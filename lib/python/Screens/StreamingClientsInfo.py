3f6862c5a9ac4a38ce984873b74a9f051e5930ed
Athanasios Oikonomou <athoik@gmail.com>
2017-02-25 20:11:45 +0200

    Allow stopping all streams from StreamingClientsInfo (#761)
    * StreamingClientsInfo: Allow stopping all streams
    
    Allows to stop all streams, releasing tuners back to enigma2
    
    * streamserver: introduce stopStreamClient function
    
    The stopStreamClient can be used to stop specific stream
    by passing ip and serviceref.
    
    TODO: Above function should be integrated into StreamClientInfo,
    where  streaming clients can be selected and stopped.
    
    A possible improvement with a small modification might be the pass
    of ip or serviceref only, in order to stop all clients from the same ip
    or all clients on the same serviceref.

--- lib/python/Screens/StreamingClientsInfo.py
+++ lib/python/Screens/StreamingClientsInfo.py
@@ -1,23 +1,37 @@
 from Screen import Screen
 from Components.ActionMap import ActionMap
+from Components.Button import Button
 from Components.ScrollLabel import ScrollLabel
 from Components.Converter.ClientsStreaming import ClientsStreaming
 import skin
-
+import enigma
 
 class StreamingClientsInfo(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		self.setTitle(_("Streaming clients info"))
+
 		clients = ClientsStreaming("INFO_RESOLVE")
 		text = clients.getText()
 
 		self["ScrollLabel"] = ScrollLabel(text or _("No stream clients"))
+		self["key_red"] = Button(text and _("Stop Streams") or "")
 
 		self["actions"] = ActionMap(["ColorActions", "SetupActions", "DirectionActions"],
 		{
 			"cancel": self.close,
 			"ok": self.close,
+			"red": self.stopStreams,
 			"up": self["ScrollLabel"].pageUp,
 			"down": self["ScrollLabel"].pageDown
 		})
+
+	def stopStreams(self):
+		streamServer = enigma.eStreamServer.getInstance()
+		if not streamServer:
+			return
+
+		for x in streamServer.getConnectedClients():
+			streamServer.stopStream()
+
+		self.close()
