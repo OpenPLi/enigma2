2fb52c1a5bb34e2bb673c9d57e921ed36e8aaf1c
ims21 <martin@volyne.cz>
2017-03-18 05:45:34 +0100

    fixed GSOD in deepstanby for boxes without hdmi output (#854)

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -12,6 +12,7 @@ from Components.Harddisk import internalHDDNotSleeping
 from Components.SystemInfo import SystemInfo
 from GlobalActions import globalActionMap
 from enigma import eDVBVolumecontrol, eTimer, eDVBLocalTimeHandler, eServiceReference
+from Tools.HardwareInfo import HardwareInfo
 
 inStandby = None
 
@@ -266,7 +267,7 @@ class TryQuitMainloop(MessageBox):
 				if not inStandby:
 					if os.path.exists("/usr/script/standby_enter.sh"):
 						Console().ePopen("/usr/script/standby_enter.sh")
-					if config.hdmicec.enabled.value and config.hdmicec.control_tv_standby.value and config.hdmicec.next_boxes_detect.value:
+					if HardwareInfo().has_hdmi() and config.hdmicec.enabled.value and config.hdmicec.control_tv_standby.value and config.hdmicec.next_boxes_detect.value:
 						import Components.HdmiCec
 						Components.HdmiCec.hdmi_cec.secondBoxActive()
 						self.delay = eTimer()
