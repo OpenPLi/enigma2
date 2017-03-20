ced395425efa5e9c1f94dec86ddb262899aafc47
Dima73 <Dima-73@inbox.lv>
2017-03-20 19:29:57 +0200

    [RemoteControlType] Ensure that zero does not get written to the remote control proc file
    -thanks  birdman
    and
    -add support zgemma h7
    -add setup tittle

--- lib/python/Screens/InputDeviceSetup.py
+++ lib/python/Screens/InputDeviceSetup.py
@@ -311,7 +311,7 @@ class RemoteControlType(Screen, ConfigListScreen):
 			("18", _("F1/F3/F4/F4-TURBO")),
 			("19", _("HD2400")),
 			("20", _("Zgemma Star S/2S/H1/H2")),
-			("21", _("Zgemma H.S/H.2S/H.2H/H5"))
+			("21", _("Zgemma H.S/H.2S/H.2H/H5/H7"))
 		]
 
 	defaultRcList = [
@@ -346,12 +346,14 @@ class RemoteControlType(Screen, ConfigListScreen):
 			("sh1", 20),
 			("h3", 21),
 			("h5", 21),
+			("h7", 21),
 			("et7000mini", 16)
 		]
 
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		self.skinName = ["RemoteControlType", "Setup" ]
+		self.setTitle(_("Remote control type setup"))
 
 		self["actions"] = ActionMap(["SetupActions"],
 		{
@@ -370,7 +372,7 @@ class RemoteControlType(Screen, ConfigListScreen):
 		self.list.append(getConfigListEntry(_("Remote control type"), self.rctype))
 		self["config"].list = self.list
 
-		self.defaultRcType = None
+		self.defaultRcType = 0
 		self.getDefaultRcType()
 
 	def getDefaultRcType(self):
@@ -379,6 +381,8 @@ class RemoteControlType(Screen, ConfigListScreen):
 			if x[0] in data:
 				self.defaultRcType = x[1]
 				break
+		if self.defaultRcType == 0:
+			self.defaultRcType = iRcTypeControl.readRcType()
 
 	def setDefaultRcType(self):
 		iRcTypeControl.writeRcType(self.defaultRcType)
