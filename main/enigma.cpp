d842ed2440d5bd495ee1fdab76013ce52a58ed60
Boris <foxbob@mail.ru>
2017-04-22 17:46:53 +0300

    add-focus-animation-for-listbox.patch

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -366,7 +366,12 @@ void setAnimation_speed(int speed)
 {
  	gles_set_animation_speed(speed);
 }
+void setAnimation_current_listbox(int a)
+{
+	gles_set_animation_listbox_func(a);
+}
 #else
 void setAnimation_current(int a) {}
 void setAnimation_speed(int speed) {}
+void setAnimation_current_listbox(int a) {}
 #endif
