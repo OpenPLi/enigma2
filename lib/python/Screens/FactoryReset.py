0b55fd1ba1b283ed9adfb3a42af6f9d00a7b74b4
ims21 <martin@volyne.cz>
2017-05-01 11:23:37 +0200

    fix typos in cs.po (#903)
    * fix typos in cs.po
    
    * Factory reset - critical operations should have No as default

--- lib/python/Screens/FactoryReset.py
+++ lib/python/Screens/FactoryReset.py
@@ -7,7 +7,7 @@ class FactoryReset(MessageBox, ProtectedScreen):
 		MessageBox.__init__(self, session, _("When you do a factory reset, you will lose ALL your configuration data\n"
 			"(including bouquets, services, satellite data ...)\n"
 			"After completion of factory reset, your receiver will restart automatically!\n\n"
-			"Really do a factory reset?"), MessageBox.TYPE_YESNO)
+			"Really do a factory reset?"), MessageBox.TYPE_YESNO, default=False)
 		self.skinName = "MessageBox"
 		ProtectedScreen.__init__(self)
 
