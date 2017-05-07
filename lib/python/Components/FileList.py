b643ada33894738f42d8cbfe21f51b9c1aa9ced7
Persian Professionals <persianpros@yahoo.com>
2017-05-07 17:40:43 +0430

    FileList.py: Cleanup

--- lib/python/Components/FileList.py
+++ lib/python/Components/FileList.py
@@ -3,8 +3,7 @@ import re
 from MenuList import MenuList
 from Components.Harddisk import harddiskmanager
 from Tools.Directories import SCOPE_CURRENT_SKIN, resolveFilename, fileExists
-from enigma import RT_HALIGN_LEFT, eListboxPythonMultiContent, \
-	eServiceReference, eServiceCenter, gFont
+from enigma import RT_HALIGN_LEFT, eListboxPythonMultiContent, eServiceReference, eServiceCenter, gFont
 from Tools.LoadPixmap import LoadPixmap
 import skin
 
