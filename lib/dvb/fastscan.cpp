a2dd9dc8169cee2e512c3207004d645205df07fa
littlesat <littlesat99@yahoo.com>
2017-05-13 23:17:51 +0200

    Revert "fastscan: limit results to package orbital position (#927)"
    This reverts commit 3b0f97467da7bbab5b64d8661dc3cf0eeeb7271b.
    
    As it was merged too early and it now always limits the fastscan to one
    satellite.

--- lib/dvb/fastscan.cpp
+++ lib/dvb/fastscan.cpp
@@ -483,13 +483,6 @@ void eFastScan::parseResult()
 			eDVBChannelID chid;
 			int orbitalposbcd = (*it)->getOrbitalPosition();
 			int orbitalpos = (orbitalposbcd & 0x0f) + ((orbitalposbcd >> 4) & 0x0f) * 10 + ((orbitalposbcd >> 8) & 0x0f) * 100;
-
-			if (transponderParameters.orbital_position != orbitalpos)
-			{
-				eDebug("[eFastScan] dropping this transponder, it's on another satellite %d.", orbitalpos);
-				continue;
-			}
-
 			chid.dvbnamespace = eDVBNamespace(orbitalpos<<16);
 			chid.transport_stream_id = eTransportStreamID((*it)->getTransportStreamId());
 			chid.original_network_id = eOriginalNetworkID((*it)->getOriginalNetworkId());
