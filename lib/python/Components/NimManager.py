OpenPLi/enigma2

commit 7de73064e866d59467794e6142eacf127a0481fa

Introduce nimListCompressed
Show dual tuner and FBC tuners as one tuner on the about screen of the
box. E.g a FBC tuner will be shown as e.g. Tuner A-H... and dual tuner as
Tuner Q-R. So one tuner with a range before it.

lib/python/Components/NimManager.py
@@ -605,7 +605,7 @@ class NIM(object):
 		return (self.frontend_id is not None) and os.access("/proc/stb/frontend/%d/fbc_id" % self.frontend_id, os.F_OK)
 
 	def isFBCRoot(self):
-		return self.isFBCTuner() and (self.slot % 8 < 2)
+		return self.isFBCTuner() and (self.slot % 8 < (self.getType() == "DVB-C" and 1 or 2))
 
 	def isFBCLink(self):
 		return self.isFBCTuner() and not (self.slot % 8 < (self.getType() == "DVB-C" and 1 or 2))
@@ -617,19 +617,22 @@ class NIM(object):
 
 	friendly_type = property(getFriendlyType)
 
+	def getFullDescription(self):
+		return self.empty and _("(empty)") or "%s (%s)" % (self.description, self.isSupported() and self.friendly_type or _("not supported"))
+
 	def getFriendlyFullDescription(self):
-		nim_text = self.slot_name + ": "
+		return "%s: %s" % (self.slot_name, self.getFullDescription())
 
-		if self.empty:
-			nim_text += _("(empty)")
-		elif not self.isSupported():
-			nim_text += self.description + " (" + _("not supported") + ")"
-		else:
-			nim_text += self.description + " (" + self.friendly_type + ")"
-
-		return nim_text
+	def getFriendlyFullDescriptionCompressed(self):
+		if self.isFBCRoot():
+			return "%s-%s: %s" % (self.slot_name, chr(ord('A') + self.slot + 7), self.getFullDescription())
+		#compress by combining dual tuners by checking if the next tuner has an rf switch
+		elif os.access("/proc/stb/frontend/%d/rf_switch" % (self.frontend_id + 1), os.F_OK):
+			return "%s-%s: %s" % (self.slot_name, chr(ord('A') + self.slot + 1), self.getFullDescription())
+		return getFriendlyFullDescription()
 
 	friendly_full_description = property(getFriendlyFullDescription)
+	friendly_full_description_compressed = property(getFriendlyFullDescriptionCompressed)
 	config_mode = property(lambda self: config.Nims[self.slot].configMode.value)
 	config = property(lambda self: config.Nims[self.slot])
 	empty = property(lambda self: self.getType() is None)
@@ -831,9 +834,11 @@ class NimManager:
 		InitNimManager(self)	#init config stuff
 
 	# get a list with the friendly full description
-	def nimList(self, showFBCTuners=True):
-		list = [slot.friendly_full_description for slot in self.nim_slots if showFBCTuners or not slot.isFBCLink()]
-		return list
+	def nimList(self):
+		return [slot.friendly_full_description for slot in self.nim_slots]
+
+	def nimListCompressed(self):
+		return [slot.friendly_full_description_compressed for slot in self.nim_slots if not (slot.isFBCLink() or slot.internally_connectable)]
 
 	def getSlotCount(self):
 		return len(self.nim_slots)

lib/python/Screens/About.py
@@ -60,7 +60,7 @@ class About(Screen):
 		self["TunerHeader"] = StaticText(_("Detected NIMs:"))
 		AboutText += "\n" + _("Detected NIMs:") + "\n"
 
-		nims = nimmanager.nimList(showFBCTuners=False)
+		nims = nimmanager.nimListCompressed()
 		for count in range(len(nims)):
 			if count < 4:
 				self["Tuner" + str(count)] = StaticText(nims[count])

commit 60c5dee2e90cd9f0f542da35c35ce6282c0f443d

NimManager - fix crash getFriendlyFullDescriptionCompressed introduced in https://github.com/OpenPLi/enigma2/commit/7de73064e866d59467794e6142eacf127a0481fa
NameError: global name 'getFriendlyFullDescription' is not defined

lib/python/Components/NimManager.py
@@ -629,7 +629,7 @@ class NIM(object):
 		#compress by combining dual tuners by checking if the next tuner has an rf switch
 		elif os.access("/proc/stb/frontend/%d/rf_switch" % (self.frontend_id + 1), os.F_OK):
 			return "%s-%s: %s" % (self.slot_name, chr(ord('A') + self.slot + 1), self.getFullDescription())
-		return getFriendlyFullDescription()
+		return self.getFriendlyFullDescription()
 
 	friendly_full_description = property(getFriendlyFullDescription)
 	friendly_full_description_compressed = property(getFriendlyFullDescriptionCompressed)