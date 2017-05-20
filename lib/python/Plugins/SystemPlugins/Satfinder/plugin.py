8f6455dc61375b38f94ebac4e49c0cd24488855c
littlesat <littlesat99@yahoo.com>
2017-05-20 15:18:08 +0200

    Revert "Satfinder - plugin exit goes in menu one level back only, as usually (#909)"
    This reverts commit 234609a8c255c4004193051db234e3fc36924a82.
    
    As it was intended to "roll-back" to the InfoBar when you select a just
    scanned service

--- lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
+++ lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
@@ -501,7 +501,7 @@ def SatfinderMain(session, close=None, **kwargs):
 	if len(nimList) == 0:
 		session.open(MessageBox, _("No satellite, terrestrial or cable tuner is configured. Please check your tuner setup."), MessageBox.TYPE_ERROR)
 	else:
-		session.open(Satfinder)
+		session.openWithCallback(close, Satfinder)
 
 def SatfinderStart(menuid, **kwargs):
 	if menuid == "scan":
