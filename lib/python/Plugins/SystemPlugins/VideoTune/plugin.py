63a28208fbada0ca9c28dcb07c4aa85eec9c9df7
ims <ims21@users.sourceforge.net>
2017-06-15 17:32:40 +0200

    fix skin selector, cosmetic code

--- lib/python/Plugins/SystemPlugins/VideoTune/plugin.py
+++ lib/python/Plugins/SystemPlugins/VideoTune/plugin.py
@@ -7,10 +7,9 @@ def videoFinetuneMain(session, **kwargs):
 
 def startSetup(menuid):
 	# show only in the menu when set at expert level
-	if menuid != "video" or config.usage.setup_level.index < 2:
-		return [ ]
-
-	return [(_("Testscreens"), videoFinetuneMain, "video_finetune", None)]
+	if menuid == "video" and config.usage.setup_level.index == 2:
+		return [(_("Testscreens"), videoFinetuneMain, "video_finetune", None)]
+	return [ ]
 
 def Plugins(**kwargs):
 	return PluginDescriptor(name=_("Testscreens"), description=_("Testscreens that are helpfull to fine-tune your display"), where = PluginDescriptor.WHERE_MENU, needsRestart = False, fnc=startSetup)
