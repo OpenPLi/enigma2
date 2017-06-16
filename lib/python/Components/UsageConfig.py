2989d38ee753b992226b188d35c1401457c0a51c
Littlesat <littlesat99@yahoo.com>
2017-06-16 12:14:16 +0200

    Add option to Toggle 12/8 character display mode when box has XcoreVFD
    Thanks Dima73!

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -360,6 +360,12 @@ def InitUsageConfig():
 		config.usage.wakeOnLAN = ConfigYesNo(default = False)
 		config.usage.wakeOnLAN.addNotifier(wakeOnLANChanged)
 
+	if SystemInfo["hasXcoreVFD"]:
+		def set12to8characterVFD(configElement):
+			open(SystemInfo["hasXcoreVFD"], "w").write(configElement.value == "8" and "1" or "0")
+		config.usage.toggle12to8characterVFD = ConfigSelection(default = "8", choices = [("12", "12"),("8", "8")])
+		config.usage.toggle12to8characterVFD.addNotifier(set12to8characterVFD)
+
 	config.epg = ConfigSubsection()
 	config.epg.eit = ConfigYesNo(default = True)
 	config.epg.mhw = ConfigYesNo(default = False)
