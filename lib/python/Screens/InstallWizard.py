02c6f2ac9d32d9ce98795381093af8994f1bc21f
littlesat <littlesat99@yahoo.com>
2017-06-17 10:17:35 +0200

    Add plugin-install screen to the startWizard

--- lib/python/Screens/InstallWizard.py
+++ lib/python/Screens/InstallWizard.py
@@ -17,6 +17,7 @@ class InstallWizard(Screen, ConfigListScreen):
 
 	STATE_UPDATE = 0
 	STATE_CHOISE_CHANNELLIST = 1
+	INSTALL_PLUGINS = 2
 
 	def __init__(self, session, args = None):
 		Screen.__init__(self, session)
@@ -48,6 +49,9 @@ class InstallWizard(Screen, ConfigListScreen):
 			modes = {"19e-23e-basis": "Astra1 Astra3 basis", "19e-23e": "Astra 1 Astra 3", "19e-23e-28e": "Astra 1 Astra 2 Astra 3", "13e-19e-23e-28e": "Astra 1 Astra 2 Astra 3 Hotbird", "kabelnl": "Kabel-NL"}
 			self.channellist_type = ConfigSelection(choices = modes, default = "19e-23e-basis")
 			self.createMenu()
+		elif self.index == self.INSTALL_PLUGINS:
+			self.enabled = ConfigYesNo(default = True)
+			self.createMenu()
 
 	def checkNetworkCB(self, data):
 		if data < 3:
@@ -75,6 +79,8 @@ class InstallWizard(Screen, ConfigListScreen):
 			self.list.append(getConfigListEntry(_("Install channel list"), self.enabled))
 			if self.enabled.value:
 				self.list.append(getConfigListEntry(_("Channel list type"), self.channellist_type))
+		elif self.index == self.INSTALL_PLUGINS:
+			self.list.append(getConfigListEntry(_("Do you want to install plugins"), self.enabled))
 		self["config"].list = self.list
 		self["config"].l.setList(self.list)
 
@@ -95,6 +101,9 @@ class InstallWizard(Screen, ConfigListScreen):
 			self.session.open(InstallWizardIpkgUpdater, self.index, _('Please wait (updating packages)'), IpkgComponent.CMD_UPDATE)
 		elif self.index == self.STATE_CHOISE_CHANNELLIST and self.enabled.value:
 			self.session.open(InstallWizardIpkgUpdater, self.index, _('Please wait (downloading channel list)'), IpkgComponent.CMD_REMOVE, {'package': 'enigma2-plugin-settings-hans-' + self.channellist_type.value})
+		elif self.index == self.INSTALL_PLUGINS and self.enabled.value:
+			from PluginBrowser import PluginDownloadBrowser
+			self.session.open(PluginDownloadBrowser, 0)
 
 class InstallWizardIpkgUpdater(Screen):
 	skin = """
