702c9f5740c3875040a53b2745139e68335e8d8c
littlesat <littlesat99@yahoo.com>
2017-07-08 10:31:54 +0200

    Show autorestore progress in console on any box
    The autorestore procedure sometimes kan take a longer time. Boxes that
    have a console can show the progress, but most boxes can't. This change
    shows a console to show the progress of the autorestore.sh script on any
    box.

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,4 +1,5 @@
 from Wizard import wizardManager
+from Screen import Screen
 from Screens.MessageBox import MessageBox
 from Screens.WizardLanguage import WizardLanguage
 from Screens.Rc import Rc
@@ -9,8 +10,11 @@ except:
 	OverscanWizard = None
 
 from Components.Pixmap import Pixmap
+from Components.ScrollLabel import ScrollLabel
 from Components.config import config, ConfigBoolean, configfile
 from LanguageSelection import LanguageWizard
+from enigma import eConsoleAppContainer
+
 import os
 
 config.misc.firstrun = ConfigBoolean(default = True)
@@ -55,6 +59,44 @@ class AutoRestoreWizard(MessageBox):
 		else:
 			MessageBox.close(self)
 
+class AutoInstallWizard(Screen):
+	skin = """<screen name="AutoInstall" position="fill" flags="wfNoBorder">
+		<panel position="left" size="5%,*"/>
+		<panel position="right" size="5%,*"/>
+		<panel position="top" size="*,5%"/>
+		<panel position="bottom" size="*,5%"/>
+		<widget name="AboutScrollLabel" font="Fixed;20" position="fill"/>
+	</screen>"""
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		self["AboutScrollLabel"] = ScrollLabel(_("Please wait"))
+		self.container = eConsoleAppContainer()
+		self.container.appClosed.append(self.appClosed)
+		self.container.dataAvail.append(self.dataAvail)
+		self.onLayoutFinish.append(self.run_console)
+
+	def run_console(self):
+		self["AboutScrollLabel"].setText("")
+		try:
+			if self.container.execute("/etc/init.d/autoinstall.sh"):
+				raise Exception, "failed to execute autoinstall.sh script"
+				self.appClosed(True)
+		except Exception, e:
+			self.appClosed(True)
+
+	def dataAvail(self, data):
+		self["AboutScrollLabel"].appendText(data)
+
+	def appClosed(self, retval):
+		if retval:
+			self["AboutScrollLabel"].setText(_("Some error occured - Please try later"))
+		self.container.appClosed.remove(self.appClosed)
+		self.container.dataAvail.remove(self.dataAvail)
+		self.container = None
+		os.remove("/etc/.doAutoinstall")
+		self.close(3)
+
+wizardManager.registerWizard(AutoInstallWizard, os.path.isfile("/etc/.doAutoinstall"), priority=0)
 wizardManager.registerWizard(AutoRestoreWizard, config.misc.languageselected.value and config.misc.firstrun.value and checkForAvailableAutoBackup(), priority=0)
 wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.value, priority=10)
 if OverscanWizard:
