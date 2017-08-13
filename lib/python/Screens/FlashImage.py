3c6812d9612b7e0dbfa1a57ca9c4082993abfba1
littlesat <littlesat99@yahoo.com>
2017-08-13 19:34:38 +0200

    FlashImage: Press exit to close instead of abort
    Thanks IMS

--- lib/python/Screens/FlashImage.py
+++ lib/python/Screens/FlashImage.py
@@ -280,7 +280,7 @@ class FlashImage(Screen):
 		self.containerofgwrite = None
 		if retval == 0:
 			self["header"].setText(_("Flashing image succesfull"))
-			self["info"].setText(_("%s\nPress exit to abort") % self.imagename)
+			self["info"].setText(_("%s\nPress exit to close") % self.imagename)
 			self["progress"].hide()
 		else:
 			self.session.openWithCallback(self.abort, MessageBox, _("Flashing image was not succesfull\n%s") % self.imagename, type=MessageBox.TYPE_ERROR, simple=True)
