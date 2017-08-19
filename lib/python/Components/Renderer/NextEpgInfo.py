079623b2ac53dc8307c3e29cad07666a63413bbd
ims <ims21@users.sourceforge.net>
2017-08-19 20:25:04 +0200

    NextEpgInfo - added some formating parameters
    noLabel ... if is set to 1 then is not show 'Next' label
    labelColor ... label color
    timeColor ... time color
    
    If color parameters missing, is used color defined in foregroundColor

--- lib/python/Components/Renderer/NextEpgInfo.py
+++ lib/python/Components/Renderer/NextEpgInfo.py
@@ -2,6 +2,7 @@ from Components.VariableText import VariableText
 from Renderer import Renderer
 from enigma import eLabel, eEPGCache, eServiceReference
 from time import localtime, strftime
+from skin import parseColor
 
 class NextEpgInfo(Renderer, VariableText):
 	def __init__(self):
@@ -9,6 +10,11 @@ class NextEpgInfo(Renderer, VariableText):
 		VariableText.__init__(self)
 		self.epgcache = eEPGCache.getInstance()
 		self.numberOfItems = 1
+		self.hideLabel = 0
+		self.timecolor = ""
+		self.labelcolor = ""
+		self.foregroundColor = "00?0?0?0"
+
 	GUI_WIDGET = eLabel
 
 	def changed(self, what):
@@ -22,17 +28,54 @@ class NextEpgInfo(Renderer, VariableText):
 					if self.numberOfItems == 1:
 						event = self.epgcache.getNextTimeEntry()
 						if event:
-							self.text = "%s: %s" % (pgettext("now/next: 'next' event label", "Next"), event.getEventName())
+							if self.hideLabel:
+								self.text = "%s%s %s%s" % (self.timecolor, strftime("%H:%M", localtime(event.getBeginTime())), self.foregroundColor, event.getEventName())
+							else:
+								self.text = "%s%s: %s%s" % (self.labelcolor, pgettext("now/next: 'next' event label", "Next"), self.foregroundColor, event.getEventName())
 					else:
 						for x in range(self.numberOfItems):
 							event = self.epgcache.getNextTimeEntry()
 							if event:
-								self.text = "%s\n%s %s" % (self.text, strftime("%H:%M", localtime(event.getBeginTime())), event.getEventName())
-						self.text = self.text and "%s%s" % (pgettext("now/next: 'next' event label", "Next"), self.text) or ""
+								self.text += "%s%s %s%s\n" % (self.timecolor, strftime("%H:%M", localtime(event.getBeginTime())), self.foregroundColor, event.getEventName())
+						if not self.hideLabel:
+							self.text = self.text and "%s%s\n%s" % (self.labelcolor, pgettext("now/next: 'next' event label", "Next"), self.text) or ""
 
 	def applySkin(self, desktop, parent):
+		attribs = []
 		for (attrib, value) in self.skinAttributes:
 			if attrib == "NumberOfItems":
 				self.numberOfItems = int(value)
-				self.skinAttributes.remove((attrib, value))
+				attribs.append((attrib, value))
+			if attrib == "noLabel":
+				self.hideLabel = int(value)
+				attribs.append((attrib, value))
+			if attrib == "timeColor":
+				self.timecolor = self.hex2strColor(parseColor(value).argb())
+				attribs.append((attrib, value))
+			if attrib == "labelColor":
+				self.labelcolor = self.hex2strColor(parseColor(value).argb())
+				attribs.append((attrib, value))
+			if attrib == "foregroundColor":
+				self.foregroundColor = self.hex2strColor(parseColor(value).argb())
+				attribs.append((attrib, value))
+		for (attrib, value) in attribs:
+			self.skinAttributes.remove((attrib, value))
+		self.timecolor = self.formatColorString(self.timecolor)
+		self.labelcolor  = self.formatColorString(self.labelcolor)
+		self.foregroundColor  = self.formatColorString(self.foregroundColor)
 		return Renderer.applySkin(self, desktop, parent)
+
+# 	hex:
+#	0 1 2 3 4 5 6 7 8 9 a b c d e f
+# 	converts to:
+#	0 1 2 3 4 5 6 7 8 9 : ; < = > ?
+	def hex2strColor(self, rgb):
+		out = ""
+		for i in range(28,-1,-4):
+			out += "%s" % chr(0x30 + (rgb>>i & 0xf))
+		return  out
+
+	def formatColorString(self, color):
+		if color:
+			return "%s%s" % ('\c', color)
+		return "%s%s" % ('\c', self.foregroundColor)
