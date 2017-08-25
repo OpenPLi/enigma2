10fae5a29883d5f051fa6f279945251704e74d63
Littlesat <littlesat99@yahoo.com>
2017-08-25 21:16:10 +0200

    Revert "frontend: properly initialize dvb_frontend_info on simulated frontends"
    This reverts commit 36abc4921da3d84c611332f6758aefd9e944550b.
    
    As it has at least a side effect on a VuUltimo4k with the fcc tuner. After (re)start of enigma2 it is mentioned that no tuners are available. After some zapping tuners become available.

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -565,7 +565,7 @@ int eDVBFrontend::openFrontend()
 	m_state=stateIdle;
 	m_tuning=0;
 
-	if (1) // always open in order to initialize fe_info
+	if (!m_simulate)
 	{
 		eDebug("[eDVBFrontend] opening frontend %d", m_dvbid);
 		if (m_fd < 0)
@@ -674,11 +674,8 @@ int eDVBFrontend::openFrontend()
 		{
 			m_simulate_fe->m_delsys = m_delsys;
 		}
-		if (!m_simulate)
-		{
-			m_sn = eSocketNotifier::create(eApp, m_fd, eSocketNotifier::Read, false);
-			CONNECT(m_sn->activated, eDVBFrontend::feEvent);
-		}
+		m_sn = eSocketNotifier::create(eApp, m_fd, eSocketNotifier::Read, false);
+		CONNECT(m_sn->activated, eDVBFrontend::feEvent);
 	}
 
 	m_multitype = m_delsys[SYS_DVBS] && (m_delsys[SYS_DVBT] || m_delsys[SYS_DVBC_ANNEX_A]);
