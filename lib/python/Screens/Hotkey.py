0003e00722312712a1f24ec914b892ec5dd4a055
Littlesat <littlesat99@yahoo.com>
2017-10-10 09:11:05 +0200

    Hotkey: Remove not required check if Ppanel plugin exists

--- lib/python/Screens/Hotkey.py
+++ lib/python/Screens/Hotkey.py
@@ -649,7 +649,7 @@ class InfoBarHotkey():
 					self.session.open(PPanel, name=selected[1] + ' PPanel', node=None, filename=ppanelFileName, deletenode=None)
 			elif selected[0] == "Shellscript":
 				command = '/usr/script/' + selected[1] + ".sh"
-				if os.path.isfile(command) and os.path.isdir('/usr/lib/enigma2/python/Plugins/Extensions/PPanel'):
+				if os.path.isfile(command):
 					from Screens.Console import Console
 					self.session.open(Console, selected[1] + " shellscript", [command], closeOnSuccess=selected[1].startswith('!'), showStartStopText=False)
 			elif selected[0] == "Menu":
