f51a64d7f425d425a4486cbdf3923ec1877df677
Persian Professionals <persianpros@yahoo.com>
2017-11-22 15:19:05 +0330

    ioprio.cpp: Better ARM/64-Bit support

--- lib/base/ioprio.cpp
+++ lib/base/ioprio.cpp
@@ -28,6 +28,12 @@ extern "C" int sys_ioprio_get(int, int);
 #elif defined(__mips__)
 #define __NR_ioprio_set		4284
 #define __NR_ioprio_get		4285
+#elif defined(__arm__) // the correct values for our kernel
+#define __NR_ioprio_set		314
+#define __NR_ioprio_get		315
+#elif defined(__aarch64__) // the correct values for our kernel
+#define __NR_ioprio_set		30
+#define __NR_ioprio_get		31
 #else
 #error "Unsupported arch"
 #endif
