0a0f51b087fc09c27fd94895a3c1e10afa7ddc12
Persian Professionals <persianpros@yahoo.com>
2017-11-28 15:14:54 +0330

    frontend.cpp: Si216 tuner SNR fix
    It won't bite if we support more hardwares but not box related stuff.

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1178,6 +1178,10 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 				break;
 		}
 	}
+	else if (!strncmp(m_description, "Si216", 5)) // New models with SI tuners
+	{
+		ret = snr;
+	}
 
 	signalqualitydb = ret;
 	if (ret == 0x12345678) // no snr db calculation avail.. return untouched snr value..
