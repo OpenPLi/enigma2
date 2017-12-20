578163365def11ec975ea4b1a7d286341782424e
Huevos <rowland@moralet.com>
2017-12-21 00:16:59 +0100

    scan.cpp: Namespace subnet by DVB type (#1179)

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -12,6 +12,8 @@ import time
 def InitUsageConfig():
 	config.usage = ConfigSubsection()
 	config.usage.subnetwork = ConfigYesNo(default = True)
+	config.usage.subnetwork_cable = ConfigYesNo(default = True)
+	config.usage.subnetwork_terrestrial = ConfigYesNo(default = True)
 	config.usage.showdish = ConfigYesNo(default = True)
 	config.misc.showrotorposition = ConfigSelection(default = "no", choices = [("no", _("no")), ("yes", _("yes")), ("withtext", _("with text")), ("tunername", _("with tuner name"))])
 	config.usage.multibouquet = ConfigYesNo(default = True)
