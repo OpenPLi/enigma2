35bcc1c67e26acf950b98beab0ad0373bc09e3c9
nautilus7 <spyros.n7@gmail.com>
2018-01-30 15:14:20 +0200

    Convert "TimerEntry" class buttons to new style

--- lib/python/Screens/TimerEntry.py
+++ lib/python/Screens/TimerEntry.py
@@ -28,12 +28,10 @@ class TimerEntry(Screen, ConfigListScreen):
 		self.entryDate = None
 		self.entryService = None
 
-		self["oktext"] = Label(_("OK"))
-		self["canceltext"] = Label(_("Cancel"))
-		self["ok"] = Pixmap()
-		self["cancel"] = Pixmap()
+		self["key_red"] = StaticText(_("Cancel"))
+		self["key_green"] = StaticText(_("Save"))
 		self["key_yellow"] = StaticText(_("Timer type"))
-		self["key_blue"] = StaticText()
+		self["key_blue"] = StaticText("")
 
 		self.createConfig()
 
@@ -46,6 +44,8 @@ class TimerEntry(Screen, ConfigListScreen):
 			"volumeDown": self.decrementStart,
 			"size+": self.incrementEnd,
 			"size-": self.decrementEnd,
+			"red": self.keyCancel,
+			"green": self.keyGo,
 			"yellow": self.changeTimerType,
 			"blue": self.changeZapWakeupType
 		}, -2)
@@ -523,7 +523,7 @@ class TimerLog(Screen):
 		self["logentry"] = Label()
 
 		self["key_red"] = StaticText(_("Delete entry"))
-		self["key_green"] = StaticText()
+		self["key_green"] = StaticText("")
 		self["key_yellow"] = StaticText("")
 		self["key_blue"] = StaticText(_("Clear log"))
 
