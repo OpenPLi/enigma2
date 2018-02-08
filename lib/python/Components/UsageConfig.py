aa8d0a6caf0b6fc7037c1ddf0ac2c42714e45ee2
Erik Slagter <erik@openpli.org>
2018-02-08 19:30:28 +0100

    Revert "Revert "Revert "Enabled/disabled fallback tuner for recordings seperately"""
    This reverts commit aca3ab41728a5aef94ecad33998672ef38ea9825.
    
    Let's bury this one for once and for all. It really cannot work, as
    described in the commit message.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -228,7 +228,6 @@ def InitUsageConfig():
 			configElement.value = "%s%s" % (not configElement.value.startswith("http://") and "http://" or "", configElement.value)
 			configElement.value = "%s%s" % (configElement.value, configElement.value.count(":") == 1 and ":8001" or "")
 	config.usage.remote_fallback_enabled = ConfigYesNo(default = False)
-	config.usage.remote_fallback_recording_enabled = ConfigYesNo(default = False)
 	config.usage.remote_fallback = ConfigText(default = "", fixed_size = False)
 	config.usage.remote_fallback.addNotifier(remote_fallback_changed, immediate_feedback=False)
 
