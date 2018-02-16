a999869dce106c7f892c1b18541618e58eeea7d4
nautilus7 <spyros.n7@gmail.com>
2018-02-16 21:54:17 +0200

    [satfinder] Remove obsolete "key_red" button

--- lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
+++ lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
@@ -7,7 +7,6 @@ from Plugins.Plugin import PluginDescriptor
 
 from Components.Sources.FrontendStatus import FrontendStatus
 from Components.ActionMap import ActionMap
-from Components.Label import Label
 from Components.NimManager import nimmanager, getConfigSatlist
 from Components.config import config, ConfigSelection, getConfigListEntry
 from Components.TuneTest import Tuner
@@ -15,6 +14,8 @@ from Tools.Transponder import getChannelNumber, channel2frequency
 from Tools.BoundFunction import boundFunction
 
 class Satfinder(ScanSetup, ServiceScan):
+	"""Inherits StaticText [key_red] and [key_green] properties from ScanSetup"""
+
 	def __init__(self, session):
 		self.initcomplete = False
 		service = session and session.nav.getCurrentService()
@@ -41,7 +42,6 @@ class Satfinder(ScanSetup, ServiceScan):
 		ScanSetup.__init__(self, session)
 		self.setTitle(_("Signal finder"))
 		self["Frontend"] = FrontendStatus(frontend_source = lambda : self.frontend, update_interval = 100)
-		self["key_red"] = Label(_("Exit"))
 
 		self["actions"] = ActionMap(["SetupActions", "ColorActions"],
 		{
