f284a99039150c3a486d458b0f81dd8b00c7509d
Athanasios Oikonomou <athoik@gmail.com>
2018-02-21 20:49:58 +0200

    TuneTest: remove wrong comma from tune parameters
    Remove wrong comma instead by mistake on commit 9b0a0fcafaf3e96172a05ba8cf992a5aa64d4abc.
    
    Adding a comma on the end makes the pls_code a tuple!
    >>> pls_code = 0,
    >>> pls_code
    (0,)
    >>> type(pls_code)
    <type 'tuple'>

--- lib/python/Components/TuneTest.py
+++ lib/python/Components/TuneTest.py
@@ -25,9 +25,9 @@ class Tuner:
 				parm.pls_mode = transponder[11]
 				parm.pls_code = transponder[12]
 			else:
-				parm.is_id = -1, # NO_STREAM_ID_FILTER
+				parm.is_id = -1 # NO_STREAM_ID_FILTER
 				parm.pls_mode = eDVBFrontendParametersSatellite.PLS_Gold
-				parm.pls_code = 0, # default gold PLS code
+				parm.pls_code = 0 # default gold PLS code
 			self.tuneSatObj(parm)
 
 	def tuneSatObj(self, transponderObj):
