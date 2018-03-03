104f9d90f015466a399a805e9257d1336822be5c
littlesat <littlesat99@yahoo.com>
2018-03-03 14:06:54 +0100

    Add ChannelsImport from fallback tuner
    Also move the Fallbacktuner configs from the Customize setup menu to the
    Service searching menu as this seems to be a better place
    
    For now you can customize when you want to do the ChannelsImport when
    you boot the box -or- when you restart the box or restart E2 -or- when
    the box is getting out of Standby. In addition you can choose if you
    want to see a notification when the ChannelsImport was performed or
    failed.
    
    2-DO: consider to add a way to manually initiate the ChannelsImport and
    other UI wishes. In addition at this moment it does download the
    channels via the OWIF and we cannot (yet) set a userid or password when
    the OWIF is password protected.

---
+++ lib/python/Components/ImportChannels.py
@@ -0,0 +1,108 @@
+import threading, urllib2, os, shutil
+from json import loads
+from enigma import eDVBDB, eEPGCache
+from Screens.MessageBox import MessageBox
+from Components.ServiceList import refreshServiceList
+from config import config
+from Tools import Notifications
+
+settingfiles = ('lamedb', 'bouquets.', 'userbouquet.', 'blacklist', 'whitelist', 'alternatives.')
+
+class ImportChannels():
+	
+	def __init__(self, callback=None, progress=None):
+		if config.usage.remote_fallback_enabled.value and config.usage.remote_fallback.value:
+			self.callback = callback
+			if "ChannelsImport" in [x.name for x in threading.enumerate()]:
+				self.ImportChannelsCallback(False, "ChannelsImport already running")
+			else:
+				self.callback = callback
+				self.progressCallback = progress
+				self.url = config.usage.remote_fallback.value.rsplit(":", 1)[0]
+				self.setProgress(0)
+				self.thread = threading.Thread(target=self.threaded_function, name="ChannelsImport")
+				self.thread.start()
+
+	def threaded_function(self):
+		try:
+			os.mkdir("/tmp/tmp")
+		except:
+			pass
+		self.setProgress(5)
+		print "Writing epg.dat file on sever box"
+		if config.usage.remote_fallback_epg.value:
+			try:
+				urllib2.urlopen("%s/web/saveepg" % self.url, timeout=5).read()
+			except:
+				self.ImportChannelsCallback(False, "Error when writing epg.dat on server")
+				return
+			self.setProgress(10)
+			print "[Import Channels] Get EPG Location"
+			try:
+				try:
+					files = [file for file in loads(urllib2.urlopen("%s/file?dir=/hdd" % self.url).read())["files"] if os.path.basename(file).startswith("epg.dat")]
+				except:
+					files = [file for file in loads(urllib2.urlopen("%s/file?dir=/" % self.url).read())["files"] if os.path.basename(file).startswith("epg.dat")] 
+				epg_location = files[0] if files else None
+			except:
+				self.ImportChannelsCallback(False, "Error while retreiving location of epg.dat on server")
+				return
+			self.setProgress(12)
+			if epg_location:
+				print "[Import Channels] Copy EPG file..."
+				try:
+					open("/hdd/epg.dat" if os.path.isdir("/hdd") else "/epg.dat", "wb").write(urllib2.urlopen("%s/file?file=%s" % (self.url, epg_location), timeout=5).read())
+				except:
+					self.ImportChannelsCallback(False, "Error while retreiving epg.dat from server")
+				self.setProgress(17)
+				print "[Import Channels] Loading EPG cache..."
+				eEPGCache.getInstance().load()
+			else:
+				self.ImportChannelsCallback(False, "No epg.dat fie found server")
+				return
+		self.setProgress(20)
+		print "[Import Channels] reading dir"
+		try:
+			files = [file for file in loads(urllib2.urlopen("%s/file?dir=/etc/enigma2" % self.url).read())["files"] if os.path.basename(file).startswith(settingfiles)]
+			count = 0
+			for file in files:
+				count += 1
+				file = file.encode("UTF-8")
+				print "[Import Channels] Downloading %s" % file, 
+				destination = "/tmp/tmp"
+				try:
+					open("%s/%s" % (destination, os.path.basename(file)), "wb").write(urllib2.urlopen("%s/file?file=%s" % (self.url, file), timeout=5).read())
+				except:
+					self.ImportChannelsCallback(False, "ERROR downloading file %s" % file)
+					return
+				self.setProgress(count * 70 / len(files) + 20)
+		except:
+			self.ImportChannelsCallback(False, "Error %s" % self.url)
+			return
+
+		print "[Import Channels] Removing files..."
+		files = [file for file in os.listdir("/etc/enigma2") if file.startswith(settingfiles)]
+		for file in files:
+			os.remove("/etc/enigma2/%s" % file)
+		print "[Import Channels] copying files..."
+		self.setProgress(95)
+		files = [x for x in os.listdir("/tmp/tmp") if x.startswith(settingfiles)]
+		for file in files:
+			shutil.move("/tmp/tmp/%s" % file, "/etc/enigma2/%s" % file)
+		os.rmdir("/tmp/tmp")
+		self.ImportChannelsCallback(True, "OK")
+
+	def ImportChannelsCallback(self, flag, errorstring):
+		if flag:
+			if config.usage.remote_fallback_ok.value:
+				Notifications.AddNotification(MessageBox, _("Channels from fallback tuner imported"), type=MessageBox.TYPE_INFO, timeout=5)
+			eDVBDB.getInstance().reloadBouquets()
+			eDVBDB.getInstance().reloadServicelist()
+		elif config.usage.remote_fallback_nok.value:
+			Notifications.AddNotification(MessageBox, _("Channels from fallback tuner failed %s" % errorstring), type=MessageBox.TYPE_ERROR, timeout=5)
+		if self.callback:
+			self.callback(flag, errorstring)
+
+	def setProgress(self, value):
+		if self.progressCallback:
+			self.progressCallback(value)
--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -230,6 +230,12 @@ def InitUsageConfig():
 	config.usage.remote_fallback_enabled = ConfigYesNo(default = False)
 	config.usage.remote_fallback = ConfigText(default = "", fixed_size = False)
 	config.usage.remote_fallback.addNotifier(remote_fallback_changed, immediate_feedback=False)
+	config.usage.remote_fallback_channelsimport = ConfigYesNo(default = False)
+	config.usage.remote_fallback_channelsimport_restart = ConfigYesNo(default = False)
+	config.usage.remote_fallback_channelsimport_standby = ConfigYesNo(default = False)
+	config.usage.remote_fallback_epg = ConfigYesNo(default = False)
+	config.usage.remote_fallback_ok = ConfigYesNo(default = False)
+	config.usage.remote_fallback_nok = ConfigYesNo(default = False)
 
 	config.usage.show_timer_conflict_warning = ConfigYesNo(default = True)
 
