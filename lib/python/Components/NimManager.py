d8cf6d55091fd1deefa76d287bdbda761295590d
Athanasios Oikonomou <athoik@gmail.com>
2018-03-08 22:30:09 +0200

    InitNimManager: when exception occurs print the error
    The reverted commit c9fbd6ec884ee8c9225b314dad87e16c436c06a3 was printing the exception message.
    Bring it back, since it can help find issues..

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -1518,8 +1518,8 @@ def InitNimManager(nimmgr, update_slots = []):
 					nimmgr.sec.update()
 			else:
 				print "[InitNimManager] tuner type is already already %d" %cur_type
-		except:
-			pass
+		except Exception as e:
+			print "[InitNimManager] tunerTypeChanged error: ", e
 
 	empty_slots = 0
 	for slot in nimmgr.nim_slots:
