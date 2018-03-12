ce30d7229ae469b6a881462ae1c77a961fbaf79b
Littlesat <littlesat99@yahoo.com>
2018-03-12 15:25:02 +0100

    ImportChannels: Always set the timemout to 5 sec during urlopen

--- lib/python/Components/ImportChannels.py
+++ lib/python/Components/ImportChannels.py
@@ -22,16 +22,16 @@ class ImportChannels():
 		if "epg" in config.usage.remote_fallback_import.value:
 			print "Writing epg.dat file on sever box"
 			try:
-				urllib2.urlopen("%s/web/saveepg" % self.url, timeout=5).read()
+				urllib2.urlopen("%s/web/saveepg" % self.urlconfig.usage.remote_fallback, timeout=5).read()
 			except:
 				self.ImportChannelsDone(False, _("Error when writing epg.dat on server"))
 				return
 			print "[Import Channels] Get EPG Location"
 			try:
 				try:
-					files = [file for file in loads(urllib2.urlopen("%s/file?dir=/hdd" % self.url).read())["files"] if os.path.basename(file).startswith("epg.dat")]
+					files = [file for file in loads(urllib2.urlopen("%s/file?dir=/hdd" % self.url, timeout=5).read())["files"] if os.path.basename(file).startswith("epg.dat")]
 				except:
-					files = [file for file in loads(urllib2.urlopen("%s/file?dir=/" % self.url).read())["files"] if os.path.basename(file).startswith("epg.dat")] 
+					files = [file for file in loads(urllib2.urlopen("%s/file?dir=/" % self.url, timeout=5).read())["files"] if os.path.basename(file).startswith("epg.dat")]
 				epg_location = files[0] if files else None
 			except:
 				self.ImportChannelsDone(False, _("Error while retreiving location of epg.dat on server"))
@@ -51,7 +51,7 @@ class ImportChannels():
 				pass
 			print "[Import Channels] reading dir"
 			try:
-				files = [file for file in loads(urllib2.urlopen("%s/file?dir=/etc/enigma2" % self.url).read())["files"] if os.path.basename(file).startswith(settingfiles)]
+				files = [file for file in loads(urllib2.urlopen("%s/file?dir=/etc/enigma2" % self.url, timeout=5).read())["files"] if os.path.basename(file).startswith(settingfiles)]
 				count = 0
 				for file in files:
 					count += 1
