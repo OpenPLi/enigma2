929adeab2a75afabc0765dbdcf33480d49a6ff54
IanSav <IanSav@users.noreply.github.com>
2018-04-07 02:08:28 +1000

    [skin.py] Add support for Setup.py menuimages
    Add decoding for "<setups>" tag block.  See /doc/SETUPGUIDE for further information.

--- skin.py
+++ skin.py
@@ -24,6 +24,7 @@ fonts = {
 }
 
 parameters = {}
+setups = {}
 
 def dump(x, i=0):
 	print " " * i + str(x)
@@ -589,6 +590,16 @@ def loadSingleSkinData(desktop, skin, path_prefix):
 				parameters[name] = "," in value and map(int, value.split(",")) or int(value)
 			except Exception, ex:
 				print "[Skin] Bad parameter", ex
+
+	for c in skin.findall("setups"):
+		for setup in c.findall("setup"):
+			get = setup.attrib.get
+			key = get("key")
+			image = get("image")
+			if key and image:
+				setups[key] = image
+			else:
+				raise SkinError("[Skin] Setup needs key and image, got '%s' '%s'" % (key, image))
 
 	for c in skin.findall("subtitles"):
 		from enigma import eSubtitleWidget
