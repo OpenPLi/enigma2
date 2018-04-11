99acd1756708958971dd8d668c65220843f97fa3
Taapat <taapat@gmail.com>
2018-04-11 18:53:40 +0300

    ConfigList: small code optimization for onDeselect
    Remove dublicate code and use function onDeselect in ConfigList

--- lib/python/Components/ConfigList.py
+++ lib/python/Components/ConfigList.py
@@ -65,8 +65,7 @@ class ConfigList(GUIComponent, object):
 	GUI_WIDGET = eListbox
 
 	def selectionChanged(self):
-		if isinstance(self.current,tuple) and len(self.current) >= 2:
-			self.current[1].onDeselect(self.session)
+		self.onDeselect()
 		self.current = self.getCurrent()
 		if isinstance(self.current,tuple) and len(self.current) >= 2:
 			self.current[1].onSelect(self.session)
@@ -89,8 +88,7 @@ class ConfigList(GUIComponent, object):
 		self.instance.setWrapAround(True)
 
 	def preWidgetRemove(self, instance):
-		if isinstance(self.current,tuple) and len(self.current) >= 2:
-			self.current[1].onDeselect(self.session)
+		self.onDeselect()
 		instance.selectionChanged.get().remove(self.selectionChanged)
 		instance.setContent(None)
 
