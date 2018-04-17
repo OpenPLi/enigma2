OpenPLi/enigma2

commit d863eed89ed781c6810180f6a05a1b244172a087

gpixmap: use a single threshold for is_a_candidate_for_accel()
Preparing support to configure a custom threshold in the future.

The threshold is measured in bytes, occupied by the surface

NOTE: this is probably how it was intended, having the same
threshold in bytes for 8 and 32 bpp pixmaps.
The original thresholds seem a bit odd, pixelsize 12000 for 8 bpp,
vs 48000 for 32 bpp.
When doing cpu fill or blit, the total time spent is more likely to be
proportional to the number of bytes to be copied or cleared than to
the number of pixels.
Also, accelerated operations on 32bpp surfaces are not likely to be
4 times faster than the same operations on 8bpp surfaces, for the
same number of pixels.
And finally, because the memory available for accelerated surfaces is
limited, it makes sense to use the same size limit for 8 bpp as for
32 bpp pixmaps.

So use a single threshold in bytes, for both 8 bpp and 32 bpp pixmaps.

lib/gdi/gpixmap.cpp
@@ -9,6 +9,11 @@
 #error "no BYTE_ORDER defined!"
 #endif
 
+/* surface acceleration threshold: do not attempt to accelerate surfaces smaller than the threshold (measured in bytes) */
+#ifndef GFX_SURFACE_ACCELERATION_THRESHOLD
+#define GFX_SURFACE_ACCELERATION_THRESHOLD 48000
+#endif
+
 // #define GPIXMAP_DEBUG
 
 #ifdef GPIXMAP_DEBUG
@@ -122,9 +127,8 @@ static bool is_a_candidate_for_accel(const gUnmanagedSurface* surface)
 	switch (surface->bpp)
 	{
 		case 8:
-			return (surface->y * surface->stride) > 12000;
 		case 32:
-			return (surface->y * surface->stride) > 48000;
+			return (surface->y * surface->stride * surface->bypp) >= GFX_SURFACE_ACCELERATION_THRESHOLD;
 		default:
 			return false;
 	}

commit 2847bf8dbafbfe1ad9fe089d663e0d67681d133e

gpixmap: fix GFX_SURFACE_ACCELERATION_THRESHOLD checking
do not multiply the surface area with bypp, stride is measured in bytes
(initialized as x * bypp)

this fixes d863eed89ed781c6810180f6a05a1b244172a087, which caused the
threshold to be 4 times lower than intended, for 32bpp surfaces.

lib/gdi/gpixmap.cpp
@@ -194,7 +194,7 @@ static bool is_a_candidate_for_accel(const gUnmanagedSurface* surface)
 	{
 		case 8:
 		case 32:
-			return (surface->y * surface->stride * surface->bypp) >= GFX_SURFACE_ACCELERATION_THRESHOLD;
+			return (surface->y * surface->stride) >= GFX_SURFACE_ACCELERATION_THRESHOLD;
 		default:
 			return false;
 	}