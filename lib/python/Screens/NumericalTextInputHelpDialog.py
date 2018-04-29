d3efed43963bf30dffd0e2d76ed295b050f194ed
ims <ims21@users.sourceforge.net>
2018-04-29 22:36:44 +0200

    NumericalTextInputHelpDialog - reduce code

--- lib/python/Screens/NumericalTextInputHelpDialog.py
+++ lib/python/Screens/NumericalTextInputHelpDialog.py
@@ -21,49 +21,39 @@ class NumericalTextInputHelpDialog(Screen):
 			self.last_marked = textinput.lastKey
 
 	def resizeFont(self):
-		item = "key5"
-		fnt, fname, fsize = self.itemFontPars(item)
-		key, text_width = self.findLongerText()
-		label_width = self[item].instance.size().width()
-		if label_width < text_width:
-			for i in range(fsize-1, 10, -1): # minimum 11 px
-				fnt = enigma.gFont(fname,i)
-				self.setTestLabel(key, fnt)
-				if self.testFilling(key, label_width):
-					self.setNewFontPars(fnt)
-					break
+		def findLongerText():
+			text_width=0
+			key_with_max=0
+			for x in (1, 2, 3, 4, 5, 6, 7, 8, 9, 0):
+				item = self["key%d" % x]
+				nowrap = item.instance.getNoWrap()
+				item.instance.setNoWrap(1)
+				width = item.instance.calculateSize().width()
+				item.instance.setNoWrap(nowrap)
+				if width > text_width:
+					text_width = width
+					key_with_max = x
+			return key_with_max, text_width
 
-	def findLongerText(self):
-		text_width=0
-		key_with_max=0
-		for x in (1, 2, 3, 4, 5, 6, 7, 8, 9, 0):
-			item = self["key%d" % x]
+		def testFilling(key, label_width):
+			item = self["key%d" % key]
 			nowrap = item.instance.getNoWrap()
 			item.instance.setNoWrap(1)
-			width = item.instance.calculateSize().width()
+			res = label_width >= item.instance.calculateSize().width()
 			item.instance.setNoWrap(nowrap)
-			if width > text_width:
-				text_width = width
-				key_with_max = x
-		return key_with_max, text_width
+			return res
 
-	def testFilling(self, x, label_width):
-		item = self["key%d" % x]
-		nowrap = item.instance.getNoWrap()
-		item.instance.setNoWrap(1)
-		res = label_width >= item.instance.calculateSize().width()
-		item.instance.setNoWrap(nowrap)
-		return res
-
-	def setTestLabel(self, x, fnt):
-		self["key%d" % x].instance.setFont(fnt)
-
-	def setNewFontPars(self, fnt):
-		for x in (1, 2, 3, 4, 5, 6, 7, 8, 9, 0):
-			self["key%d" % x].instance.setFont(fnt)
-		self["help1"].instance.setFont(fnt)
-		self["help2"].instance.setFont(fnt)
-
-	def itemFontPars(self, item):	# get item's font parameters from skin
+		item = "key0"
 		fnt = self[item].instance.getFont()
-		return fnt, fnt.family, fnt.pointSize
+		label_width = self[item].instance.size().width()
+		key, text_width = findLongerText()
+		if label_width < text_width:
+			for i in range(int(fnt.pointSize)-1, 10, -1): # minimum 11 px
+				fnt = enigma.gFont(fnt.family, i)
+				self["key%d" % key].instance.setFont(fnt)
+				if testFilling(key, label_width):
+					for x in (1, 2, 3, 4, 5, 6, 7, 8, 9, 0):
+						self["key%d" % x].instance.setFont(fnt)
+					self["help1"].instance.setFont(fnt)
+					self["help2"].instance.setFont(fnt)
+					break
