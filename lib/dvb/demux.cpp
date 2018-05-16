8441bcda6ad34a50abd844173a45a5fe5f6b2efe
Athanasios Oikonomou <athoik@gmail.com>
2018-05-16 20:59:32 +0200

    demux: lower eWarning to eDebug in eDVBPESReader ctor
    There is no need to warn every time we create eDVBPESReader about opening demux.
    If demux opening fails then we will get the real warning.

--- lib/dvb/demux.cpp
+++ lib/dvb/demux.cpp
@@ -347,7 +347,7 @@ void eDVBPESReader::data(int)
 
 eDVBPESReader::eDVBPESReader(eDVBDemux *demux, eMainloop *context, RESULT &res): m_demux(demux), m_active(0)
 {
-	eWarning("[eDVBPESReader] Created. Opening demux");
+	eDebug("[eDVBPESReader] Created. Opening demux");
 	m_fd = m_demux->openDemux();
 	if (m_fd >= 0)
 	{
