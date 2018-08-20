ab506a27ef824c8b3b75af8e7745cdda885b09e5
Dimitrij <Dima-73@inbox.lv>
2018-08-20 11:17:34 +0300

    [config] add new ConfigDate - using numer key only

--- lib/python/Components/config.py
+++ lib/python/Components/config.py
@@ -776,6 +776,11 @@ class ConfigClock(ConfigSequence):
 		# Trigger change
 		self.changed()
 
+date_limits = [(1, 31), (1, 12), (1970, 2050)]
+class ConfigDate(ConfigSequence):
+	def __init__(self, default):
+		d = localtime(default)
+		ConfigSequence.__init__(self, seperator=".", limits=date_limits, default=[d.tm_mday, d.tm_mon, d.tm_year])
 
 integer_limits = (0, 9999999999)
 class ConfigInteger(ConfigSequence):
