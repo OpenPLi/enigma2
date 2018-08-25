55241cde3627cb5919f1d9923e5b3bd073c1af49
Taapat <taapat@gmail.com>
2018-08-25 22:23:29 +0300

    VirtualKeyBoard: change function processClick to okClicked
    As before, to keep compatibility.
    I am using this feature in my plugin and maybe someone else uses it.
    Maybe the new name is better, but I do not think that it needs to be changed.

--- lib/python/Screens/VirtualKeyBoard.py
+++ lib/python/Screens/VirtualKeyBoard.py
@@ -325,7 +325,7 @@ class VirtualKeyBoard(Screen, HelpableScreen):
 			"save": (self.save, _("Save any text changes and exit")),
 			"locale": (self.localeMenu, _("Select the virtual keyboard locale from a menu")),
 			"shift": (self.shiftClicked, _("Select the virtual keyboard shifted character set")),
-			"ok": (self.processClick, _("Select the character or action under the virtual keyboard cursor")),
+			"ok": (self.okClicked, _("Select the character or action under the virtual keyboard cursor")),
 			"up": (self.up, _("Move the virtual keyboard cursor up")),
 			"left": (self.left, _("Move the virtual keyboard cursor left")),
 			"right": (self.right, _("Move the virtual keyboard cursor right")),
@@ -573,7 +573,7 @@ class VirtualKeyBoard(Screen, HelpableScreen):
 
 	def smsGotChar(self):
 		if self.smsChar and self.selectAsciiKey(self.smsChar):
-			self.processClick()
+			self.okClicked()
 
 	def setLocale(self):
 		self.language, self.location, self.keyList = self.locales.get(self.lang, [None, None, None])
@@ -634,7 +634,7 @@ class VirtualKeyBoard(Screen, HelpableScreen):
 		self.previousSelectedKey = self.selectedKey
 		self["list"].setList(self.list)
 
-	def processClick(self):
+	def okClicked(self):
 		self.smsChar = None
 		text = self.keyList[self.shiftLevel][self.selectedKey / self.keyboardWidth][self.selectedKey % self.keyboardWidth].encode("UTF-8")
 		if text == u"":
@@ -775,7 +775,7 @@ class VirtualKeyBoard(Screen, HelpableScreen):
 	def keyGotAscii(self):
 		self.smsChar = None
 		if self.selectAsciiKey(str(unichr(getPrevAsciiCode()).encode("utf-8"))):
-			self.processClick()
+			self.okClicked()
 
 	def selectAsciiKey(self, char):
 		if char == u" ":
