OpenPLi/enigma2

commit fbe82a1064859bda26ed800994d1ead75b2f2497

Make the skope to find pictures for skins wider
Pictures are searched in the own skin directory and also the
skin_default directory. In addition it seems some other images put e.g.
the vkey pngs in the buttons subdir. Now pngs are search in the given
directory first and then also in the root

lib/python/Tools/Directories.py
@@ -57,18 +57,16 @@ def resolveFilename(scope, base = "", path_prefix = None):
 		from Components.config import config
 		# allow files in the config directory to replace skin files
 		tmp = defaultPaths[SCOPE_CONFIG][0]
-		if base and pathExists(tmp + base):
+		if base and pathExists("%s%s" % (tmp, base)):
 			path = tmp
 		else:
-			tmp = defaultPaths[SCOPE_SKIN][0]
+			path = defaultPaths[SCOPE_SKIN][0]
 			pos = config.skin.primary_skin.value.rfind('/')
 			if pos != -1:
-				#if basefile is not available use default skin path as fallback
-				tmpfile = tmp+config.skin.primary_skin.value[:pos+1] + base
-				if pathExists(tmpfile):
-					path = tmp+config.skin.primary_skin.value[:pos+1]
-				else:
-					path = tmp
+				for dir in ("%s%s" % (path, config.skin.primary_skin.value[:pos+1]), "%s%s" % (path, "skin_default/")):
+					for file in (base, os.path.basename(base)):
+						if pathExists("%s%s"% (dir, file)):
+							return "%s%s" % (dir, file)
 			else:
 				path = tmp
 

commit 0df4dbc20040854c0ac1b24f71291174cee911bc

resolveFilename: do not check the alternatives if the original path exists
Broken in: https://github.com/OpenPLi/enigma2/commit/fbe82a1064859bda26ed800994d1ead75b2f2497

For example:
In the skin I specified the path pixmap="SimpleGrayHD/window/volume.png".
defaultPaths[SCOPE_SKIN][0] is "/usr/share/enigma2/"
base "SimpleGrayHD/window/volume.png"

Instead of using this path, they are looking for alternatives.
An alternative is found in skin_default and the image is used from skin_default, instead from skin.
Only if the alternatives are not found, path from skin is used.

This should fix this.

lib/python/Tools/Directories.py
@@ -61,6 +61,8 @@ def resolveFilename(scope, base = "", path_prefix = None):
 			path = tmp
 		else:
 			path = defaultPaths[SCOPE_SKIN][0]
+			if pathExists("%s%s" % (path, base)):
+				return "%s%s" % (path, base)
 			pos = config.skin.primary_skin.value.rfind('/')
 			if pos != -1:
 				for dir in ("%s%s" % (path, config.skin.primary_skin.value[:pos+1]), "%s%s" % (path, "skin_default/")):