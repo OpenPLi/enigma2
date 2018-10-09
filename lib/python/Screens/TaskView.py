56cc9373e14087a21c1f7a0d1f924ee3bea96903
nautilus7 <spyros.n7@gmail.com>
2018-10-09 22:57:06 +0300

    Put all imports at the top of the file

--- lib/python/Screens/TaskView.py
+++ lib/python/Screens/TaskView.py
@@ -1,18 +1,19 @@
-from Screen import Screen
-from Components.ConfigList import ConfigListScreen
+from Components.ActionMap import ActionMap
 from Components.config import config, ConfigSubsection, ConfigSelection, getConfigListEntry
+from Components.ConfigList import ConfigListScreen
+from Components.Sources.Boolean import Boolean
+from Components.Sources.Progress import Progress
+from Components.Sources.StaticText import StaticText
 from Components.SystemInfo import SystemInfo
 from Components.Task import job_manager
 from InfoBarGenerics import InfoBarNotifications
-import Screens.Standby
 from Tools import Notifications
+from Screen import Screen
+from Screens.MessageBox import MessageBox
+import Screens.Standby
 
 class JobView(InfoBarNotifications, Screen, ConfigListScreen):
 	def __init__(self, session, job, parent=None, cancelable = True, backgroundable = True, afterEventChangeable = True):
-		from Components.Sources.StaticText import StaticText
-		from Components.Sources.Progress import Progress
-		from Components.Sources.Boolean import Boolean
-		from Components.ActionMap import ActionMap
 		Screen.__init__(self, session, parent)
 		InfoBarNotifications.__init__(self)
 		ConfigListScreen.__init__(self, [])
@@ -128,7 +129,6 @@ class JobView(InfoBarNotifications, Screen, ConfigListScreen):
 			return
 		elif self.settings.afterEvent.getValue() == "close" and self.job.status == self.job.FINISHED:
 			self.close(False)
-		from Screens.MessageBox import MessageBox
 		if self.settings.afterEvent.getValue() == "deepstandby":
 			if not Screens.Standby.inTryQuitMainloop:
 				Notifications.AddNotificationWithCallback(self.sendTryQuitMainloopNotification, MessageBox, _("A sleep timer wants to shut down\nyour receiver. Shutdown now?"), timeout = 20)
