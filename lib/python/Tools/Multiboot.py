5d86fb27160dd1d2daa1834f622a74e6713134d1
Athanasios Oikonomou <athoik@gmail.com>
2018-11-25 13:31:07 +0200

    Multiboot: cosmetic use string format instead string join

--- lib/python/Tools/Multiboot.py
+++ lib/python/Tools/Multiboot.py
@@ -3,7 +3,7 @@ from Components.Console import Console
 import os
 
 def GetCurrentImage():
-	return SystemInfo["canMultiBoot"] and (int(open('/sys/firmware/devicetree/base/chosen/bootargs', 'r').read()[:-1].split(SystemInfo["canMultiBoot"][2]+'p')[1].split(' ')[0])-SystemInfo["canMultiBoot"][0])/2
+	return SystemInfo["canMultiBoot"] and (int(open('/sys/firmware/devicetree/base/chosen/bootargs', 'r').read()[:-1].split("%sp" % SystemInfo["canMultiBoot"][2])[1].split(' ')[0])-SystemInfo["canMultiBoot"][0])/2
 
 def GetCurrentImageMode():
 	return SystemInfo["canMultiBoot"] and SystemInfo["canMode12"] and int(open('/sys/firmware/devicetree/base/chosen/bootargs', 'r').read().replace('\0', '').split('=')[-1])
