7c3b4d334c70a42a1e56eb2e7a27ee3f00f1c601
littlesat <littlesat99@yahoo.com>
2018-11-27 20:27:00 +0100

    Mediaplayer: Remove a bit more duplicate code

--- lib/python/Plugins/Extensions/MediaPlayer/plugin.py
+++ lib/python/Plugins/Extensions/MediaPlayer/plugin.py
@@ -713,13 +713,13 @@ class MediaPlayer(Screen, InfoBarBase, InfoBarScreenSaver, InfoBarSeek, InfoBarA
 				listpath.append((i,playlistdir + i))
 		except IOError,e:
 			print "Error while scanning subdirs ",e
+		if config.mediaplayer.sortPlaylists.value:
+			listpath.sort()
 		return listpath
 
 	def load_playlist(self):
 		listpath = self.get_playlists()
 		if listpath:
-			if config.mediaplayer.sortPlaylists.value:
-				listpath.sort()
 			self.session.openWithCallback(self.PlaylistSelected, ChoiceBox, title=_("Please select a playlist..."), list = listpath)
 		else:
 			self.session.open(MessageBox, _("There are no saved playlists to delete!"), MessageBox.TYPE_ERROR)
@@ -739,8 +739,6 @@ class MediaPlayer(Screen, InfoBarBase, InfoBarScreenSaver, InfoBarSeek, InfoBarA
 	def delete_saved_playlist(self):
 		listpath = self.get_playlists()
 		if listpath:
-			if config.mediaplayer.sortPlaylists.value:
-				listpath.sort()
 			self.session.openWithCallback(self.DeletePlaylistSelected, ChoiceBox, title=_("Please select a playlist to delete..."), list = listpath)
 		else:
 			self.session.open(MessageBox, _("There are no saved playlists to delete!"), MessageBox.TYPE_ERROR)
