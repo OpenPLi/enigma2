0e3c402d1ca688848e6053e92ba6fe9a770d1e11
Persian Prince <persianpros@yahoo.com>
2018-12-09 10:44:22 +0330

    frontend.cpp: DVB-C 3128 FBC SNR fix
    It won't bite if we support more hardwares but not box related stuff.

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1113,6 +1113,10 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 	{
 		ret = (int)((((double(snr) / (65535.0 / 100.0)) * 0.1950) - 1.0000) * 100);
 	}
+	else if (!strcmp(m_description, "DVB-C NIM(3128 FBC)"))
+	{
+		ret = (int)(snr / 17);
+	}
 	else if (!strcmp(m_description, "Genpix"))
 	{
 		ret = (int)((snr << 1) / 5);
