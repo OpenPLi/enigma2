46650aaab03cdc91db03452499cddc57c8436766
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2018-12-17 15:19:03 +0200

    - change addSkin by default to search in SCOPE_CURRENT_SKIN

--- skin.py
+++ skin.py
@@ -42,7 +42,7 @@ class SkinError(Exception):
 
 dom_skins = [ ]
 
-def addSkin(name, scope = SCOPE_SKIN):
+def addSkin(name, scope = SCOPE_CURRENT_SKIN):
 	# read the skin
 	filename = resolveFilename(scope, name)
 	if fileExists(filename):
@@ -99,12 +99,8 @@ addSkin('skin_box.xml')
 addSkin('skin_second_infobar.xml')
 
 display_skin_id = 1
-displaySkinXml = 'skin_display.xml';
-displaySkinFilename = resolveFilename(SCOPE_CURRENT_SKIN, displaySkinXml)
-if fileExists(displaySkinFilename):
-  addSkin(displaySkinXml, SCOPE_CURRENT_SKIN)
-else:
-  addSkin(displaySkinXml)
+
+addSkin(displaySkinXml)
 
 addSkin('skin_text.xml')
 
