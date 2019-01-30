e42faa0f731fe3e75fc8863a6efc236edeb22920
v8droide2 <v8droide2>
2019-01-30 23:56:05 +0100

    Add t2-mi plp_id scan option for e2 Tuners which can do multistream should been able to t2-mi but the stream should been decap first (like done with the plugin astra-sm) As there is no info provide by the demod or tuner if it has the capability to handle t2-mi the option will been only available when the tuner can do multistream
    todo: t2-mi cluster stream should been decap first to get the pmt info
    todo: add in transponder list the plp_id for t2-mi transponders
    
    (based on the original patch for Openatv)
    Credits go to:
    From: zhtq 36952536+zhtq@users.noreply.github.com
    Date: Mon, 28 Jan 2019 11:13:01 +0100
    Subject: [PATCH] add t2mi support)

--- lib/dvb/frontendparms.h
+++ lib/dvb/frontendparms.h
@@ -63,9 +63,13 @@ public:
 		PLS_Default_Gold_Code, PLS_Default_Root_Code
 	};
 
+	enum {
+		No_T2MI_PLP_Id = eDVBFrontendParametersSatellite::No_Stream_Id_Filter
+	};
+
 	bool no_rotor_command_on_tune;
 	unsigned int frequency, symbol_rate;
-	int polarisation, fec, inversion, orbital_position, system, modulation, rolloff, pilot, is_id, pls_mode, pls_code;
+	int polarisation, fec, inversion, orbital_position, system, modulation, rolloff, pilot, is_id, pls_mode, pls_code, t2mi_plp_id;
 };
 SWIG_ALLOW_OUTPUT_SIMPLE(eDVBFrontendParametersSatellite);
 
@@ -223,6 +227,7 @@ public:
 	int getIsId() const;
 	int getPLSMode() const;
 	int getPLSCode() const;
+	int getT2MIPlpId() const;
 	int getBandwidth() const;
 	int getCodeRateLp() const;
 	int getCodeRateHp() const;
@@ -257,6 +262,7 @@ public:
 	int getIsId() const;
 	int getPLSMode() const;
 	int getPLSCode() const;
+	int getT2MIPlpId() const;
 };
 
 class eDVBCableTransponderData : public eDVBTransponderData
