c37735ce13e7b61f8fa6740778cce399053baae7
Dimitrij <Dima-73@inbox.lv>
2019-02-06 21:38:17 +0200

    [Subservices] fix play service in selection list(press OK)
    -groupedservices add  more Sky Bundesliga/ Sky Sport services
    -fix cur_num is 0

--- lib/python/Screens/SubservicesQuickzap.py
+++ lib/python/Screens/SubservicesQuickzap.py
@@ -52,7 +52,7 @@ class SubservicesQuickzap(InfoBarBase, InfoBarShowHide, InfoBarMenu, \
 
 	def onLayoutFinished(self):
 		cur_num = self.currentlyPlayingSubservice
-		if cur_num:
+		if cur_num is not None:
 			self.currentSubserviceNumberLabel.setText(str(cur_num + 1))
 
 	def nextSubservice(self):
@@ -80,7 +80,7 @@ class SubservicesQuickzap(InfoBarBase, InfoBarShowHide, InfoBarMenu, \
 
 	def subserviceSelected(self, service):
 		if service:
-			self.playSubservice(service[1])
+			self.playSubservice([x[1] for x in self.subservices].index(service[1]))
 
 	def keyOK(self):
 		self.doShow()
