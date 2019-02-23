44b3c48fbf18351791f72ad8f446b63ed84f398e
Betacentauri <betacentauri@arcor.de>
2019-02-23 17:19:15 +0100

    Make genmetaindex.py compatible to python2 and python3

--- tools/genmetaindex.py
+++ tools/genmetaindex.py
@@ -4,6 +4,7 @@ import os
 from xml.etree.ElementTree import ElementTree, Element
 
 root = Element("index")
+encoding = ("unicode" if sys.version_info[0] >= 3 else "utf-8")
 
 for file in sys.argv[1:]:
 	p = ElementTree()
@@ -50,4 +51,4 @@ def indent(elem, level=0):
 
 indent(root)
 
-ElementTree(root).write(sys.stdout)
+ElementTree(root).write(sys.stdout, encoding=encoding)
