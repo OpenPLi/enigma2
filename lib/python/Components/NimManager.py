66dcbb2adc1ea6240f8f3d70744f43db80d25d0a
littlesat <littlesat99@yahoo.com>
2019-03-05 09:43:14 +0100

    NimManager: Avoid over detection of hotswitchable tuners

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -517,7 +517,7 @@ class NIM(object):
 				types.remove("DVB-S")
 			if len(types) > 1:
 				self.multi_type = {}
-				self.hotswitchable = True
+				self.hotswitchable = self.description.upper().startswith("AVL")
 				for type in types:
 					self.multi_type[str(types.index(type))] = type
 
