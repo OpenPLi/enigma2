bd27a568176e84532e20d8bd8e08cffdccefab0a
littlesat <littlesat99@yahoo.com>
2019-03-25 08:40:09 +0100

    NimManager: Add comment to the 'conversion' code
    So later we rememeber why we add this

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -1623,6 +1623,8 @@ def InitNimManager(nimmgr, update_slots = []):
 				nim.configMode.value = "nothing"
 
 		if slot.isCombined():
+
+			#Convert during an upgrade that multiType is 'converted' to combined config when needed
 			if nim.configMode.value and not slot.getTunerTypesEnabled():
 				nim.multiType = ConfigText(default = "")
 				if nim.multiType.value:
