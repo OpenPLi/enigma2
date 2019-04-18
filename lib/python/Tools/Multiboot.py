087805ef8aa77894ae6d94069b86f0a066081f1c
HD-Digital <hd.stb.digital@gmail.com>
2019-04-18 19:27:22 +0200

    multiboot: only check slot 1 for the subdir linuxrootfs1 when linuxrootfs was stored in userdata all slots wwith the info of linuxrootfs1

--- lib/python/Tools/Multiboot.py
+++ lib/python/Tools/Multiboot.py
@@ -38,7 +38,7 @@ class GetImagelist():
 	def appClosed(self, data, retval, extra_args):
 		if retval == 0 and self.phase == self.MOUNT:
 			if SystemInfo["HasRootSubdir"]:
-				if os.path.isfile("/tmp/testmount/linuxrootfs1/usr/bin/enigma2"):
+				if self.slot == 1 and os.path.isfile("/tmp/testmount/linuxrootfs1/usr/bin/enigma2"):
 					import datetime
 					self.imagelist[self.slot] =  { 'imagename': "%s (%s)" % (open("/tmp/testmount/linuxrootfs1/etc/issue").readlines()[-2].capitalize().strip()[:-6], datetime.datetime.fromtimestamp(os.stat("/tmp/testmount/linuxrootfs1/usr/share/bootlogo.mvi").st_mtime).strftime('%Y-%m-%d'))}
 				elif os.path.isfile("/tmp/testmount/linuxrootfs%s/usr/bin/enigma2" % self.slot):
