8e2624f5b441c90514b5d8f6ba7b440e4bdfc8f7
Littlesat <reijng1@medtronic.com>
2019-08-26 14:21:30 +0200

    NimManager: Solve mistake in previous commit

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -901,7 +901,7 @@ class NimManager:
 				if type.startswith("DVB-S") and n.config_mode in ("loopthrough", "satposdepends"):
 					root_id = nimmanager.sec.getRoot(n.slot_id, int(n.config.connectedTo.value))
 					if n.type = nimmanager.nim_slots[root_id].type: # check if connected from a DVB-S to DVB-S2 Nim or vice versa
-						return True
+						return False
 				return True	
 		return [x.slot for x in self.nim_slots if x.slot != exception and enabled(x)]
 
