6fc43c3cc997720d109305ed594b3f01e3ea1d0f
Littlesat <reijng1@medtronic.com>
2019-08-27 14:22:57 +0200

    Also show green subservices button when only one subservice in set
    As during watching active subservices might be added to the set

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -176,7 +176,7 @@ def getActiveSubservicesForCurrentChannel(service):
 
 def hasActiveSubservicesForCurrentChannel(service):
 	activeSubservices = getActiveSubservicesForCurrentChannel(service)
-	return bool(activeSubservices and len(activeSubservices) >= 2)
+	return bool(activeSubservices)
 
 class InfoBarDish:
 	def __init__(self):
