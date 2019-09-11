caeefee7ca9989af4b1819a4a2d99a89aed51117
Littlesat <reijng1@medtronic.com>
2019-09-11 10:02:34 +0200

    ECMInfo: Use text instead of color for indicating active ecm pids
    Thanks Nautulus7

--- lib/python/Screens/ServiceInfo.py
+++ lib/python/Screens/ServiceInfo.py
@@ -386,8 +386,8 @@ class ServiceInfo(Screen):
 						extra_info = "extra data=%s" % caid[2]
 				from Tools.GetEcmInfo import GetEcmInfo
 				ecmdata = GetEcmInfo().getEcmData()
-				color = "\c00??;?00" if caid[0] == int(ecmdata[1], 16) and (caid[1] == int(ecmdata[3], 16) or str(int(ecmdata[2], 16)) in provid) else ""
-				tlist.append(ServiceInfoListEntry("%sECMPid %04X (%d) %04X-%s %s" % (color, caid[1], caid[1], caid[0], CaIdDescription, extra_info)))
+				formatstring = "ECMPid %04X (%d) %04X-%s %s (used)" if caid[0] == int(ecmdata[1], 16) and (caid[1] == int(ecmdata[3], 16) or str(int(ecmdata[2], 16)) in provid) else "ECMPid %04X (%d) %04X-%s %s"
+				tlist.append(ServiceInfoListEntry(formatstring % (caid[1], caid[1], caid[0], CaIdDescription, extra_info)))
 			if not tlist:
 				tlist.append(ServiceInfoListEntry(_("No ECMPids available (FTA Service)")))
 			self["infolist"].l.setList(tlist)
