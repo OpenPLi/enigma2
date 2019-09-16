f79b588fb14e3e90b746b752a6c082d777ac6389
Littlesat <reijng1@medtronic.com>
2019-09-16 09:09:26 +0200

    Revert "Harddisk.py: Replace missing os.system with Console().ePopen"
    This reverts commit 6587ab18bf9b400d01ecb3dd7ef267213bc3f80c.

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -239,7 +239,7 @@ class Harddisk:
 			return 0
 		cmd = 'umount ' + dev
 		print "[Harddisk]", cmd
-		res = Console().ePopen(cmd)
+		res = os.system(cmd)
 		return (res >> 8)
 
 	def createPartition(self):
