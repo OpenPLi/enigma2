aeb14f47d9b0721a24a9664acb113abeca140067
jack2015 <jack2015@users.noreply.github.com>
2019-10-08 14:03:36 +0800

    Update Lcd.py

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -75,10 +75,10 @@ def InitLcd():
 			config.lcd.contrast.addNotifier(setLCDcontrast);
 		else:
 			config.lcd.contrast = ConfigNothing()
-+			if getBoxType() in ('dm900','dm920'):
-+				standby_default = 4
-+			else:
-+				standby_default = 1
+			if getBoxType() in ('dm900','dm920'):
+				standby_default = 4
+			else:
+				standby_default = 1
 
 		config.lcd.standby = ConfigSlider(default=standby_default, limits=(0, 10))
 		config.lcd.standby.addNotifier(setLCDbright);
