57a3188e9f075f16e6940e9d3fd2ffd0e95816ea
IanSav <IanSav@users.noreply.github.com>
2020-02-17 12:20:59 +1100

    [SystemInfo.py] Restore previous cmdline processing
    This change reverts the change to reading the cmdline.  An open().read() has an implied close() so that an explicit close is not required.

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -12,9 +12,7 @@ from Tools.Multiboot import getMultibootStartupDevice, getMultibootslots  # This
 
 # Parse the boot commandline.
 #
-fd = open("/proc/cmdline", "r")
-cmdline = fd.read()
-fd.close()
+cmdline = open("/proc/cmdline", "r").read()
 cmdline = {k: v.strip('"') for k, v in re.findall(r'(\S+)=(".*?"|\S+)', cmdline)}
 
 def getNumVideoDecoders():
