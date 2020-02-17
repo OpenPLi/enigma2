a8d017c3e8047daa3d0831ae6fb072779b65f2e4
jack2015 <jack2015@users.noreply.github.com>
2020-02-17 19:35:53 +0800

    Update VNetSpeedInfo.py

--- lib/python/Components/Converter/VNetSpeedInfo.py
+++ lib/python/Components/Converter/VNetSpeedInfo.py
@@ -1,56 +1,36 @@
-# -*- coding: utf-8 -*-
-
-#This plugin is free software, you are allowed to
-#modify it (if you keep the license),
-#but you are not allowed to distribute/publish
-#it without source code (this version and your modifications).
-#This means you also have to distribute
-#source code of your modifications.
-#
-#
-#######################################################################
-#
-# NetSpeedInfo for VU+
-# Coded by markusw (c) 2013
-# www.vuplus-support.org
-#
-####################################################################### 
-
-import os
 from Components.Label import Label
 from Components.Converter.Converter import Converter
 from Components.Element import cached
 from Poll import Poll
-from enigma import iServiceInformation, iPlayableService
 
 class VNetSpeedInfo(Poll, Converter, object):
-	RCL = 0				# Receive Lan in Megabit/s = Geschwindigkeit/Bandbreite
-	TML = 1				# Transmit Lan in Megabit/s = Geschwindigkeit/Bandbreite
-	RCW = 2				# Receive Wlan in Megabit/s = Geschwindigkeit/Bandbreite
-	TMW = 3				# Transmit Wlan in Megabit/s = Geschwindigkeit/Bandbreite
-	RCLT = 4			# Receive Lan-total seit dem letzten Neustart in Megabyte
-	TMLT = 5			# Transmit Lan-total seit dem letzten Neustart in Megabyte
-	RCWT = 6			# Receive Wlan-total seit dem letzten Neustart in Megabyte
-	TMWT = 7			# Transmit Wlan-total seit dem letzten Neustart in Megabyte
-	RCL_MB = 8			# Receive Lan in Megabyte/s = Geschwindigkeit/Bandbreite
-	TML_MB = 9			# Transmit Lan in Megabyte/s = Geschwindigkeit/Bandbreite
-	RCW_MB = 10			# Receive Wlan in Megabyte/s = Geschwindigkeit/Bandbreite
-	TMW_MB = 11			# Transmit Wlan in Megabyte/s = Geschwindigkeit/Bandbreite
-	RC = 12				# Receive Lan oder Wlan in Megabit/s = Geschwindigkeit/Bandbreite - wenn beides vorhanden wird Lan ausgegeben
-	TM = 13				# Transmit Lan oder Wlan in Megabit/s = Geschwindigkeit/Bandbreite - wenn beides vorhanden wird Lan ausgegeben
-	RCT = 14			# Receive Lan oder Wlan total seit dem letzten Neustart in Megabyte - wenn beides vorhanden wird Lan ausgegeben
-	TMT = 15			# Transmit Lan oder Wlan total seit dem letzten Neustart in Megabyte - wenn beides vorhanden wird Lan ausgegeben
-	RC_MB = 16			# Receive Lan oder Wlan in Megabyte/s = Geschwindigkeit/Bandbreite - wenn beides vorhanden wird Lan ausgegeben
-	TM_MB = 17			# Transmit Lan oder Wlan in Megabyte/s = Geschwindigkeit/Bandbreite - wenn beides vorhanden wird Lan ausgegeben
-	NET_TYP = 18			# Lan - Wlan - Lan+Wlan
-	ERR_RCL = 19			# Fehler Lan-Receive
-	ERR_TML = 20			# Fehler Lan-Transmit
-	DRO_RCL = 21			# Drop Lan-Receive
-	DRO_TML = 22			# Drop Lan-Transmit
-	ERR_RCW = 23			# Fehler WLan-Receive
-	ERR_TMW = 24			# Fehler WLan-Transmit
-	DRO_RCW = 25			# Drop WLan-Receive
-	DRO_TMW = 26			# Drop WLan-Transmit
+	RCL = 0
+	TML = 1
+	RCW = 2
+	TMW = 3
+	RCLT = 4
+	TMLT = 5
+	RCWT = 6
+	TMWT = 7
+	RCL_MB = 8
+	TML_MB = 9
+	RCW_MB = 10
+	TMW_MB = 11
+	RC = 12
+	TM = 13
+	RCT = 14
+	TMT = 15
+	RC_MB = 16
+	TM_MB = 17
+	NET_TYP = 18
+	ERR_RCL = 19
+	ERR_TML = 20
+	DRO_RCL = 21
+	DRO_TML = 22
+	ERR_RCW = 23
+	ERR_TMW = 24
+	DRO_RCW = 25
+	DRO_TMW = 26
 
 	def __init__(self, type,update_interval = 1500):
 		Poll.__init__(self)
@@ -147,6 +127,12 @@ class VNetSpeedInfo(Poll, Converter, object):
 
 	@cached
 	def getText(self):
+		service = self.source.service
+		info = service and service.info()
+		if not info:
+			return ""
+		if service.streamed() is None:
+			return ""
 		textvalue = ""
 		textvalue = self.updateNetSpeedInfoStatus()
 		return textvalue
@@ -154,11 +140,6 @@ class VNetSpeedInfo(Poll, Converter, object):
 	text = property(getText)
 
 	def updateNetSpeedInfoStatus(self):
-		service = self.source.service
-		info = service and service.info()
-		if not info or service.streamed() is None:
-			return ""
-
 		flaglan=0
 		flagwlan=0
 		bwm=open("/proc/net/dev")
