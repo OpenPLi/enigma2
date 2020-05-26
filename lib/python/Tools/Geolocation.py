2c6b0e069a08396913c008cfa923d63d1526d87a
Harro Verton <wanwizard@openpli.org>
2020-05-26 12:42:00 +0100

    only retrieve geolocation fields that are actually required

--- lib/python/Tools/Geolocation.py
+++ lib/python/Tools/Geolocation.py
@@ -5,39 +5,39 @@ from Components.config import ConfigYesNo, config
 
 # Data available from http://ip-api.com/json/:
 #
-# 	Name		Description				Example			Type
-# 	--------------	--------------------------------------	----------------------	------
-# 	status		success or fail				success			string
+# 	Name		Description				Example			Type	Collected
+# 	--------------	--------------------------------------	----------------------	------	---------
+# 	status		success or fail				success			string	Yes
 # 	message		Included only when status is fail. Can
 # 			be one of the following: private range,
-# 			reserved range, invalid query		invalid query		string
-# 	continent	Continent name				North America		string
-# 	continentCode	Two-letter continent code		NA			string
-# 	country		Country name				United States		string
+# 			reserved range, invalid query		invalid query		string	Yes
+# 	continent	Continent name				North America		string	No
+# 	continentCode	Two-letter continent code		NA			string	No
+# 	country		Country name				United States		string	No
 # 	countryCode	Two-letter country code
-# 			ISO 3166-1 alpha-2			US			string
-# 	region		Region/state short code (FIPS or ISO)	CA or 10		string
-# 	regionName	Region/state				California		string
-# 	city		City					Mountain View		string
-# 	district	District (subdivision of city)		Old Farm District	string
-# 	zip		Zip code				94043			string
-# 	lat		Latitude				37.4192			float
-# 	lon		Longitude				-122.0574		float
-# 	timezone	City timezone				America/Los_Angeles	string
-# 	currency	National currency			USD			string
-# 	isp		ISP name				Google			string
-# 	org		Organization name			Google			string
+# 			ISO 3166-1 alpha-2			US			string	No
+# 	region		Region/state short code (FIPS or ISO)	CA or 10		string	No
+# 	regionName	Region/state				California		string	No
+# 	city		City					Mountain View		string	No
+# 	district	District (subdivision of city)		Old Farm District	string	No
+# 	zip		Zip code				94043			string	No
+# 	lat		Latitude				37.4192			float	No
+# 	lon		Longitude				-122.0574		float	No
+# 	timezone	City timezone				America/Los_Angeles	string	Yes
+# 	currency	National currency			USD			string	No
+# 	isp		ISP name				Google			string	No
+# 	org		Organization name			Google			string	No
 # 	as		AS number and organization, separated
 # 			by space (RIR). Empty for IP blocks 
-# 			not being announced in BGP tables.	AS15169 Google Inc.	string
+# 			not being announced in BGP tables.	AS15169 Google Inc.	string	No
 # 	asname		AS name (RIR). Empty for IP blocks not
-# 			being announced in BGP tables.		GOOGLE			string
+# 			being announced in BGP tables.		GOOGLE			string	No
 # 	reverse		Reverse DNS of the IP
-# 			(Not fetched as it delays response)	wi-in-f94.1e100.net	string
-# 	mobile		Mobile (cellular) connection		true			bool
-# 	proxy		Proxy, VPN or Tor exit address		true			bool
-# 	hosting		Hosting, colocated or data center	true			bool
-# 	query		IP used for the query			173.194.67.94		string
+# 			(Not fetched as it delays response)	wi-in-f94.1e100.net	string	No
+# 	mobile		Mobile (cellular) connection		true			bool	No
+# 	proxy		Proxy, VPN or Tor exit address		true			bool	Yes
+# 	hosting		Hosting, colocated or data center	true			bool	No
+# 	query		IP used for the query			173.194.67.94		string	No
 
 config.misc.enableGeolocation = ConfigYesNo(default=True)
 geolocation = {}
@@ -47,7 +47,7 @@ def InitGeolocation():
 	if config.misc.enableGeolocation.value:
 		if len(geolocation) == 0:
 			try:
-				response = urlopen("http://ip-api.com/json/?fields=33288191", data=None, timeout=10).read()
+				response = urlopen("http://ip-api.com/json/?fields=status,message,timezone,proxy", data=None, timeout=10).read()
 				# print "[Geolocation] DEBUG:", response
 				if response:
 					geolocation = loads(response)
