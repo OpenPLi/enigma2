674285acfa9358b4561ca6dd154b39f600dfc2d1
Dimitrij <Dima-73@inbox.lv>
2020-08-17 19:24:07 +0300

    Add option "Auto play last service in radio mode"
    -Only option "Hide channel list in radio mode" set disable.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -60,6 +60,7 @@ def InitUsageConfig():
 
 	config.usage.quickzap_bouquet_change = ConfigYesNo(default = False)
 	config.usage.e1like_radio_mode = ConfigYesNo(default = True)
+	config.usage.e1like_radio_mode_last_play = ConfigYesNo(default = True)
 	choicelist = [("0", _("No timeout"))]
 	for i in range(1, 12):
 		choicelist.append((str(i), ngettext("%d second", "%d seconds", i) % i))
