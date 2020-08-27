8ad75a21ab8f9a813b0e038037ffebbee1c6e20d
littlesat <littlesat99@yahoo.com>
2020-08-27 19:24:35 +0200

    Add support for auto resizable embeded skin templates
    When the embeded skin included in python code is a list then this means
    that this is an autoreziable skin template that can adjust to the
    resulition of the skin. The first element in the list is the skin as xml
    code where variable parameters are entered as %d and the following
    elements are the values for an 1280x720 sized skin. During parsing the
    embeded skin these values will be auto adjusted to the correct skin
    resolution.
    
    For an example see the next merge request (example in SkinSelecktor.py)
    
    This avoids to call auto size code on multiple places as it now can be
    done centralized.
    
    I also choose not to use SkinTemplate and scaleData as two extra
    variables for this as it can be done in one standard skin parameter due
    to using the the check on list.

--- skin.py
+++ skin.py
@@ -994,7 +994,11 @@ def readSkin(screen, skin, names, desktop):
 	if myScreen is None:  # Otherwise try embedded skin.
 		myScreen = getattr(screen, "parsedSkin", None)
 	if myScreen is None and getattr(screen, "skin", None):  # Try uncompiled embedded skin.
-		skin = screen.skin
+		if isinstance(screen.skin, list):
+			print("[Skin] Resizable embedded skin %s template found!" % name)
+			skin = screen.skin[0] % tuple([x * getDesktop(0).size().height() / 720 for x in screen.skin[1:]])
+		else:
+			skin = screen.skin
 		print("[Skin] Parsing embedded skin '%s'." % name)
 		if isinstance(skin, tuple):
 			for s in skin:
