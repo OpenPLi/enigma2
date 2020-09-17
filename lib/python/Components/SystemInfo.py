4ab84c51fb070315bfdc449253c202fffc197514
Dimitrij <Dima-73@inbox.lv>
2020-09-17 18:34:41 +0300

    [SystemInfo/NimManager] add option ignore delsys info

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -1,8 +1,5 @@
 import re
-
 from enigma import Misc_Options, eDVBCIInterfaces, eDVBResourceManager, eGetEnigmaDebugLvl
-
-from Components.Console import Console
 from Tools.Directories import SCOPE_PLUGINS, fileCheck, fileExists, fileHas, pathExists, resolveFilename
 from Tools.HardwareInfo import HardwareInfo
 
@@ -130,3 +127,4 @@ SystemInfo["CanDownmixAAC"] = fileHas("/proc/stb/audio/aac_choices", "downmix")
 SystemInfo["HDMIAudioSource"] = fileCheck("/proc/stb/hdmi/audio_source")
 SystemInfo["BootDevice"] = getBootdevice()
 SystemInfo["FbcTunerPowerAlwaysOn"] = HardwareInfo().get_device_model() in ("vusolo4k", "vuduo4k", "vuultimo4k", "vuuno4k", "vuuno4kse", "gbquad4k", "gbue4k")
+SystemInfo["TunerIgnoreDelsysInfo"] = ["Sundtek",]
