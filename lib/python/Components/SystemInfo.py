34123fe4c70291ce964ae9921bc0303c6dd7d1ac
littlesat <littlesat99@yahoo.com>
2020-09-19 12:25:22 +0200

    Revert "[SystemInfo/NimManager] add option ignore delsys info"
    Very sorry Dima73... we need to think about a real solution. Maybe push
    this to SundTek.
    
    This reverts commit b5d6302e474047463121160460f6fd7812f4a05e.

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -1,5 +1,8 @@
 import re
+
 from enigma import Misc_Options, eDVBCIInterfaces, eDVBResourceManager, eGetEnigmaDebugLvl
+
+from Components.Console import Console
 from Tools.Directories import SCOPE_PLUGINS, fileCheck, fileExists, fileHas, pathExists, resolveFilename
 from Tools.HardwareInfo import HardwareInfo
 
@@ -127,4 +130,3 @@ SystemInfo["CanDownmixAAC"] = fileHas("/proc/stb/audio/aac_choices", "downmix")
 SystemInfo["HDMIAudioSource"] = fileCheck("/proc/stb/hdmi/audio_source")
 SystemInfo["BootDevice"] = getBootdevice()
 SystemInfo["FbcTunerPowerAlwaysOn"] = HardwareInfo().get_device_model() in ("vusolo4k", "vuduo4k", "vuultimo4k", "vuuno4k", "vuuno4kse", "gbquad4k", "gbue4k")
-SystemInfo["TunerIgnoreDelsysInfo"] = ["Sundtek",]
