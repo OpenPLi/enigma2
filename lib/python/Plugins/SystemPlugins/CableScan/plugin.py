754d43b40487a4003b500eb68af278c401d77436
technl <tech@openpli.org>
2020-11-14 10:18:29 +0000

    Set the default scanning variables to the values that are used in the Dutch cable network

--- lib/python/Plugins/SystemPlugins/CableScan/plugin.py
+++ lib/python/Plugins/SystemPlugins/CableScan/plugin.py
@@ -100,8 +100,8 @@ class CableScanStatus(Screen):
 config.plugins.CableScan = ConfigSubsection()
 config.plugins.CableScan.keepnumbering = ConfigYesNo(default = False)
 config.plugins.CableScan.hdlist = ConfigYesNo(default = False)
-config.plugins.CableScan.frequency = ConfigFloat(default = [164, 0], limits = [(50, 999),(0, 999)])
-config.plugins.CableScan.symbolrate = ConfigInteger(default = 6900, limits = (1, 9999))
+config.plugins.CableScan.frequency = ConfigFloat(default = [474, 0], limits = [(50, 999),(0, 999)])
+config.plugins.CableScan.symbolrate = ConfigInteger(default = 6875, limits = (1, 9999))
 config.plugins.CableScan.networkid = ConfigInteger(default = 5555, limits = (0, 99999))
 config.plugins.CableScan.modulation = ConfigSelection(
 	choices =
