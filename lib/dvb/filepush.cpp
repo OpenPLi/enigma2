d13d20cb738bf8fe0a83e1b847b3b38a631dfdd4
Erik Slagter <erik@openpli.org>
2020-12-13 19:49:15 +0100

    filepush: fix (first?) bug in filepush patch.
    Use thread specific signal blocking api.

--- lib/dvb/filepush.cpp
+++ lib/dvb/filepush.cpp
@@ -354,7 +354,7 @@ void eFilePushThreadRecorder::thread()
 
 	sigemptyset(&sigset);
 	sigaddset(&sigset, SIGUSR1);
-	sigprocmask(SIG_BLOCK, &sigset, (sigset_t *)0);
+	pthread_sigmask(SIG_BLOCK, &sigset, (sigset_t *)0);
 
 	hasStarted();
 
