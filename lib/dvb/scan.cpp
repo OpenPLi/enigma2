e6d70834d96ae4d72db57d5b387acea6aaf7df6c
Athanasios Oikonomou <athoik@gmail.com>
2020-12-25 00:30:10 +0200

    gcc: silence parentheses warnings
    This commit silences the following gcc parenteses warnings:
    lib/base/estring.cpp:447:38: warning: suggest parentheses around '&&' within '||' [-Wparentheses]
    lib/base/estring.cpp:449:35: warning: suggest parentheses around '&&' within '||' [-Wparentheses]
    lib/base/estring.cpp:828:8: warning: suggest parentheses around operand of '!' or change '&' to '&&' or '!' to '~' [-Wparentheses]
    lib/base/estring.cpp:829:9: warning: suggest parentheses around operand of '!' or change '&' to '&&' or '!' to '~' [-Wparentheses]
    lib/dvb/scan.cpp:57:40: warning: suggest parentheses around '&&' within '||' [-Wparentheses]

--- lib/dvb/scan.cpp
+++ lib/dvb/scan.cpp
@@ -54,7 +54,7 @@ eDVBScan::~eDVBScan()
 
 int eDVBScan::isValidONIDTSID(int orbital_position, eOriginalNetworkID onid, eTransportStreamID tsid)
 {
-	if(onid.get() == 0 || onid.get() == 1 && tsid < 2 || onid.get() >= 0xFF00)
+	if(onid.get() == 0 || (onid.get() == 1 && tsid < 2) || onid.get() >= 0xFF00)
 	{
 		return 0;
 	}
