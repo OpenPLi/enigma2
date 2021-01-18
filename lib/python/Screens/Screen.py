b77f3f5be165f8c3a636f29bd86ce380ead490b5
littlesat <littlesat99@yahoo.com>
2021-01-18 19:38:00 +0100

    Do not show InfoBar on menu path
    But I still have a wierd thing to solve. I have on my VU+Ultimo 4K now
    that InfoBar is shown, but on my H9Combo it is not shown and now I miss
    MainMenu and I have at this moment no idea why it is not shown. Maybe we
    should consider to only show it from MainMenu so there is a kind of 2do
    here....

--- lib/python/Screens/Screen.py
+++ lib/python/Screens/Screen.py
@@ -146,8 +146,8 @@ class Screen(dict):
 
 	def setTitle(self, title, showPath=True):
 		try:  # This protects against calls to setTitle() before being fully initialised like self.session is accessed *before* being defined.
-			if self.session and len(self.session.dialog_stack) > 1:
-				self.screenPath = " > ".join(ds[0].getTitle() for ds in self.session.dialog_stack[1:])
+			if self.session and len(self.session.dialog_stack) > 2:
+				self.screenPath = " > ".join(ds[0].getTitle() for ds in self.session.dialog_stack[2:])
 			else:
 				self.screenPath = ""
 			if self.instance:
