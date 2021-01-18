d4c3160ba4169e6b71f55f2e97f257fbbf3b1676
littlesat <littlesat99@yahoo.com>
2021-01-18 19:16:24 +0100

    StartWizard: Streamline some code
    Save the config is not needed when the user selects do NOT understand
    the develop image intentions

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -89,8 +89,9 @@ class DevelopWizard(MessageBox):
 		MessageBox.__init__(self, session, _("This image is intended for developers and testers.\nNo support will be provided!\nDo you understand this?"), type=MessageBox.TYPE_YESNO, simple=True)
 
 	def close(self, value):
-		config.misc.check_developimage.value = not value
-		config.misc.check_developimage.save()
+		if value:
+			config.misc.check_developimage.value = False
+			config.misc.check_developimage.save()
 		MessageBox.close(self)
 
 class AutoInstallWizard(Screen):
