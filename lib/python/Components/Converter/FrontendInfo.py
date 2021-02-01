6a31785f51884dda6dd06d3901af45d0d1a0585e
Littlesat <reijng1@medtronic.com>
2021-02-01 11:46:04 +0100

    Revert "Do not show SNR on OSD when no 'watching' tuner is active"
    This reverts commit efb881e08c2a475357b42847d9ee2e889aff9ace.
    
    As it does not work as the source slot_number is not accesable there

--- lib/python/Components/Converter/FrontendInfo.py
+++ lib/python/Components/Converter/FrontendInfo.py
@@ -56,18 +56,12 @@ class FrontendInfo(Converter):
 		elif self.type == self.AGC:
 			percent = self.source.agc
 		elif (self.type == self.SNR and not swapsnr) or (self.type == self.SNRdB and swapsnr):
-			if self.source.slot_number:
-				percent = self.source.snr
-			else:
-				return _("N/A")
+			percent = self.source.snr
 		elif self.type == self.SNR or self.type == self.SNRdB:
-			if self.source.slot_number:
-				if self.source.snr_db is not None:
-					return _("%3.01f dB") % (self.source.snr_db / 100.0)
-				elif self.source.snr is not None: #fallback to normal SNR...
-					percent = self.source.snr
-			else:
-				return ""
+			if self.source.snr_db is not None:
+				return _("%3.01f dB") % (self.source.snr_db / 100.0)
+			elif self.source.snr is not None: #fallback to normal SNR...
+				percent = self.source.snr
 		elif self.type == self.TUNER_TYPE:
 			return self.source.frontend_type or _("Unknown")
 		elif self.type == self.STRING:
