ae6b8a6afa564d94b7878116eeeaa9c24cd334ab
Taapat <taapat@gmail.com>
2021-03-14 17:38:37 +0200

    Add experimental svg picom support
    Only for testers.
    It is known that colors may not be displayed correctly and may be scaling issues.

--- lib/service/listboxservice.cpp
+++ lib/service/listboxservice.cpp
@@ -1,3 +1,4 @@
+#include <lib/base/wrappers.h>
 #include <lib/service/listboxservice.h>
 #include <lib/service/service.h>
 #include <lib/gdi/font.h>
@@ -894,7 +895,12 @@ void eListboxServiceContent::paint(gPainter &painter, eWindowStyle &style, const
 							{
 								std::string piconFilename = PyString_AS_STRING(pRet);
 								if (!piconFilename.empty())
-									loadPNG(piconPixmap, piconFilename.c_str());
+								{
+									if (endsWith(piconFilename, ".svg"))
+										loadSVG(piconPixmap, piconFilename.c_str());
+									else
+										loadPNG(piconPixmap, piconFilename.c_str());
+								}
 							}
 							Py_DECREF(pRet);
 						}
