49e2635e8d29702fe693f10fab8b4c9683a4df7a
Taapat <taapat@gmail.com>
2021-04-05 18:24:26 +0300

    LoadPixmap: calculate the svg scaling factor on each call.
    On enigma init LoadPixmap is used when the main skin resolution has not been set and therefore the wrong SKIN_FACTOR is set.
    Thanks Huevos for pointing this.

--- lib/python/Tools/LoadPixmap.py
+++ lib/python/Tools/LoadPixmap.py
@@ -1,13 +1,6 @@
 from enigma import loadPNG, loadJPG, loadSVG, getDesktop
 
 
-# Return a scaling factor (float) that can be used to rescale images
-# to suit the current resolution of the skin.
-# The scales are based on a default screen resolution of HD (720p).
-# That is the scale factor for a HD screen will be 1.
-SKIN_FACTOR = getDesktop(0).size().height() / 720.0
-
-
 # If cached is not supplied, LoadPixmap defaults to caching PNGs and not caching JPGs
 # Split alpha channel JPGs are never cached as the C++ layer's caching is based on
 # a single file per image in the cache
@@ -19,7 +12,8 @@ def LoadPixmap(path, desktop=None, cached=None, width=0, height=0):
 		# don't cache unless caller explicity requests caching
 		ptr = loadJPG(path, 1 if cached is True else 0)
 	elif path[-4:] == ".svg":
-		ptr = loadSVG(path, 0 if cached is False else 1, width, height, SKIN_FACTOR)
+		scale = getDesktop(0).size().height() / 720.0 if height == 0 else 0
+		ptr = loadSVG(path, 0 if cached is False else 1, width, height, scale)
 	elif path[-1:] == ".":
 		# caching mechanism isn't suitable for multi file images, so it's explicitly disabled
 		alpha = loadPNG(path + "a.png", 0, 0)
