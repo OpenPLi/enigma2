2ed0af16881f369c8c109f98620d58da031b83f2
WanWizard <wanwizard@wanwizard.eu>
2021-04-17 22:19:48 +0100

    fixed import from fallback when using utf-8 encoded filenames

--- lib/python/Components/ImportChannels.py
+++ lib/python/Components/ImportChannels.py
@@ -5,6 +5,7 @@ from Screens.MessageBox import MessageBox
 from config import config, ConfigText
 from Tools import Notifications
 from base64 import encodestring
+from urllib import quote
 import xml.etree.ElementTree as et
 
 settingfiles = ('lamedb', 'bouquets.', 'userbouquet.', 'blacklist', 'whitelist', 'alternatives.')
@@ -95,8 +96,9 @@ class ImportChannels():
 					file = file.encode("UTF-8")
 					print "[Import Channels] Downloading %s" % file
 					try:
-						open(os.path.join(self.tmp_dir, os.path.basename(file)), "wb").write(self.getUrl("%s/file?file=%s" % (self.url, file)).read())
-					except:
+						open(os.path.join(self.tmp_dir, os.path.basename(file)), "wb").write(self.getUrl("%s/file?file=%s" % (self.url, quote(file))).read())
+					except Exception, e:
+						print "[Import Channels] Exception: %s" % str(e)
 						self.ImportChannelsDone(False, _("ERROR downloading file %s") % file)
 						return
 			except:
