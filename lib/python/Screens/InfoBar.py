9ec36dc1a3dd9ea7aafffb6d320ec014ff0ac88f
Harro Verton <wanwizard@wanwizard.eu>
2021-07-26 18:02:13 +0100

    Revert "- fix wrong widget used"
    This reverts commit 841b6225e3fe2e6f2ff83a9cc7816a6ad69d2f28.

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -222,7 +222,7 @@ class MoviePlayer(InfoBarBase, InfoBarShowHide, InfoBarMenu, InfoBarSeek, InfoBa
 	def __playMVStateChanged(self, state):
 		self.currentSeekState = state
 		statetext = state[3]
-		self["statetexticon"].setText(statetext)
+		self["statetext"].setText(statetext)
 		if state[1] > 1:
 			self["statetext"].setText("x%d" % state[1])
 		elif state[1] < 0:
