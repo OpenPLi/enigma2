ddd37777d95daa6cd5801053460613d05d522fd3
Harro Verton <wanwizard@wanwizard.eu>
2021-07-26 18:02:13 +0100

    Revert "- removed PVRState completely - get rid from unneded code - renamed properly the widgets"
    This reverts commit 6296bae09de4e22abf315811eb2e9ed18fccd43f.

---
+++ lib/python/Screens/PVRState.py
@@ -0,0 +1,13 @@
+from Screen import Screen
+
+from Components.Label import Label
+
+
+class PVRState(Screen):
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		self["state"] = Label(text="")
+
+
+class TimeshiftState(PVRState):
+	pass
