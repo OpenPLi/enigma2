c4dd0a73ce444786f644ae9d2f7138a11b22b912
ims <ims21@users.sourceforge.net>
2021-08-22 11:26:25 +0200

    MovieInfo - added Name and FullDescription pars

--- lib/python/Components/Converter/MovieInfo.py
+++ lib/python/Components/Converter/MovieInfo.py
@@ -9,6 +9,8 @@ class MovieInfo(Converter):
 	MOVIE_META_DESCRIPTION = 1 # just meta description when available
 	MOVIE_REC_SERVICE_NAME = 2 # name of recording service
 	MOVIE_REC_FILESIZE = 3 # filesize of recording
+	MOVIE_FULL_DESCRIPTION = 4 # short and exended description
+	MOVIE_NAME = 5 # recording name
 
 	def __init__(self, type):
 		if type == "ShortDescription":
@@ -19,6 +21,10 @@ class MovieInfo(Converter):
 			self.type = self.MOVIE_REC_SERVICE_NAME
 		elif type == "FileSize":
 			self.type = self.MOVIE_REC_FILESIZE
+		elif type == "FullDescription":
+			self.type = self.MOVIE_FULL_DESCRIPTION
+		elif type == "Name":
+			self.type = self.MOVIE_NAME
 		else:
 			raise ElementError("'%s' is not <ShortDescription|MetaDescription|RecordServiceName|FileSize> for MovieInfo converter" % type)
 		Converter.__init__(self, type)
@@ -36,10 +42,26 @@ class MovieInfo(Converter):
 				return (info.getInfoString(service, iServiceInformation.sDescription)
 					or (event and event.getShortDescription())
 					or service.getPath())
+			elif self.type == self.MOVIE_FULL_DESCRIPTION:
+				if (service.flags & eServiceReference.flagDirectory) == eServiceReference.flagDirectory:
+					return ""
+				description = (event and event.getShortDescription()
+						or info.getInfoString(service, iServiceInformation.sDescription))
+				extended = event and event.getExtendedDescription()
+				if description and extended:
+					if description.replace('\n', '') == extended.replace('\n', ''):
+						return extended
+					description += "\n"
+				return description + (extended if extended else "")
 			elif self.type == self.MOVIE_META_DESCRIPTION:
 				return ((event and (event.getExtendedDescription() or event.getShortDescription()))
 					or info.getInfoString(service, iServiceInformation.sDescription)
 					or service.getPath())
+			elif self.type == self.MOVIE_NAME:
+				if (service.flags & eServiceReference.flagDirectory) == eServiceReference.flagDirectory:
+					# Name for directory is the full path
+					return service.getPath()
+				return event and event.getEventName() or info and info.getName(service)
 			elif self.type == self.MOVIE_REC_SERVICE_NAME:
 				rec_ref_str = info.getInfoString(service, iServiceInformation.sServiceref)
 				return ServiceReference(rec_ref_str).getServiceName()
