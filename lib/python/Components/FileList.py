OpenPLi/enigma2

commit 57c67cd6e7c2c2d5fcc250609bb6663f5806bc62

[FileList] fix tuple index out of range (#3062)
File "/usr/lib/enigma2/python/Plugins/Extensions/AutoBackup/ui.py", line
415, in changeSelectionState
  File
"/usr/lib/enigma2/python/Components/FileList.py", line 352, in
changeSelectionState
    if not x[0][3].startswith('<'):
IndexError:
tuple index out of range

lib/python/Components/FileList.py
@@ -345,30 +345,31 @@ class MultiFileSelectList(FileList):
 			f()
 
 	def changeSelectionState(self):
-		idx = self.l.getCurrentSelectionIndex()
-		newList = self.list[:]
-		x = self.list[idx]
-		if not x[0][3].startswith('<'):
-			if x[0][1] is True:
-				realPathname = x[0][0]
-			else:
-				realPathname = self.current_directory + x[0][0]
-			if x[0][2]:
-				SelectState = False
-				try:
-					self.selectedFiles.remove(realPathname)
-				except:
+		if len(self.list):
+			idx = self.l.getCurrentSelectionIndex()
+			newList = self.list[:]
+			x = self.list[idx]
+			if x and len(x[0]) > 2 and not x[0][3].startswith('<'):
+				if x[0][1] is True:
+					realPathname = x[0][0]
+				else:
+					realPathname = self.current_directory + x[0][0]
+				if x[0][2]:
+					SelectState = False
 					try:
-						self.selectedFiles.remove(os.path.normpath(realPathname))
+						self.selectedFiles.remove(realPathname)
 					except:
-						print "Couldn't remove:", realPathname
-			else:
-				SelectState = True
-				if (realPathname not in self.selectedFiles) and (os.path.normpath(realPathname) not in self.selectedFiles):
-					self.selectedFiles.append(realPathname)
-			newList[idx] = MultiFileSelectEntryComponent(name=x[0][3], absolute=x[0][0], isDir=x[0][1], selected=SelectState)
-		self.list = newList
-		self.l.setList(self.list)
+						try:
+							self.selectedFiles.remove(os.path.normpath(realPathname))
+						except (IOError, OSError) as err:
+							print "[FileList] Error: Can't remove '%s'!  (%s)" % (err.errno, realPathname, err.strerror)
+				else:
+					SelectState = True
+					if (realPathname not in self.selectedFiles) and (os.path.normpath(realPathname) not in self.selectedFiles):
+						self.selectedFiles.append(realPathname)
+				newList[idx] = MultiFileSelectEntryComponent(name=x[0][3], absolute=x[0][0], isDir=x[0][1], selected=SelectState)
+			self.list = newList
+			self.l.setList(self.list)
 
 	def getSelectedList(self):
 		return self.selectedFiles

commit e4b13f3f2eea7b6461fa9738caf6923f8894f5fc

[FileList] Fixed bug in print command
Introduced in https://github.com/OpenPLi/enigma2/commit/57c67cd6e7c2c2d5fcc250609bb6663f5806bc62 and pointed by @Huevos

lib/python/Components/FileList.py
@@ -362,7 +362,7 @@ class MultiFileSelectList(FileList):
 						try:
 							self.selectedFiles.remove(os.path.normpath(realPathname))
 						except (IOError, OSError) as err:
-							print "[FileList] Error: Can't remove '%s'!  (%s)" % (err.errno, realPathname, err.strerror)
+							print "[FileList] Error %d: Can't remove '%s'!  (%s)" % (err.errno, realPathname, err.strerror)
 				else:
 					SelectState = True
 					if (realPathname not in self.selectedFiles) and (os.path.normpath(realPathname) not in self.selectedFiles):