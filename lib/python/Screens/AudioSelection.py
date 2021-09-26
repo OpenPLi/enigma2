6cab04202d6484b4a10f37e6492cab08bddb5e30
Betacentauri <betacentauri@arcor.de>
2021-09-26 11:01:21 +0200

    Use python3 like repr
    Used:
    futurize -w -n -f repr *.py
    
    Change is compatible with Python2

--- lib/python/Screens/AudioSelection.py
+++ lib/python/Screens/AudioSelection.py
@@ -224,7 +224,7 @@ class AudioSelection(Screen, ConfigListScreen):
 
 			conflist.append(getConfigListEntry(_("To audio selection"), self.settings.menupage))
 
-			if self.infobar.selected_subtitle and self.infobar.selected_subtitle != (0, 0, 0, 0) and not ".DVDPlayer'>" in `self.infobar`:
+			if self.infobar.selected_subtitle and self.infobar.selected_subtitle != (0, 0, 0, 0) and not ".DVDPlayer'>" in repr(self.infobar):
 				self["key_blue"].setBoolean(True)
 				conflist.append(getConfigListEntry(_("Subtitle Quickmenu"), ConfigNothing()))
 
