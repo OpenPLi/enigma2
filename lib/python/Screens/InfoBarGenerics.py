0914c0910da9a51657e3b1d1c8af4e5cce50352c
Dimitrij <Dima-73@inbox.lv>
2021-12-05 10:42:06 +0200

    use “%s” % structure instead of “ “ + style

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -183,9 +183,8 @@ def getActiveSubservicesForCurrentChannel(service):
 				if title and "Sendepause" not in title:
 					starttime = datetime.datetime.fromtimestamp(event[0]).strftime('%H:%M')
 					endtime = datetime.datetime.fromtimestamp(event[0] + event[1]).strftime('%H:%M')
-					servicename = ServiceReference(subservice).getServiceName()
 					schedule = str(starttime) + "-" + str(endtime)
-					activeSubservices.append((servicename + " " + "[ " + schedule + " ]  - " + " " + title, subservice))
+					activeSubservices.append(("%s [%s] %s" % (ServiceReference(subservice).getServiceName(), schedule, title), subservice))
 	if not activeSubservices:
 		subservices = service and service.subServices()
 		if subservices:
