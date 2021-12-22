d970396aa285a80e73f4a982bc0351c25b390d2f
Hains <hainsvdbosch@ziggo.nl>
2021-12-22 14:38:51 +0100

    UsageConfig.py: Cast map() to list
    map() returns iterator instead of list with Python 3.
    
    File "/usr/lib/enigma2/python/Components/config.py", line 337, in setChoices
        self.choices = choicesList(choices)
      File "/usr/lib/enigma2/python/Components/config.py", line 193, in __init__
        assert False, "choices must be dict or list!"
    AssertionError: choices must be dict or list!

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -831,7 +831,7 @@ def updateChoices(sel, choices):
 				if x < val:
 					defval = str(x)
 					break
-		sel.setChoices(map(str, choices), defval)
+		sel.setChoices(list(map(str, choices)), defval)
 
 
 def preferredPath(path):
