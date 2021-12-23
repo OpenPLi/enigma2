eb368df9e6dd1e66e7b0ac6980525002c85f39d5
Hains <hainsvdbosch@ziggo.nl>
2021-12-23 20:37:23 +0100

    Fix MenuList imports

--- lib/python/Components/PluginList.py
+++ lib/python/Components/PluginList.py
@@ -1,4 +1,4 @@
-from MenuList import MenuList
+from Components.MenuList import MenuList
 
 from Tools.Directories import resolveFilename, SCOPE_CURRENT_SKIN
 from Components.MultiContent import MultiContentEntryText, MultiContentEntryPixmapAlphaBlend
