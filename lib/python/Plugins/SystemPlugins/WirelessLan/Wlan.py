0d6cf76a477842d8e83e9c30511e31426c9ba490
Hains <hainsvdbosch@ziggo.nl>
2021-12-25 16:37:48 +0100

    WirelessLan: Remove strip import as well

--- lib/python/Plugins/SystemPlugins/WirelessLan/Wlan.py
+++ lib/python/Plugins/SystemPlugins/WirelessLan/Wlan.py
@@ -6,7 +6,6 @@ from Components.Network import iNetwork
 import enigma
 
 import os
-from string import strip
 from pythonwifi.iwlibs import Wireless, getWNICnames
 from pythonwifi import flags as wififlags
 
