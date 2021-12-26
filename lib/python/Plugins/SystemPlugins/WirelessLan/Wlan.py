37096a5412e0bd1539bb5bad2763ead9ae7a558e
Hains <hainsvdbosch@ziggo.nl>
2021-12-26 09:39:40 +0100

    Revert "WirelessLan: Update to new python wifi module"
    https://github.com/OpenPLi/openpli-oe-core/commit/df08e754b93b874737bae1eaac723643cc69f8a9
    
    This reverts commit db5a6c19bb741a8643714f5a71e35855fb6dc44d.

--- lib/python/Plugins/SystemPlugins/WirelessLan/Wlan.py
+++ lib/python/Plugins/SystemPlugins/WirelessLan/Wlan.py
@@ -6,8 +6,8 @@ from Components.Network import iNetwork
 import enigma
 
 import os
-
-from wifi.scan import Cell
+from pythonwifi.iwlibs import Wireless, getWNICnames
+from pythonwifi import flags as wififlags
 
 list = []
 list.append("Unencrypted")
