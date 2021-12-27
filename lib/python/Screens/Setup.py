fa8017d55ed6deb42cc180ed3c4bedc9b73a8c1f
Hains <hainsvdbosch@ziggo.nl>
2021-12-27 18:28:38 +0100

    Setup.py: Don't encode to byte strings
    To fix <not-a-string> and 'b' prefixes in menu items.

--- lib/python/Screens/Setup.py
+++ lib/python/Screens/Setup.py
@@ -125,8 +125,8 @@ class Setup(ConfigListScreen, Screen):
 				if conditional and not eval(conditional):
 					continue
 
-				item_text = _(x.get("text", "??").encode("UTF-8"))
-				item_description = _(x.get("description", " ").encode("UTF-8")) # don't change
+				item_text = _(x.get("text", "??"))
+				item_description = _(x.get("description", " ")) # don't change
 				b = eval(x.text or "")
 				if b == "":
 					continue
@@ -169,5 +169,5 @@ def getSetupTitle(id):
 	xmldata = setupdom.getroot()
 	for x in xmldata.findall("setup"):
 		if x.get("key") == id:
-			return x.get("title", "").encode("UTF-8")
+			return x.get("title", "")
 	raise SetupError("unknown setup id '%s'!" % repr(id))
