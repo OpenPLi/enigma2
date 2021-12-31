e14c19b0fbd23dabfb554b4ae2ed7518ab4a9d83
Hains <hainsvdbosch@ziggo.nl>
2021-12-31 19:02:47 +0100

    Fix Poll module import

--- lib/python/Components/Converter/VfdDisplay.py
+++ lib/python/Components/Converter/VfdDisplay.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from Poll import Poll
+from Components.Converter.Poll import Poll
 from Components.Converter.Converter import Converter
 from Components.Element import cached
 
