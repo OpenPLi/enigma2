77b62e8c9fad3e2ca57ff60bfda509ad82f05e32
Hains <hainsvdbosch@ziggo.nl>
2022-01-01 11:25:40 +0100

    Satconfig.py: Fix up/down key tuner configuration

--- lib/python/Screens/Satconfig.py
+++ lib/python/Screens/Satconfig.py
@@ -612,6 +612,8 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 		self["actions"] = ActionMap(["SetupActions", "SatlistShortcutAction"],
 		{
 			"ok": self.keyOk,
+			"keyup": self.keyUp,
+			"keydown": self.keyDown,
 			"save": self.keySave,
 			"cancel": self.keyCancel,
 			"changetype": self.changeConfigurationMode,
@@ -755,6 +757,11 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 			return self.countrycodes[cc.upper()]
 		return cc
 
+	def keyUp(self):
+		self["config"].instance.moveSelection(self["config"].instance.moveUp)
+
+	def keyDown(self):
+		self["config"].instance.moveSelection(self["config"].instance.moveDown)
 
 class NimSelection(Screen):
 	def __init__(self, session):
