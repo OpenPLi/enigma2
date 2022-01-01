1a7f725cf1b82b6cb6e2f8cf8b5b5a94d3cecec2
technl <tech@openpli.org>
2022-01-01 21:24:03 +0000

    Changed Hanssettings commitlog to the correct repo

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -220,7 +220,7 @@ class CommitInfo(Screen):
 			("https://api.github.com/repos/openpli/enigma2-plugin-skins-magic/commits", "Skin Magic SD"),
 			("https://api.github.com/repos/littlesat/skin-PLiHD/commits", "Skin PLi HD"),
 			("https://api.github.com/repos/E2OpenPlugins/e2openplugin-OpenWebif/commits", "OpenWebif"),
-			("https://api.github.com/repos/haroo/HansSettings/commits", "Hans settings")
+			("https://api.github.com/repos/technl/HansSettings/commits", "Hans settings")
 		]
 		self.cachedProjects = {}
 		self.Timer = eTimer()
