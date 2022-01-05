993edcc78c2201a8cd44e79404f10feb6c1c4e6d
Hains <hainsvdbosch@ziggo.nl>
2022-01-05 08:00:51 +0100

    Downloader.py: Fix import urlparse

--- lib/python/Tools/Downloader.py
+++ lib/python/Tools/Downloader.py
@@ -1,6 +1,6 @@
 from twisted.web import client
 from twisted.internet import reactor, defer
-from urlparse import urlparse
+from urllib.parse import urlparse
 
 
 class HTTPProgressDownloader(client.HTTPDownloader):
