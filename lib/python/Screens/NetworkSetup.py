b9c8533df352fb8ea0b6ea4f2d91ea93fbb3f8b1
Hains <hainsvdbosch@ziggo.nl>
2022-01-05 09:27:48 +0100

    NetworkSetup.py: Decode 'line' to unicode

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -950,7 +950,7 @@ class AdapterSetupConfiguration(Screen, HelpableScreen):
 	def dataAvail(self, data):
 		self.LinkState = None
 		for line in data.splitlines():
-			line = line.strip()
+			line = line.strip().decode()
 			if 'Link detected:' in line:
 				if "yes" in line:
 					self.LinkState = True
