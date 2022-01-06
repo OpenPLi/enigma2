f7daf28aae1ec558bc0c34b0da24be7a8cc36da0
Hains <hainsvdbosch@ziggo.nl>
2022-01-06 21:51:11 +0100

    Satfinder: Use floor division
    File "/usr/lib/enigma2/python/Plugins/SystemPlugins/Satfinder/plugin.py", line 480, in retuneSat
        self.tuner.tune(transponder)
      File "/usr/lib/enigma2/python/Components/TuneTest.py", line 15, in tune
        parm.frequency = transponder[0] * 1000
    TypeError: in method 'eDVBFrontendParametersSatellite_frequency_set', argument 2 of type 'unsigned int'

--- lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
+++ lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
@@ -474,7 +474,7 @@ class Satfinder(ScanSetup, ServiceScan):
 			tps = nimmanager.getTransponders(satpos, int(self.satfinder_scan_nims.value))
 			if len(tps) > self.preDefTransponders.index:
 				tp = tps[self.preDefTransponders.index]
-				transponder = (tp[1] / 1000, tp[2] / 1000,
+				transponder = (tp[1] // 1000, tp[2] // 1000,
 					tp[3], tp[4], 2, satpos, tp[5], tp[6], tp[8], tp[9], tp[10], tp[11], tp[12], tp[13], tp[14])
 				if self.initcomplete:
 					self.tuner.tune(transponder)
