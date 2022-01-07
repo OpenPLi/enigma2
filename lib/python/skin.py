OpenPLi/enigma2

commit 10d2b65dfb7ec74d69e2a5a5562744f278fa0aff

skin.py: Cast result map() to list
map() returns an object of map class, which leads to error like:

File "/usr/lib/enigma2/python/Screens/Wizard.py", line 186, in __init__
    self["config"] = ConfigList([], session=session)
  File "/usr/lib/enigma2/python/Components/ConfigList.py", line 16, in __init__
    height, space = parameters.get("ConfigListSlider", (17, 0))
ValueError: not enough values to unpack (expected 2, got 0)

lib/python/skin.py
@@ -843,7 +843,7 @@ def loadSingleSkinData(desktop, screenID, domSkin, pathSkin, scope=SCOPE_CURRENT
 			try:
 				name = parameter.attrib.get("name")
 				value = parameter.attrib.get("value")
-				parameters[name] = map(parseParameter, [x.strip() for x in value.split(",")]) if "," in value else parseParameter(value)
+				parameters[name] = list(map(parseParameter, [x.strip() for x in value.split(",")]) if "," in value else parseParameter(value))
 			except Exception as err:
 				raise SkinError("Bad parameter: '%s'" % str(err))
 	for tag in domSkin.findall("menus"):

commit 63a2199720a07bb06bad858a893b8c8c8672061c

skin.py: Fix bug introduced in https://github.com/OpenPLi/enigma2/commit/10d2b65dfb7ec74d69e2a5a5562744f278fa0aff
This fixes issue: Bad parameter: ''int' object is not iterable'!

lib/python/skin.py
@@ -843,7 +843,7 @@ def loadSingleSkinData(desktop, screenID, domSkin, pathSkin, scope=SCOPE_CURRENT
 			try:
 				name = parameter.attrib.get("name")
 				value = parameter.attrib.get("value")
-				parameters[name] = list(map(parseParameter, [x.strip() for x in value.split(",")]) if "," in value else parseParameter(value))
+				parameters[name] = list(map(parseParameter, [x.strip() for x in value.split(",")])) if "," in value else parseParameter(value)
 			except Exception as err:
 				raise SkinError("Bad parameter: '%s'" % str(err))
 	for tag in domSkin.findall("menus"):