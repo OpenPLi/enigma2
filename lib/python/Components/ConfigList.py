d393e1aa1cb8d94d7ad14e0b3b3500ae50c63a98
Hains <hainsvdbosch@ziggo.nl>
2022-01-07 16:25:10 +0100

    Revert "ConfigList.py: Add up/down keys"
    Resolved with:
    https://github.com/OpenPLi/enigma2/commit/055ec3d78b8a6ac7e1421da967836b6575a4de62
    
    This reverts commit 01d6516e8740fcbd91d24e087efd23be53932f1b.

--- lib/python/Components/ConfigList.py
+++ lib/python/Components/ConfigList.py
@@ -144,8 +144,6 @@ class ConfigListScreen:
 			"deleteForward": self.keyDelete,
 			"deleteBackward": self.keyBackspace,
 			"toggleOverwrite": self.keyToggleOW,
-			"keyup": self.keyUp,
-			"keydown": self.keyDown,
 			"pageUp": self.keyPageUp,
 			"pageDown": self.keyPageDown,
 			"1": self.keyNumberGlobal,
@@ -259,12 +257,6 @@ class ConfigListScreen:
 	def keyNumberGlobal(self, number):
 		self["config"].handleKey(KEY_0 + number)
 		self.__changed()
-
-	def keyUp(self):
-		self["config"].instance.moveSelection(self["config"].instance.moveUp)
-
-	def keyDown(self):
-		self["config"].instance.moveSelection(self["config"].instance.moveDown)
 
 	def keyPageDown(self):
 		self["config"].pageDown()
