1ea068f432c381f8dd2e3d077d712e2ca370cff0
nautilus7 <spyros.n7@gmail.com>
2022-01-07 22:29:24 +0200

    skin.py: Parse tree from root
    This fixes issue: ''ElementTree' object has no attribute 'tag''!

--- lib/python/skin.py
+++ lib/python/skin.py
@@ -149,7 +149,7 @@ def loadSkin(filename, scope=SCOPE_SKIN, desktop=getDesktop(GUI_SKIN_ID), screen
 							# print("[Skin] DEBUG: Processing a windowstyle ID='%s'." % scrnID)
 							domStyle = xml.etree.cElementTree.ElementTree(xml.etree.cElementTree.Element("skin"))
 							domStyle.getroot().append(element)
-							windowStyles[scrnID] = (desktop, screenID, domStyle, filename, scope)
+							windowStyles[scrnID] = (desktop, screenID, domStyle.getroot(), filename, scope)
 					# Element is not a screen or windowstyle element so no need for it any longer.
 				reloadWindowStyles()  # Reload the window style to ensure all skin changes are taken into account.
 				print("[Skin] Loading skin file '%s' complete." % filename)
