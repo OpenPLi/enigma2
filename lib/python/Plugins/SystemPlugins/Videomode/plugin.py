643d12de290732844295a6b6ab2aa34378e4e710
ims21 <ims@openpli.org>
2022-01-09 18:50:37 +0100

    Videomode - fixed helptexts (#3264)

--- lib/python/Plugins/SystemPlugins/Videomode/plugin.py
+++ lib/python/Plugins/SystemPlugins/Videomode/plugin.py
@@ -182,22 +182,17 @@ class VideoSetup(Screen, ConfigListScreen):
 			self.keySave()
 
 	# for summary:
-	def changedEntry(self):
-		for x in self.onChangedEntry:
-			x()
-
 	def getCurrentEntry(self):
-		return self["config"].getCurrent()[0]
-
-	def getCurrentValue(self):
-		return str(self["config"].getCurrent()[1].getText())
-
-	def getCurrentDescription(self):
-		return self["config"].getCurrent() and len(self["config"].getCurrent()) > 2 and self["config"].getCurrent()[2] or ""
+		self.updateDescription()
+		return ConfigListScreen.getCurrentEntry(self)
 
 	def createSummary(self):
 		from Screens.Setup import SetupSummary
 		return SetupSummary
+	###
+
+	def updateDescription(self):
+		self["description"].setText("%s" % self.getCurrentDescription())
 
 
 class VideomodeHotplug:
