085dfc909f3209184ae5e97586438b365b9d6d79
Hains <hainsvdbosch@ziggo.nl>
2022-01-10 17:08:41 +0100

    Replace cPickle by pickle

--- lib/python/Tools/ISO639.py
+++ lib/python/Tools/ISO639.py
@@ -1,4 +1,4 @@
-import cPickle
+import pickle
 import enigma
 with open(enigma.eEnv.resolve("${datadir}/enigma2/iso-639-3.pck"), 'rb') as f:
-	LanguageCodes = cPickle.load(f)
+	LanguageCodes = pickle.load(f)
