9cc6f42833f449be6d519ec4a8672864685694ec
Hains <hainsvdbosch@ziggo.nl>
2022-01-10 17:07:08 +0100

    Use more absolute import paths

--- lib/python/Components/Sources/StreamService.py
+++ lib/python/Components/Sources/StreamService.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from Source import Source
+from Components.Sources.Source import Source
 from Components.Element import cached
 from Components.SystemInfo import SystemInfo
 from enigma import eServiceReference
