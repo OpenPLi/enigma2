df8ebb2388a557799c91697edc0f21d6860fc6da
Hains <hainsvdbosch@ziggo.nl>
2022-01-10 17:10:46 +0100

    Use absolute import paths

--- lib/python/Screens/Wizard.py
+++ lib/python/Screens/Wizard.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from Screen import Screen
+from Screens.Screen import Screen
 from Screens.MessageBox import MessageBox
 from Components.config import config, ConfigText, ConfigPassword, KEY_LEFT, KEY_RIGHT, KEY_0, KEY_DELETE, KEY_BACKSPACE, KEY_ASCII
 
