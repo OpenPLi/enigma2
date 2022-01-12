3d2a95cc8b6dff0b9c81586d6f0f9b6c4f5952f3
Hains <hainsvdbosch@ziggo.nl>
2022-01-12 06:56:41 +0100

    Replace long() with int()
    long() is deprecated and merged with int.

--- lib/python/Tools/Directories.py
+++ lib/python/Tools/Directories.py
@@ -338,7 +338,7 @@ def getRecordingFilename(basename, dirname=None):
 	# but must not truncate in the middle of a multi-byte utf8 character!
 	# So convert the truncation to unicode and back, ignoring errors, the
 	# result will be valid utf8 and so xml parsing will be OK.
-	filename = unicode(filename[:247], "utf8", "ignore").encode("utf8", "ignore")
+	filename = filename[:247]
 	if dirname is not None:
 		if not dirname.startswith("/"):
 			dirname = os.path.join(defaultRecordingLocation(), dirname)
