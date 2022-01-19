70ac15e5088383925b906b9ef989c6a177fe09e4
nautilus7 <spyros.n7@gmail.com>
2022-01-19 22:19:00 +0200

    Fix urllib.request imports
    Imports were not done propely in commits 4689d33ab8980332cc0f42709c58e80e30a01f15 and 0a406a950d0026999a53a723b27df621fa9cff14

--- lib/python/Components/ImportChannels.py
+++ lib/python/Components/ImportChannels.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import threading
-import urllib
 import os
 import re
 import shutil
@@ -13,6 +12,7 @@ from time import sleep
 from Tools.Notifications import AddNotificationWithID
 from base64 import encodebytes
 from urllib.parse import quote
+from urllib.request import Request, urlopen
 import xml.etree.ElementTree as et
 
 settingfiles = ('lamedb', 'bouquets.', 'userbouquet.', 'blacklist', 'whitelist', 'alternatives.')
@@ -36,7 +36,7 @@ class ImportChannels():
 			self.thread.start()
 
 	def getUrl(self, url, timeout=5):
-		request = urllib.request.Request(url)
+		request = Request(url)
 		if self.header:
 			request.add_header("Authorization", self.header)
 		try:
