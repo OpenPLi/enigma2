e1343fed24c1a5fbe753b40e49829d7a9e15d6e3
Hains <hainsvdbosch@ziggo.nl>
2022-01-22 23:33:09 +0100

    Get rid of /xc2
    Show e.g 23.5° E  instead of 23.5Â° E in service info, timer list etc.

--- lib/python/Components/Converter/ServiceName.py
+++ lib/python/Components/Converter/ServiceName.py
@@ -41,7 +41,7 @@ class ServiceName(Converter):
 			name = ref and info.getName(ref)
 			if name is None:
 				name = info.getName()
-			return name.replace('\xc2\x86', '').replace('\xc2\x87', '').replace('_', ' ')
+			return name.replace('\x86', '').replace('\x87', '').replace('_', ' ')
 		elif self.type == self.PROVIDER:
 			return info.getInfoString(iServiceInformation.sProvider)
 		elif self.type == self.REFERENCE or self.type == self.EDITREFERENCE and hasattr(self.source, "editmode") and self.source.editmode:
