3c6cbd5c99e9ee5b7b86a5be45ebf7633b6ff171
nautilus7 <spyros.n7@gmail.com>
2022-01-24 00:47:38 +0200

    Allow unicode strings in user bouquet filenames
    Until now, only ascii characters were allowed in user bouquet file names and non ascii were stripped away, for example "userbouquet.mybouquet.tv." Since unicode is the default for python3 and we are in the 2022, this should be changed. In addition this could lead to weird naming when a user creates a bouquet using only non ascii characters as they are all filtered out by the old code. For example a bouquet name of "ελληνικά" would result in a corresponding user bouquet file name of "userbouquet..tv".
    
    This PR will allow for all valid utf-8 characters to be used as bouquet file names, except from some special or blacklisted characters (in windows, etc.). The old code prevented these special characters as well.
    
    The same restrictions and freedom is applied in names of picons, meaning picons can now contain utf-8 characters. In other words the picon name can match the service name (although the code might need some polish for this to work).
    
    The whole file name check is moved to a new function in the "Directories.py" module. This function is taken from the "sanitize-filename" module in https://pypi.org/project/sanitize-filename/ with the addition of a couple of blacklisted characters (parenthesis and space) which were also blacklisted in the old code.
    
    The "sanitizeFilename" function makes some more tests (like the maximum number of characters allowed in windows for a file name), which may seem overkill for our use case, but the point is to have reusable code that can applied to other files like recordings, etc.

--- lib/python/Tools/Directories.py
+++ lib/python/Tools/Directories.py
@@ -5,8 +5,9 @@ import inspect
 import os
 
 from enigma import eEnv
-from re import compile
+from re import compile, split
 from stat import S_IMODE
+from unicodedata import normalize
 
 pathExists = os.path.exists
 isMount = os.path.ismount  # Only used in OpenATV /lib/python/Plugins/SystemPlugins/NFIFlash/downloader.py.
@@ -532,3 +533,50 @@ def mediafilesInUse(session):
 
 def shellquote(s):
 	return "'%s'" % s.replace("'", "'\\''")
+
+
+def sanitizeFilename(filename):
+	"""Return a fairly safe version of the filename.
+
+	We don't limit ourselves to ascii, because we want to keep municipality
+	names, etc, but we do want to get rid of anything potentially harmful,
+	and make sure we do not exceed Windows filename length limits.
+	Hence a less safe blacklist, rather than a whitelist.
+	"""
+	blacklist = ["\\", "/", ":", "*", "?", "\"", "<", ">", "|", "\0", "(", ")", " "]
+	reserved = [
+		"CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5",
+		"COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5",
+		"LPT6", "LPT7", "LPT8", "LPT9",
+	]  # Reserved words on Windows
+	filename = "".join(c for c in filename if c not in blacklist)
+	# Remove all charcters below code point 32
+	filename = "".join(c for c in filename if 31 < ord(c))
+	filename = normalize("NFKD", filename)
+	filename = filename.rstrip(". ")  # Windows does not allow these at end
+	filename = filename.strip()
+	if all([x == "." for x in filename]):
+		filename = "__" + filename
+	if filename in reserved:
+		filename = "__" + filename
+	if len(filename) == 0:
+		filename = "__"
+	if len(filename) > 255:
+		parts = split(r"/|\\", filename)[-1].split(".")
+		if len(parts) > 1:
+			ext = "." + parts.pop()
+			filename = filename[:-len(ext)]
+		else:
+			ext = ""
+		if filename == "":
+			filename = "__"
+		if len(ext) > 254:
+			ext = ext[254:]
+		maxl = 255 - len(ext)
+		filename = filename[:maxl]
+		filename = filename + ext
+		# Re-check last character (if there was no extension)
+		filename = filename.rstrip(". ")
+		if len(filename) == 0:
+			filename = "__"
+	return filename
