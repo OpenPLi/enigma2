af8284869a482aa25f4dab6a8662b1332d67b75b
nautilus7 <spyros.n7@gmail.com>
2022-01-29 21:48:43 +0200

    [Downloader.py] Twisted works with byte strings

--- lib/python/Tools/Downloader.py
+++ lib/python/Tools/Downloader.py
@@ -5,29 +5,29 @@ from urllib.parse import urlparse
 
 class HTTPProgressDownloader(client.HTTPDownloader):
 	def __init__(self, url, outfile, headers=None):
-		client.HTTPDownloader.__init__(self, url, outfile, headers=headers, agent="Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenPLi;;;)")
+		client.HTTPDownloader.__init__(self, url, outfile, headers=headers, agent=b"Enigma2 HbbTV/1.1.1 (+PVR+RTSP+DL;OpenPLi;;;)")
 		self.status = self.progress_callback = self.error_callback = self.end_callback = None
 		self.deferred = defer.Deferred()
 
 	def noPage(self, reason):
-		if self.status == "304":
-			client.HTTPDownloader.page(self, "")
+		if self.status == b"304":
+			client.HTTPDownloader.page(self, b"")
 		else:
 			client.HTTPDownloader.noPage(self, reason)
 		if self.error_callback:
 			self.error_callback(reason.getErrorMessage(), self.status)
 
 	def gotHeaders(self, headers):
-		if self.status == "200":
-			if "content-length" in headers:
-				self.totalbytes = int(headers["content-length"][0])
+		if self.status == b"200":
+			if b"content-length" in headers:
+				self.totalbytes = int(headers[b"content-length"][0])
 			else:
 				self.totalbytes = 0
 			self.currentbytes = 0.0
 		return client.HTTPDownloader.gotHeaders(self, headers)
 
 	def pagePart(self, packet):
-		if self.status == "200":
+		if self.status == b"200":
 			self.currentbytes += len(packet)
 		if self.totalbytes and self.progress_callback:
 			self.progress_callback(self.currentbytes, self.totalbytes)
@@ -42,12 +42,14 @@ class HTTPProgressDownloader(client.HTTPDownloader):
 
 class downloadWithProgress:
 	def __init__(self, url, outputfile, contextFactory=None, *args, **kwargs):
+		if isinstance(url, str):
+			url = url.encode("UTF-8")
 		parsed = urlparse(url)
 		scheme = parsed.scheme
 		host = parsed.hostname
-		port = parsed.port or (443 if scheme == 'https' else 80)
+		port = parsed.port or (443 if scheme == b'https' else 80)
 		self.factory = HTTPProgressDownloader(url, outputfile, *args, **kwargs)
-		if scheme == 'https':
+		if scheme == b'https':
 			from twisted.internet import ssl
 			if contextFactory is None:
 				contextFactory = ssl.ClientContextFactory()
