5534ace20e17af8d3c1477e439184e235a56b0dd
Taapat <taapat@gmail.com>
2022-02-04 17:22:15 +0200

    Remove forgotten description in ScanSetup, thx Huevos

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -647,13 +647,6 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 			self["introduction"].text = _("Nothing to scan! Setup your tuner and try again.")
 			return
 		self.createSetup()
-		if not self.selectionChanged in self["config"].onSelectionChanged:
-			self["config"].onSelectionChanged.append(self.selectionChanged)
-		self.selectionChanged()
-
-	def selectionChanged(self):
-		print("selectionChanged")
-		self["description"].setText(self["config"].getCurrent() and len(self["config"].getCurrent()) > 2 and self["config"].getCurrent()[2] or "")
 
 	def runAsync(self, finished_cb):
 		self.finished_cb = finished_cb
