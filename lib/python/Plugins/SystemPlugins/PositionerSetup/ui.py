OpenPLi/enigma2

commit 44a7b7d91cf58960f19f4c237b806fca14cc7121

Fix SyntaxError on lambda's
Properly use syntax for lambda and also use unpack into a list literal syntax [*iter]

***   File "lib/python/Plugins/SystemPlugins/PositionerSetup/ui.py", line 1054
    yi = list(map(lambda (x, y): x, readings.values()))
                         ^
SyntaxError: invalid syntax

lib/python/Plugins/SystemPlugins/PositionerSetup/ui.py
@@ -1050,9 +1050,9 @@ class PositionerSetup(Screen):
 			print((_("Lock ratio") + "     %5.1f" + chr(176) + "   : %6.2f") % (pos, lock), file=log)
 
 		def optimise(readings):
-			xi = readings.keys()
-			yi = list(map(lambda (x, y): x, readings.values()))
-			x0 = sum(list(map(mul, xi, yi))) / sum(yi)
+			xi = [*readings]
+			yi = list(map(lambda x: x[0], readings.values()))
+			x0 = sum(map(mul, xi, yi)) // sum(yi)
 			xm = xi[yi.index(max(yi))]
 			return (x0, xm)
 
@@ -1185,9 +1185,9 @@ class PositionerSetup(Screen):
 			print((_("Lock ratio") + " [%2d]       : %6.2f") % (pos, lock), file=log)
 
 		def optimise(readings):
-			xi = readings.keys()
-			yi = list(map(lambda (x, y): x, readings.values()))
-			x0 = int(round(sum(list(map(mul, xi, yi))) / sum(yi)))
+			xi = [*readings]
+			yi = list(map(lambda x[0]: x, readings.values()))
+			x0 = int(round(sum(map(mul, xi, yi)) / sum(yi)))
 			xm = xi[yi.index(max(yi))]
 			return (x0, xm)
 

commit e501aa7a3d2ed86fea555b143fed33e97b0d9a96

[positionerSetup] Fix typo in lambda
Introduced with https://github.com/OpenPLi/enigma2/commit/44a7b7d91cf58960f19f4c237b806fca14cc7121#diff-f6eef0f3172644568b0dcf95c318f9e74fc89a29997b5a93aaafe912fcea67c8R1189

lib/python/Plugins/SystemPlugins/PositionerSetup/ui.py
@@ -1185,7 +1185,7 @@ class PositionerSetup(Screen):
 
 		def optimise(readings):
 			xi = [*readings]
-			yi = list(map(lambda x[0]: x, readings.values()))
+			yi = list(map(lambda x: x[0], readings.values()))
 			x0 = int(round(sum(map(mul, xi, yi)) / sum(yi)))
 			xm = xi[yi.index(max(yi))]
 			return (x0, xm)