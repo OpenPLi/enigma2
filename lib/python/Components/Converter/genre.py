77d61daba9fc93fd9148ef31d65038c2de41e587
Pr2 <pr2git@users.noreply.github.com>
2022-02-06 15:30:46 +0100

    Fix theme assignment based on ETSI EN 300 707 V1.2.1 (en_300707v010201p.pdf), current implementation was based on draft. (#3355)

--- lib/python/Components/Converter/genre.py
+++ lib/python/Components/Converter/genre.py
@@ -5,31 +5,27 @@
 # may grow or be replaced..
 #
 maintype = [	_("Reserved"),
-		_("Movie/Drama"),
-		_("News Current Affairs"),
-		_("Show Games show"),
+		_("Drama and Films"),
+		_("News/Current Affairs/Social"),
+		_("Show/Game Show/Leisure hobbies"),
 		_("Sports"),
-		_("Children/Youth"),
+		_("Children/Youth/Education/Science"),
 		_("Music/Ballet/Dance"),
-		_("Arts/Culture"),
-		_("Social/Political/Economics"),
-		_("Education/Science/..."),
-		_("Leisure hobbies"),
-		_("Other")]
+		_("Arts/Culture (without music)")]
 
 
 subtype = {}
 # Movie/Drama
 subtype[1] = [
-					_("movie/drama (general)"),
+					_("movie (general)"),
 					_("detective/thriller"),
 					_("adventure/western/war"),
 					_("science fiction/fantasy/horror"),
 					_("comedy"),
-					_("soap/melodram/folkloric"),
+					_("soap/melodrama/folkloric"),
 					_("romance"),
-					_("serious/classical/religious/historical movie/drama"),
-					_("adult movie/drama")]
+					_("serious/classical/religious/historical drama"),
+					_("adult movie")]
 
 # News Current Affairs
 subtype[2] = [
@@ -37,14 +33,25 @@ subtype[2] = [
 					_("news/weather report"),
 					_("news magazine"),
 					_("documentary"),
-					_("discussion/interview/debate")]
+					_("discussion/interview/debate"),
+					_("social/political issues/economics (general)"),
+					_("magazines/reports/documentary"),
+					_("economics/social advisory"),
+					_("remarkable people")]
 
 # Show Games show
 subtype[3] = [
 					_("show/game show (general)"),
 					_("game show/quiz/contest"),
 					_("variety show"),
-					_("talk show")]
+					_("talk show"),
+					_("leisure hobbies (general)"),
+					_("tourism/travel"),
+					_("handicraft"),
+					_("motoring"),
+					_("fitness and health"),
+					_("cooking"),
+					_("advertisement/shopping")]
 
 # Sports
 subtype[4] = [
@@ -53,22 +60,31 @@ subtype[4] = [
 					_("sports magazine"),
 					_("football/soccer"),
 					_("tennis/squash"),
-					_("team sports"),
+					_("team sports/excluding football"),
 					_("athletics"),
 					_("motor sport"),
 					_("water sport"),
 					_("winter sport"),
 					_("equestrian"),
-					_("martial sports")]
+					_("martial sports"),
+					_("local sports")]
 
 # Children/Youth
 subtype[5] = [
-					_("childrens's/youth program (general)"),
+					_("childrens's youth program (general)"),
 					_("pre-school children's program"),
 					_("entertainment (6-14 year old)"),
 					_("entertainment (10-16 year old)"),
 					_("information/education/school program"),
-					_("cartoon/puppets")]
+					_("cartoon/puppets"),
+					_("educational/science/factual topics (general)"),
+					_("nature/animals/environment"),
+					_("technology/natural sciences"),
+					_("medicine/physiology/psychology"),
+					_("foreign countries/expeditions"),
+					_("social/spiritual sciences"),
+					_("further education"),
+					_("languages")]
 
 # Music/Ballet/Dance
 subtype[6] = [
@@ -80,7 +96,7 @@ subtype[6] = [
 					_("musical/opera"),
 					_("ballet")]
 
-# Arts/Culture
+# Arts/Culture (without music)
 subtype[7] = [
 					_("arts/culture (without music, general)"),
 					_("performing arts"),
@@ -94,42 +110,6 @@ subtype[7] = [
 					_("new media"),
 					_("arts/culture magazine"),
 					_("fashion")]
-
-# Social/Political/Economics
-subtype[8] = [
-					_("social/political issues/economics (general)"),
-					_("magazines/reports/documentary"),
-					_("economics/social advisory"),
-					_("remarkable people")]
-
-# Education/Science/...
-subtype[9] = [
-					_("education/science/factual topics (general)"),
-					_("nature/animals/environment"),
-					_("technology/natural science"),
-					_("medicine/physiology/psychology"),
-					_("foreign countries/expeditions"),
-					_("social/spiritual science"),
-					_("further education"),
-					_("languages")]
-
-# Leisure hobies
-subtype[10] = [
-					_("leisure hobbies (general)"),
-					_("tourism/travel"),
-					_("handicraft"),
-					_("motoring"),
-					_("fitness & health"),
-					_("cooking"),
-					_("advertisement/shopping"),
-					_("gardening")]
-
-# Other
-subtype[11] = [
-					_("original language"),
-					_("black & white"),
-					_("unpublished"),
-					_("live broadcast")]
 
 
 def getGenreStringMain(hn, ln):
