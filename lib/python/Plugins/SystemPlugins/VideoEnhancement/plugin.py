99b52c3d2fe049518c48d70c959381f0fd6e778f
georgeflu <58104618+georgeflu@users.noreply.github.com>
2022-02-26 23:24:27 +0200

    Update plugin.py
    fix  VideoEnhancement crash 
    thanks to SDG team

--- lib/python/Plugins/SystemPlugins/VideoEnhancement/plugin.py
+++ lib/python/Plugins/SystemPlugins/VideoEnhancement/plugin.py
@@ -5,6 +5,7 @@ from Components.ActionMap import ActionMap
 from Components.Sources.StaticText import StaticText
 from Screens.Screen import Screen
 from Screens.MessageBox import MessageBox
+from .import VideoEnhancement
 import os
 import skin
 
