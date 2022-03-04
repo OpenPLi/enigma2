097b5b47a96c9f20e5666fc15f49ae95c3fb598c
Hains <hainsvdbosch@ziggo.nl>
2022-03-04 17:01:28 +0100

    Scanner.py: Temp. solution for x-debian-package, thx @blzr
    Workaround to display *.ipk files in Software management system,
    so that users able to install IPK package.
    
    Needs proper fix, along with:
    https://github.com/OpenPLi/enigma2/commit/7be1cf451e36a4781704ccfed22f3e3abd4efdb0

--- lib/python/Components/Scanner.py
+++ lib/python/Components/Scanner.py
@@ -79,6 +79,10 @@ def getType(file):
 
 		if ext == "dat" and file[-11:-6].lower() == "avseq":
 			return "video/x-vcd"
+
+		if ext == "ipk":
+			return "application/x-debian-package"
+
 	return type
 
 
