9ccb96b7ce6bde697a386f03528178df346cddb3
Dimitrij <Dima-73@inbox.lv>
2022-03-06 14:43:07 +0200

    [VariableValue] remove try/except (#3391)

--- lib/python/Components/VariableValue.py
+++ lib/python/Components/VariableValue.py
@@ -7,17 +7,14 @@ class VariableValue(object):
 	def setValue(self, value):
 		self.__value = (isinstance(value, int) and value) or (isinstance(value, str) and value.isdigit() and int(value)) or 0
 		if self.instance:
-			try:
-				self.instance.setValue(self.__value)
-			except TypeError:
-				self.instance.setValue(0)
+			self.instance.setValue(self.__value)
+
 
 	def getValue(self):
 		return self.__value
 
 	def postWidgetCreate(self, instance):
-		print self
-		print self.GUI_WIDGET
-		self.instance.setValue(self.__value)
+		if self.instance:
+			self.instance.setValue(self.__value)
 
 	value = property(getValue, setValue)
