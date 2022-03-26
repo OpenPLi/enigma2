15e6e0ee4c9550795701108b332d4411edec0c80
Dima73 <dima-73@inbox.lv>
2022-03-26 08:17:42 +0200

    Do not close root FBC frontend
    -root FBC frontend always applies voltage to the LNB

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1712,7 +1712,7 @@ int eDVBFrontend::tuneLoopInt()  // called by m_tuneTimer
 		if (tmp == -1 && sec_fe != this && !prev->m_inuse) {
 			int state = sec_fe->m_state;
 			// workaround to put the kernel frontend thread into idle state!
-			if (state != eDVBFrontend::stateIdle && state != stateClosed)
+			if (!m_fbc && state != eDVBFrontend::stateIdle && state != stateClosed)
 			{
 				sec_fe->closeFrontend(true);
 				state = sec_fe->m_state;
