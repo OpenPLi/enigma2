703ca6098f6db2fb02627ca88319e5519429f36d
Dima73 <dima-73@inbox.lv>
2022-04-23 11:09:55 +0300

    Add "Http(s) stream start delay" setting
    -If stream from another box(old default  delay 0 ms)
    
    Tnanks @jbleyel

--- lib/base/httpstream.cpp
+++ lib/base/httpstream.cpp
@@ -16,10 +16,15 @@ eHttpStream::eHttpStream()
 	partialPktSz = 0;
 	tmpBufSize = 32;
 	tmpBuf = (char*)malloc(tmpBufSize);
+	startDelay = 0;
 	if (eConfigManager::getConfigBoolValue("config.usage.remote_fallback_enabled", false))
 		startDelay = 500000;
 	else
-		startDelay = 0;
+	{
+		int _startDelay = eConfigManager::getConfigIntValue("config.usage.http_startdelay");
+		if (_startDelay > 0)
+			startDelay = _startDelay * 1000;
+	}
 }
 
 eHttpStream::~eHttpStream()
