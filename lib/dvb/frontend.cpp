67a8121d26368092ca77a29c2ffd300dc09f58c2
Dimitrij <Dima-73@inbox.lv>
2022-05-18 21:52:46 +0300

    [frontend] update snr/db "GIGA DVB-T2/C NIM (TT3L10)" (#3451)
    -dual plug & play tuner GB UE/Quad UHD 4K

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1234,7 +1234,6 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 		|| !strcmp(m_description, "GIGA DVB-C/T2 NIM (SI4768)")
 		|| !strcmp(m_description, "GIGA DVB-C/T2 NIM (SI41682)")
 		|| !strcmp(m_description, "GIGA DVB-T2/C NIM (TT2L10)")
-		|| !strcmp(m_description, "GIGA DVB-T2/C NIM (TT3L10)")
 		)
 	{
 		int type = -1;
@@ -1250,6 +1249,10 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 				ter_max = 4200;
 				break;
 		}
+	}
+	else if (!strcmp(m_description, "GIGA DVB-T2/C NIM (TT3L10)")) // dual plug & play tuner GB UE/Quad UHD 4K 
+	{
+		ret = (int)(snr / 15);
 	}
 	else if (!strcmp(m_description, "BCM7356 DVB-S2 NIM (internal)") // VU+ Solo2
 		|| !strcmp(m_description, "BCM7346 DVB-S2 NIM (internal)")
