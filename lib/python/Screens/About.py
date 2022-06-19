dae7f441cae175881d5633df0c9e3e2263ea7b6b
Dimitrij <Dima-73@inbox.lv>
2022-06-19 15:18:16 +0300

    [About] not visible  fp_version is zero (#3473)

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -72,7 +72,7 @@ class About(Screen):
 		fp_version = getFPVersion()
 		if fp_version is None:
 			fp_version = ""
-		else:
+		elif fp_version != 0:
 			fp_version = _("Frontprocessor version: %s") % fp_version
 			AboutText += fp_version + "\n"
 
