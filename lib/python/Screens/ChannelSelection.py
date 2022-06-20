debeed84dda5fc14d286c189e81bf2ac203320ad
Dimitrij <Dima-73@inbox.lv>
2022-06-20 14:44:36 +0300

    [ChannelSelection]  allow to use the power button in the channel selector (#3479)

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -1931,6 +1931,8 @@ config.servicelist.startupmode = ConfigText(default="tv")
 
 
 class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelectionEPG, SelectionEventInfo):
+	ALLOW_SUSPEND = True
+
 	def __init__(self, session):
 		ChannelSelectionBase.__init__(self, session)
 		ChannelSelectionEdit.__init__(self)
