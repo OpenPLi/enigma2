af508c35038f54092a7c89e8e42088a506da4480
Betacentauri <betacentauri@arcor.de>
2022-09-03 16:01:56 +0200

    [Navigation] Streaming: Retry play service
    If a stream is active which blocks a tuner on the same box,
    changing the channel might not work. The stream client is stopped
    before zapping, but the streamserver still blocks the tuner when e2
    tries to allocate the tuner.
    Use an eTimer to give the streamserver the time to release the tuner
    and then try again.

--- lib/python/Navigation.py
+++ lib/python/Navigation.py
@@ -132,6 +132,11 @@ class Navigation:
 							print "[Navigation] Failed to start: ", alternativeref.toString()
 							self.currentlyPlayingServiceReference = None
 							self.currentlyPlayingServiceOrGroup = None
+							if oldref and "://" in oldref.getPath():
+								print "[Navigation] Streaming was active -> try again" # use timer to give the streamserver the time to deallocate the tuner
+								self.retryServicePlayTimer = eTimer()
+								self.retryServicePlayTimer.callback.append(boundFunction(self.playService, ref, checkParentalControl, forceRestart, adjust))
+								self.retryServicePlayTimer.start(500, True)
 						else:
 							print "[Navigation] alternative ref as simulate: ", alternativeref.toString()
 					return 0
@@ -184,6 +189,11 @@ class Navigation:
 					print "[Navigation] Failed to start: ", playref.toString()
 					self.currentlyPlayingServiceReference = None
 					self.currentlyPlayingServiceOrGroup = None
+					if oldref and "://" in oldref.getPath():
+						print "[Navigation] Streaming was active -> try again" # use timer to give the streamserver the time to deallocate the tuner
+						self.retryServicePlayTimer = eTimer()
+						self.retryServicePlayTimer.callback.append(boundFunction(self.playService, ref, checkParentalControl, forceRestart, adjust))
+						self.retryServicePlayTimer.start(500, True)
 				self.skipServiceReferenceReset = False
 				if setPriorityFrontend:
 					setPreferredTuner(int(config.usage.frontend_priority.value))
