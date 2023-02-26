341315bddb5ef1a272ee9381e4692360042f1a87
Dima73 <dima-73@inbox.lv>
2023-02-26 08:56:39 +0200

    [frontend] more calculate dB VU+ Zero4K

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1325,7 +1325,7 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 	}
 	else if (!strcmp(m_description, "Vuplus DVB-S NIM(SI2166)")) // VU+ Zero4K
 	{
-		ret = snr;
+		ret = snr / 40;
 	}
 	else if (!strncmp(m_description, "Si216", 5)) // New models with SI tuners
 	{
