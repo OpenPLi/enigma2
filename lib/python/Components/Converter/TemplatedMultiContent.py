ef1723cd5dceb7b855d705541004502260b4a94e
Rowland Creed <rowland@moralet.com>
2023-03-14 15:33:35 +0100

    [Listbox.py] add applySkin
    This allows feeding attributes from the skin widget directly to listbox.cpp.
    The current attribute "selectionFrame" allows lisbox borders to be disabled individually.
    
    (cherry picked from commit 5a71a4c4b5bc982085e65dcfb57e91a681b47f67)

--- lib/python/Components/Converter/TemplatedMultiContent.py
+++ lib/python/Components/Converter/TemplatedMultiContent.py
@@ -20,8 +20,13 @@ class TemplatedMultiContent(StringList):
 		assert "template" in self.template or "templates" in self.template
 		assert "template" in self.template or "default" in self.template["templates"]  # We need to have a default template.
 		if "template" not in self.template:  # Default template can be ["template"] or ["templates"]["default"].
-			self.template["template"] = self.template["templates"]["default"][1]
-			self.template["itemHeight"] = self.template["template"][0]
+			self.template["template"] = self.template["templates"]["default"][1] # mandatory
+			self.template["itemHeight"] = self.template["template"][0] # mandatory
+			self.template["selectionEnabled"] = self.template["template"][2] if len(self.template["template"]) > 2 and self.template["template"][2] is not None else True  # optional. To skip use None padding.
+			self.template["scrollbarMode"] = self.template["template"][3] if len(self.template["template"]) > 3 and self.template["template"][3] is not None  else "showOnDemand"  # optional. To skip use None padding.
+			# if present self.template["template"][4] is a dict which should have "orientation" and "itemWidth"
+			self.template["itemWidth"] =  self.template["template"][4].get("itemWidth") if len(self.template["template"]) > 4 else None # optional
+			self.template["orientation"] = self.template["template"][4].get("orientation") if len(self.template["template"]) > 4 else None # optional
 
 	def changed(self, what):
 		if not self.content:
@@ -44,16 +49,26 @@ class TemplatedMultiContent(StringList):
 			templates = self.template.get("templates")  # If skin defined "templates", that means that it defines multiple styles in a dict. template should still be a default.
 			template = self.template.get("template")
 			itemheight = self.template["itemHeight"]
+			itemwidth = self.template.get("itemWidth")
+			orientation = self.template.get("orientation")
 			selectionEnabled = self.template.get("selectionEnabled", True)
 			scrollbarMode = self.template.get("scrollbarMode", "showOnDemand")
 			if templates and style and style in templates:  # If we have a custom style defined in the source, and different templates in the skin, look it up
+				# "template" and "itemheight" are mandatory in a template. selectionEnabled, scrollbarMode, itemwidth, and orientation are optional.
 				template = templates[style][1]
 				itemheight = templates[style][0]
-				if len(templates[style]) > 2:
+				if len(templates[style]) > 2 and templates[style][2] is not None:
 					selectionEnabled = templates[style][2]
-				if len(templates[style]) > 3:
+				if len(templates[style]) > 3 and templates[style][3] is not None:
 					scrollbarMode = templates[style][3]
+				if len(templates[style]) > 4 and templates[style][4] is not None and templates[style][4].get("itemWidth") is not None and templates[style][4].get("orientation") is not None:
+					itemwidth = templates[style][4]["itemWidth"]
+					orientation = templates[style][4]["orientation"]
+				
 			self.content.setTemplate(template)
+			if orientation is not None and itemwidth is not None:
+				self.content.setOrientation(int(orientation))
+				self.content.setItemWidth(int(itemwidth))
 			self.content.setItemHeight(int(itemheight))
 			self.selectionEnabled = selectionEnabled
 			self.scrollbarMode = scrollbarMode
