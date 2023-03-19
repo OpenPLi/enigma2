d0dc997c06c00b5f5598458702f42369fc7cf52f
Taapat <taapat@gmail.com>
2023-03-19 18:17:34 +0200

    Introduces partial event search in EventView and EpgSelection
    These screens already have a similar event search, but it does not show all possible events.
    Therefore, the option to search for partial events is added to the unused yellow button, so that partially similar events can also be found and used.
    
    Also remove imported but unused and fix over-indented.

--- lib/python/Screens/EpgSelection.py
+++ lib/python/Screens/EpgSelection.py
@@ -4,7 +4,7 @@ import Screens.InfoBar
 from Components.config import config, ConfigClock
 from Components.Pixmap import Pixmap
 from Components.Label import Label
-from Components.EpgList import EPGList, EPG_TYPE_SINGLE, EPG_TYPE_SIMILAR, EPG_TYPE_MULTI
+from Components.EpgList import EPGList, EPG_TYPE_SINGLE, EPG_TYPE_SIMILAR, EPG_TYPE_MULTI, EPG_TYPE_PARTIAL
 from Components.ActionMap import ActionMap
 from Components.UsageConfig import preferredTimerPath
 from Components.Sources.ServiceEvent import ServiceEvent
@@ -47,9 +47,16 @@ class EPGSelection(Screen):
 		if isinstance(service, str) and eventid is not None:
 			self.type = EPG_TYPE_SIMILAR
 			self.setTitle(_("Similar EPG"))
+			self["key_yellow"] = StaticText(_("Partial"))
+			self["key_blue"] = StaticText()
+			self.currentService = service
+			self.eventid = eventid
+			self.zapFunc = None
+		elif not service and isinstance(eventid, str):
+			self.type = EPG_TYPE_PARTIAL
+			self.title = _("Partial EPG")
 			self["key_yellow"] = StaticText()
 			self["key_blue"] = StaticText()
-			self["key_red"] = StaticText()
 			self.currentService = service
 			self.eventid = eventid
 			self.zapFunc = None
@@ -198,20 +205,22 @@ class EPGSelection(Screen):
 
 	#just used in multipeg
 	def onCreate(self):
-		l = self["list"]
-		l.recalcEntrySize()
+		li = self["list"]
+		li.recalcEntrySize()
 		if self.type == EPG_TYPE_MULTI:
-			l.fillMultiEPG(self.services, self.ask_time)
-			l.moveToService(Screens.InfoBar.InfoBar.instance and Screens.InfoBar.InfoBar.instance.servicelist.getCurrentSelection() or self.session.nav.getCurrentlyPlayingServiceOrGroup())
+			li.fillMultiEPG(self.services, self.ask_time)
+			li.moveToService(Screens.InfoBar.InfoBar.instance and Screens.InfoBar.InfoBar.instance.servicelist.getCurrentSelection() or self.session.nav.getCurrentlyPlayingServiceOrGroup())
 		elif self.type == EPG_TYPE_SINGLE:
 			service = self.currentService
 			self["Service"].newService(service.ref)
 			if not self.saved_title:
 				self.saved_title = self.instance.getTitle()
 			self.setTitle(self.saved_title + ' - ' + service.getServiceName())
-			l.fillSingleEPG(service)
+			li.fillSingleEPG(service)
+		elif self.type == EPG_TYPE_PARTIAL:
+			li.fill_partial_list(self.eventid)
 		else:
-			l.fillSimilarList(self.currentService, self.eventid)
+			li.fillSimilarList(self.currentService, self.eventid)
 
 	def eventViewCallback(self, setEvent, setService, val):
 		l = self["list"]
@@ -271,6 +280,12 @@ class EPGSelection(Screen):
 				self.sort_type = 0
 			self["list"].sortSingleEPG(self.sort_type)
 			self.setSortDescription()
+		elif self.type == EPG_TYPE_SIMILAR:
+			cur = self["list"].getCurrent()
+			cur_event = cur and cur[0]
+			event = cur_event and cur_event.getEventName()
+			if event:
+				self.session.open(EPGSelection, None, None, event)
 
 	def setSortDescription(self):
 		if self.sort_type == 1:
@@ -574,8 +589,8 @@ class EPGSelection(Screen):
 				self.key_red_choice = self.EMPTY
 			return
 		elif self.key_red_choice != self.ZAP and self.zapFunc is not None:
-				self["key_red"].setText(_("Zap"))
-				self.key_red_choice = self.ZAP
+			self["key_red"].setText(_("Zap"))
+			self.key_red_choice = self.ZAP
 
 		if event is None:
 			if self.key_green_choice != self.EMPTY:
