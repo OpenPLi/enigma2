c7b413d8c0a6fe3638fc1e328fe11b1f73741f51
WanWizard <wanwizard@wanwizard.eu>
2023-05-11 10:21:18 +0100

    Revert "remove check on [ in the title, legacy code"
    Not so legacy as was suggested, breaks move mode and edit.
    
    This reverts commit f76cce42d66d30b9670a695c866f0f184b5ad99b.

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -1477,7 +1477,9 @@ class ChannelSelectionBase(Screen):
 
 	def buildTitleString(self):
 		titleStr = self.getTitle()
-		pos = titleStr.find(')')
+		pos = titleStr.find(']')
+		if pos == -1:
+			pos = titleStr.find(')')
 		if pos != -1:
 			titleStr = titleStr[:pos + 1]
 			Len = len(self.servicePath)
