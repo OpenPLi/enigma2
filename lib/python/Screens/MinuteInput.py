568844f06c3c8ca99e1b55590217a84f3be0e114
ims <ims21@users.sourceforge.net>
2023-08-26 08:32:10 +0200

    MinuteInput - added posibility set maxValue for minute input

--- lib/python/Screens/MinuteInput.py
+++ lib/python/Screens/MinuteInput.py
@@ -4,10 +4,10 @@ from Components.Input import Input
 
 
 class MinuteInput(Screen):
-		def __init__(self, session, basemins=5):
+		def __init__(self, session, basemins=5, maxValue=False):
 			Screen.__init__(self, session)
 
-			self["minutes"] = Input(str(basemins), type=Input.NUMBER)
+			self["minutes"] = Input(str(basemins), type=Input.NUMBER, maxValue=maxValue)
 
 			self["actions"] = NumberActionMap(["InputActions", "MinuteInputActions", "TextEntryActions", "KeyboardInputActions"],
 			{
