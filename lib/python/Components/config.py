af2eedea1f34e58a25211e2368383efbde93902a
Littlesat <littlesat@openpli.org>
2023-09-18 11:24:16 +0200

    Add property to ConfigDirectory in config.py

--- lib/python/Components/config.py
+++ lib/python/Components/config.py
@@ -1240,6 +1240,8 @@ class ConfigDirectory(ConfigText):
 			val = ""
 		ConfigText.setValue(self, val)
 
+	value = property(getValue, setValue)
+
 	def getMulti(self, selected):
 		if self.text == "":
 			return ("mtext"[1 - selected:], _("List of storage devices"), list(range(0)))
