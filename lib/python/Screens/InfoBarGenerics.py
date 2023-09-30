38cd20457a885ace3ee4c9a999197a5f92718dbc
Taapat <taapat@gmail.com>
2023-09-30 21:01:52 +0300

    ChoiceBox improvements
    Use variable parent class methods.
    Use property objects.
    Use the instance variable list to check if list exists.
    Checks if the variable list is. not the size of list.
    Use enumerate to get position in list.
    Other code style fixes.

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2218,7 +2218,7 @@ class ExtensionsList(ChoiceBox):
 			for idx, x in enumerate(removed):
 				del self.list[x - idx]
 				del self.summarylist[x - idx]
-			self["list"].setList(self.list)
+			self["list"].list = self.list
 			self.updateSummary(self["list"].getSelectionIndex())
 			if removed:
 				for f in self.onLayoutFinish:  # For screen resize
