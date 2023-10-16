3884629f550133aa782f5591a6705cec45af3621
Littlesat <littlesat@openpli.org>
2023-10-16 14:10:10 +0200

    Resolve mistake in previous commit
    Thanks Huevos
    
    By the way your commit is broken.
    
    enumerate(ref.split
    
    Should be ...
    
    enumerate(playrefstring.split

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -159,7 +159,7 @@ def streamrelayChecker(playref):
 	if '%3a//' not in playrefstring and playrefstring in whitelist.streamrelay:
 		url = "http://%s:%s/" % (config.misc.softcam_streamrelay_url.getHTML(), config.misc.softcam_streamrelay_port.value)
 		if "127.0.0.1" in url:
-				playrefmod = ":".join([("%x" % (int(x[1], 16) + 1)).upper() if x[0] == 6 else x[1] for x in enumerate(ref.split(':'))])
+				playrefmod = ":".join([("%x" % (int(x[1], 16) + 1)).upper() if x[0] == 6 else x[1] for x in enumerate(playrefstring.split(':'))])
 		else:
 				playrefmod = playrefstring
 		playref = eServiceReference("%s%s%s:%s" % (playrefmod, url.replace(":", "%3a"), playrefstring.replace(":", "%3a"), ServiceReference(playref).getServiceName()))
