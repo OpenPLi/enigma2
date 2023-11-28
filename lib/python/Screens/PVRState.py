ac28ec4a0f8e42ed158fd9f147aa843086cac1da
Littlesat <littlesat@openpli.org>
2023-11-28 22:25:54 +0100

    Resolve GSOD due to I forgot something to commit in ca9196357
    Traceback (most recent call last):
      File "/usr/lib/enigma2/python/Navigation.py", line 87, in dispatchEvent
        x(i)
      File "/usr/lib/enigma2/python/Components/ServiceEventTracker.py", line 82, in event
        func[2]()
      File "/usr/lib/enigma2/python/Screens/InfoBarGenerics.py", line 2127, in __serviceStarted
        self.__seekableStatusChanged()
      File "/usr/lib/enigma2/python/Screens/InfoBarGenerics.py", line 2118, in __seekableStatusChanged
        self.setSeekState(self.SEEK_STATE_PLAY)
      File "/usr/lib/enigma2/python/Screens/InfoBarGenerics.py", line 1573, in setSeekState
        c(self.seekstate)
      File "/usr/lib/enigma2/python/Screens/InfoBarGenerics.py", line 1811, in __playStateChanged
        self.pvrStateDialog["statusicon"].setPixmapNum([">", '||', 'END', '>>', '<<', '/'].index(playstate[0]))
    KeyError: 'statusicon'
    [ePyObject] (CallObject(<bound method Navigation.dispatchEvent of <Navigation.Navigation object at 0xb165d700>>,(0,)) failed)
    
    I forgot to change the PVRState.py screen sorry

--- lib/python/Screens/PVRState.py
+++ lib/python/Screens/PVRState.py
@@ -1,13 +1,15 @@
 from Screens.Screen import Screen
 
 from Components.Label import Label
+from Components.Pixmap import MultiPixmap
 
 
 class PVRState(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		self["state"] = Label(text="")
-
+		self["speed"] = Label()
+		self["statusicon"] = MultiPixmap()
 
 class TimeshiftState(PVRState):
 	pass
