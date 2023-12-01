1071167d933ef33c76b0a7090fdbbe4eb1233186
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2023-12-01 17:46:26 +0200

    [Updated] Renamed parameter to enable blinking

--- lib/python/Components/Converter/ConditionalShowHide.py
+++ lib/python/Components/Converter/ConditionalShowHide.py
@@ -8,7 +8,7 @@ class ConditionalShowHide(Converter):
 		Converter.__init__(self, argstr)
 		args = argstr.split(',')
 		self.invert = "Invert" in args
-		self.blink = "Blink" in args and config.usage.disable_blinking.value
+		self.blink = "Blink" in args and config.usage.enable_blinking.value
 		if self.blink:
 			self.blinktime = len(args) > 1 and args[1].isdigit() and int(args[1]) or 500
 			if len(args) == 3:
