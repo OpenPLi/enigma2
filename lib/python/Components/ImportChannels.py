61ee91c4842bd3a3075f764c9b1ee75fb62a68a9
Littlesat <littlesat@openpli.org>
2024-02-12 13:03:59 +0100

    Adapt Importchannels to bouquets with mixed bouquetlist
    You can have bouquets that refer to mixed TV and Radio bouqeuts list.
    Previously these could not be imported by the fallback client box. This
    change does resolve it as when an item is not a TV bouquet list it
    checks also for being a Radion bouquet list

--- lib/python/Components/ImportChannels.py
+++ lib/python/Components/ImportChannels.py
@@ -90,8 +90,6 @@ class ImportChannels:
 	def ImportGetFilelist(self, remote=False, *files):
 		result = []
 		for file in files:
-			# determine the type of bouquet file
-			type = 1 if file.endswith('.tv') else 2
 			# read the contents of the file
 			try:
 				if remote:
@@ -113,12 +111,11 @@ class ImportChannels:
 			# check the contents for more bouquet files
 			for line in content:
 #				print ("[Import Channels] %s" % line)
-				# check if it contains another bouquet reference
-				r = re.match('#SERVICE 1:7:%d:0:0:0:0:0:0:0:FROM BOUQUET "(.*)" ORDER BY bouquet' % type, line)
+				# check if it contains another bouquet reference, first tv type then radio type
+				r = re.match('#SERVICE 1:7:1:0:0:0:0:0:0:0:FROM BOUQUET "(.*)" ORDER BY bouquet', line) or re.match('#SERVICE 1:7:2:0:0:0:0:0:0:0:FROM BOUQUET "(.*)" ORDER BY bouquet', line)
 				if r:
 					# recurse
 					result.extend(self.ImportGetFilelist(remote, r.group(1)))
-
 			# add add the file itself
 			result.append(file)
 
