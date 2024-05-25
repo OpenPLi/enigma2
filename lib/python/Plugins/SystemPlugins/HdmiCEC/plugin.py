e803a3ca495de65a9b02cc359ce6d49e19738fe6
Huevos <rowland@moralet.com>
2024-05-25 16:21:23 +0200

    [HdmiCEC] inherit UI from ConfigListScreen

--- lib/python/Plugins/SystemPlugins/HdmiCEC/plugin.py
+++ lib/python/Plugins/SystemPlugins/HdmiCEC/plugin.py
@@ -1,8 +1,16 @@
-from Screens.Screen import Screen
+from os import path
+
+import Components.HdmiCec
+from Components.ActionMap import ActionMap
+from Components.Button import Button
 from Components.ConfigList import ConfigListScreen
 from Components.config import config
 from Components.Label import Label
 from Components.Sources.StaticText import StaticText
+from Plugins.Plugin import PluginDescriptor
+from Screens.LocationBox import LocationBox
+from Screens.Screen import Screen
+
 
 
 class HdmiCECSetupScreen(ConfigListScreen, Screen):
@@ -11,30 +19,19 @@ class HdmiCECSetupScreen(ConfigListScreen, Screen):
 		self.skinName = "Setup"
 		self.setTitle(_("HDMI-CEC setup"))
 
-		from Components.ActionMap import ActionMap
-		from Components.Button import Button
-
-		self["key_red"] = StaticText(_("Cancel"))
-		self["key_green"] = StaticText(_("OK"))
 		self["key_yellow"] = StaticText(_("Set fixed"))
 		self["key_blue"] = StaticText(_("Clear fixed"))
 		self["description"] = Label("")
 
-		self["actions"] = ActionMap(["SetupActions", "ColorActions", "MenuActions"],
+		self["actions"] = ActionMap(["ColorActions"],
 		{
-			"ok": self.keyOk,
-			"save": self.keyGo,
-			"cancel": self.keyCancel,
-			"green": self.keyGo,
-			"red": self.keyCancel,
 			"yellow": self.setFixedAddress,
 			"blue": self.clearFixedAddress,
-			"menu": self.closeRecursive,
 		}, -2)
 
 		self.list = []
 		self.logpath_entry = None
-		ConfigListScreen.__init__(self, self.list, session=self.session, on_change=self.createSetup)
+		ConfigListScreen.__init__(self, self.list, session=self.session, on_change=self.createSetup, fullUI=True)
 		self.createSetup()
 		self.updateAddress()
 
@@ -72,33 +69,25 @@ class HdmiCECSetupScreen(ConfigListScreen, Screen):
 		self["config"].list = self.list
 
 	def getCurrentDescription(self):
-		description = self["config"].getCurrent()[2] if len(self["config"].getCurrent()) > 2 else ""
+		description = ConfigListScreen.getCurrentDescription(self)
 		return "%s\n%s\n\n%s" % (self.current_address, self.fixed_address, description) if config.hdmicec.enabled.value else description
 
-	def keyGo(self):
-		for x in self["config"].list:
-			x[1].save()
-		self.close()
-
-	def keyOk(self):
+	def keySelect(self):
 		currentry = self["config"].getCurrent()
 		if currentry == self.logpath_entry:
 			self.set_path()
 		else:
-			self.keyGo()
+			ConfigListScreen.keySelect(self)
 
 	def setFixedAddress(self):
-		import Components.HdmiCec
 		Components.HdmiCec.hdmi_cec.setFixedPhysicalAddress(Components.HdmiCec.hdmi_cec.getPhysicalAddress())
 		self.updateAddress()
 
 	def clearFixedAddress(self):
-		import Components.HdmiCec
 		Components.HdmiCec.hdmi_cec.setFixedPhysicalAddress("0.0.0.0")
 		self.updateAddress()
 
 	def updateAddress(self):
-		import Components.HdmiCec
 		self.current_address = _("Current CEC address") + ":\t" + Components.HdmiCec.hdmi_cec.getPhysicalAddress()
 		if config.hdmicec.fixed_physical_address.value == "0.0.0.0":
 			self.fixed_address = _("Press yellow button to set CEC address again")
@@ -112,7 +101,6 @@ class HdmiCECSetupScreen(ConfigListScreen, Screen):
 
 	def set_path(self):
 		inhibitDirs = ["/autofs", "/bin", "/boot", "/dev", "/etc", "/lib", "/proc", "/sbin", "/sys", "/tmp", "/usr"]
-		from Screens.LocationBox import LocationBox
 		txt = _("Select directory for logfile")
 		self.session.openWithCallback(self.logPath, LocationBox, text=txt, currDir=config.hdmicec.log_path.value,
 				bookmarks=config.hdmicec.bookmarks, autoAdd=False, editDir=True,
@@ -132,9 +120,6 @@ def startSetup(menuid):
 
 
 def Plugins(**kwargs):
-	from os import path
 	if path.exists("/dev/hdmi_cec") or path.exists("/dev/misc/hdmi_cec0"):
-		import Components.HdmiCec
-		from Plugins.Plugin import PluginDescriptor
 		return [PluginDescriptor(where=PluginDescriptor.WHERE_MENU, fnc=startSetup)]
 	return []
