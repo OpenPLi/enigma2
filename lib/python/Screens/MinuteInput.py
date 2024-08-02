cc690fbafb6a45e42aac24d3949ad00a0692854a
Dima73 <dima-73@inbox.lv>
2024-08-02 15:38:48 +0300

    [MinuteInput] add screen title when this need

--- lib/python/Screens/MinuteInput.py
+++ lib/python/Screens/MinuteInput.py
@@ -4,7 +4,7 @@ from Components.Input import Input
 
 
 class MinuteInput(Screen):
-		def __init__(self, session, basemins=5, maxValue=False):
+		def __init__(self, session, basemins=5, maxValue=False, windowTitle=None):
 			Screen.__init__(self, session)
 
 			self["minutes"] = Input(str(basemins), type=Input.NUMBER, maxValue=maxValue)
@@ -33,9 +33,11 @@ class MinuteInput(Screen):
 				"cancel": self.cancel
 			})
 
+			if windowTitle is not None:
+				self.setTitle(windowTitle, showPath=False)
+
 		def keyNumberGlobal(self, number):
 			self["minutes"].number(number)
-			pass
 
 		def left(self):
 			self["minutes"].left()
