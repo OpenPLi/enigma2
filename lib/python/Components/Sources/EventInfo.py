e25dc3c71017f054e5b49f67143fe570402da1bc
Dima73 <dima-73@inbox.lv>
2024-09-05 07:53:29 +0300

    [EventInfo] add option updateSource(ref)
    -this more functionality  you can assign your own service
    
    thanks OpenViX team

--- lib/python/Components/Sources/EventInfo.py
+++ lib/python/Components/Sources/EventInfo.py
@@ -1,8 +1,7 @@
 from time import time
-
+from enigma import iPlayableService, iServiceInformation, eServiceReference, eEPGCache, eServiceCenter
 from Components.PerServiceDisplay import PerServiceBase
 from Components.Element import cached
-from enigma import iPlayableService, iServiceInformation, eServiceReference, eEPGCache
 from Components.Sources.Source import Source
 
 
@@ -22,8 +21,8 @@ class pServiceEvent:
 		self.m_ExtendedDescriptionNext = ""
 		self.m_Duration = 0
 		self.m_Begin = int(time())
-
-		sTagTitle = info.getInfoString(iServiceInformation.sTagTitle)
+		isPtr = not isinstance(service, eServiceReference)
+		sTagTitle = info.getInfoString(iServiceInformation.sTagTitle) if isPtr else info.getInfoString(service, iServiceInformation.sTagTitle)
 		if sTagTitle:
 			sTagTitleList = sTagTitle.split(" - ")
 			element1 = sTagTitleList[0] if len(sTagTitleList) >= 1 else ""
@@ -36,22 +35,22 @@ class pServiceEvent:
 				self.m_EventNameNow = element1 + " - " + element2
 				self.m_EventNameNext = element3
 
-		sTagGenre = info.getInfoString(iServiceInformation.sTagGenre)
+		sTagGenre = info.getInfoString(iServiceInformation.sTagGenre) if isPtr else info.getInfoString(service, iServiceInformation.sTagGenre)
 		if sTagGenre:
 			element4 = sTagGenre
 			self.m_ShortDescriptionNow = element4
 
-		sTagOrganization = info.getInfoString(iServiceInformation.sTagOrganization)
+		sTagOrganization = info.getInfoString(iServiceInformation.sTagOrganization) if isPtr else info.getInfoString(service, iServiceInformation.sTagOrganization)
 		if sTagOrganization:
 			element5 = sTagOrganization
 			self.m_ExtendedDescriptionNow = element5
 
-		sTagLocation = info.getInfoString(iServiceInformation.sTagLocation)
+		sTagLocation = info.getInfoString(iServiceInformation.sTagLocation) if isPtr else info.getInfoString(service, iServiceInformation.sTagLocation)
 		if sTagLocation:
 			element6 = sTagLocation
 			self.m_ExtendedDescriptionNow += "\n\n" + element6
 
-		seek = service and service.seek()
+		seek = service and isPtr and service.seek()
 		if seek:
 			length = seek.getLength()
 			if length[0] == 0:
@@ -127,17 +126,23 @@ class EventInfo(PerServiceBase, Source):
 			}, with_event=True)
 		self.now_or_next = now_or_next
 		self.epgQuery = eEPGCache.getInstance().lookupEventTime
+		self.__service = None
 
 	@cached
 	def getEvent(self):
-		service = self.navcore.getCurrentService()
-		info = service and service.info()
-		ret = info and info.getEvent(self.now_or_next)
+		isPtr = not isinstance(self.__service, eServiceReference)
+		service = self.navcore.getCurrentService() if isPtr else self.__service
+		if isPtr:
+			info = service and service.info()
+			ret = info and info.getEvent(self.now_or_next)
+		else:
+			info = eServiceCenter.getInstance().info(self.__service)
+			ret = info and info.getEvent(self.__service, self.now_or_next)
 		if info:
 			if not ret or ret.getEventName() == "":
-				refstr = info.getInfoString(iServiceInformation.sServiceref)
+				refstr = info.getInfoString(iServiceInformation.sServiceref) if isPtr else self.__service.toString()
 				ret = self.epgQuery(eServiceReference(refstr), -1, self.now_or_next and 1 or 0)
-				if not ret and refstr.split(':')[0] in ['4097', '5001', '5002', '5003']: # No EPG Try to get Meta
+				if not ret and refstr.split(':')[0] in ['4097', '5001', '5002', '5003']:  # No EPG Try to get Meta
 					ev = pServiceEvent(info, self.now_or_next, service)
 					if ev.getEventName:
 						return ev
@@ -147,6 +152,7 @@ class EventInfo(PerServiceBase, Source):
 
 	def gotEvent(self, what):
 		if what == iPlayableService.evEnd:
+			self.__service = None
 			self.changed((self.CHANGED_CLEAR,))
 		else:
 			self.changed((self.CHANGED_ALL,))
@@ -154,3 +160,11 @@ class EventInfo(PerServiceBase, Source):
 	def destroy(self):
 		PerServiceBase.destroy(self)
 		Source.destroy(self)
+
+	def updateSource(self, ref):
+		if not ref:
+			self.__service = None
+			self.changed((self.CHANGED_CLEAR,))
+		else:
+			self.__service = ref
+			self.changed((self.CHANGED_ALL,))
