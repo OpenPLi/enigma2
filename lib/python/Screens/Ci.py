e2c8683d872f5b111725d2e9022733cdd1159a00
Dimitrij <Dima-73@inbox.lv>
2024-09-20 16:51:02 +0300

    [CI] use "Exclude first CA device" only pulse4k(mini) (#4065)

--- lib/python/Screens/Ci.py
+++ lib/python/Screens/Ci.py
@@ -475,7 +475,8 @@ class CiSelection(Screen):
 		self.list.append((_("Show CI messages"), config.ci[slot].show_ci_messages, 3, slot))
 		self.list.append((_("Disable operator profiles"), config.ci[slot].disable_operator_profile, 3, slot))
 		self.list.append((_("Multiple service support"), config.ci[slot].canDescrambleMultipleServices, 3, slot))
-		self.list.append((_("Exclude first CA device"), config.ci[slot].exclude_ca0_device, 3, slot))
+		if BoxInfo.getItem("CiAlternativeCaHandling"):
+			self.list.append((_("Exclude first CA device"), config.ci[slot].exclude_ca0_device, 3, slot))
 		if BoxInfo.getItem("CI%dSupportsHighBitrates" % slot):
 			self.list.append((_("High bitrate support"), config.ci[slot].highBitrate, 3, slot))
 		if BoxInfo.getItem("CI%dRelevantPidsRoutingSupport" % slot):
