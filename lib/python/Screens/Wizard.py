214b36dc1d8a49c068d3b6a6559b467d9d861798
Huevos <rowland@moralet.com>
2024-09-30 12:57:36 +0200

    [Wizard.py] add sanity check that "evaluatedlist" exists

--- lib/python/Screens/Wizard.py
+++ lib/python/Screens/Wizard.py
@@ -445,7 +445,7 @@ class Wizard(Screen):
 
 		if self.showConfig and self.wizard[self.currStep]["config"]["screen"] is not None:
 			self["config"].instance.moveSelection(self["config"].instance.moveUp)
-		elif self.showList and len(self.wizard[self.currStep]["evaluatedlist"]) > 0:
+		elif self.showList and hasattr(self.wizard[self.currStep], "evaluatedlist") and len(self.wizard[self.currStep]["evaluatedlist"]) > 0:
 			if "onselect" in self.wizard[self.currStep]:
 				self.selection = self["list"].current[-1]
 				print("self.selection:", self.selection)
