82971a5bad3f0b8ecc8d58c98b1e57823d92a5c2
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2026-04-26 13:15:11 +0200

    [lcd.cpp] Fix build with Wrynose branch
    Fix:
    
    | ../../sources/enigma2-3.14+git/lib/gdi/lcd.cpp: In member function 'virtual void eDBoxLCD::update()':
    | ../../sources/enigma2-3.14+git/lib/gdi/lcd.cpp:247:22: error: ignoring return value of '
    ssize_t write(int, const void*, size_t)' declared with attribute 'warn_unused_result' [-Werror=unused-result]
    |   247 |                 write(lcdfd, raw, 132 * 8);
    |       |                 ~~~~~^~~~~~~~~~~~~~~~~~~~~
    | ../../sources/enigma2-3.14+git/lib/gdi/lcd.cpp:268:30: error: ignoring return value of
    'ssize_t write(int, const void*, size_t)' declared with attribute 'warn_unused_result' [-Werror=unused-result]
    |   268 |                         write(lcdfd, raw, _stride * height);
    |       |                         ~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- lib/gdi/lcd.cpp
+++ lib/gdi/lcd.cpp
@@ -244,7 +244,9 @@ void eDBoxLCD::update()
 					raw[y * 132 + x] = pix ^ inverted;
 			}
 		}
-		write(lcdfd, raw, 132 * 8);
+		if (write(lcdfd, raw, 132 * 8) == -1) {
+			eDebug("[eDboxLCD] write() (1) failed");
+		}
 	}
 	else if (lcd_type == 3)
 	{
@@ -265,10 +267,14 @@ void eDBoxLCD::update()
 						raw[y * width + x] = _buffer[y * width + x] ^ inverted;
 				}
 			}
-			write(lcdfd, raw, _stride * height);
+			if (write(lcdfd, raw, _stride * height) == -1) {
+				eDebug("[eDboxLCD] write() (2) failed");
+			}
 		}
 		else
-			write(lcdfd, _buffer, _stride * res.height());
+			if (write(lcdfd, _buffer, _stride * res.height()) == -1) {
+				eDebug("[eDboxLCD] write() (3) failed");
+			}
 	}
 	else /* lcd_type == 1 */
 	{
@@ -295,7 +301,9 @@ void eDBoxLCD::update()
 					raw[y * 64 + x] = pix;
 			}
 		}
-		write(lcdfd, raw, 64 * 64);
+		if (write(lcdfd, raw, 64 * 64) == -1) {
+			eDebug("[eDboxLCD] write() (4) failed");
+		}
 	}
 #endif
 }
