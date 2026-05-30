5b93b9505f13dc9f48a3a7111d622d09367b4144
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2026-05-30 13:52:58 +0200

    fixed

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -61,7 +61,7 @@ print('MDEVICES AS:\n', piconpathss)
 
 def InitUsageConfig():
 	config.usage = ConfigSubsection()
-	config.usage.dns = ConfigSelection(default="dhcp-router", choices=[
+	config.usage.dns = ConfigSelection(default="google", choices=[
 		("dhcp-router", _("DHCP Router")),
 		("staticip", _("Static IP Router")),
 		("google", _("Google DNS")),
