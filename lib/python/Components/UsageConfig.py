7336334eeb09c35b82c2c1716e2022f1a6897fe3
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2026-07-03 07:44:27 +0200

    [UsageConfig.py] Drop config.ntp.server_old
    Set initial_call to False to call method only when user change server.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -872,8 +872,7 @@ def InitUsageConfig():
 	config.ntp.timesync = ConfigSelection(default="auto", choices=[("auto", _("auto")), ("dvb", _("Transponder Time")), ("ntp", _("Internet (ntp)"))])
 	config.ntp.timesync.addNotifier(timesyncChanged)
 	config.ntp.server = ConfigText("", fixed_size=False)
-	config.ntp.server_old = ConfigText("")
-	config.ntp.server.addNotifier(ntpHandler.setServer, immediate_feedback=False)
+	config.ntp.server.addNotifier(ntpHandler.setServer, initial_call=False, immediate_feedback=False)
 
 
 def updateChoices(sel, choices):
@@ -1069,7 +1068,7 @@ class NtpHandler:
 				print("[UsageConfig] Unsupported Chrony status action: %s" % action)
 
 	def setServer(self, configElement):
-		if configElement.value != config.ntp.server_old.value and " " not in configElement.value:
+		if " " not in configElement.value:
 			f = open("/etc/chrony.conf", "r")
 			lst = f.readlines()
 			f.close()
@@ -1085,7 +1084,6 @@ class NtpHandler:
 						x = "server %s iburst minpoll 3 prefer\n" % configElement.value
 				f.write(x)
 			f.close()
-			config.ntp.server_old.value = configElement.value
 			self.console.ePopen('/etc/init.d/chronyd status', self.chronyStatusFinished, 'sync')
 			print("[UsageConfig] NTP enabled, local server is set to: %s" % configElement.value)
 
