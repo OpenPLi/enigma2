fa933468d23ccd328c598f8bdb0f2316b6f3200c
ims <ims21@users.sourceforge.net>
2026-09-13 12:29:33 +0200

    ePositionGauge - add hidePointerOnZeroLength skin parameter
    - add hidePointerOnZeroLength skin parameter to hide pointer when playback length is zero.
    - default behavior remains unchanged for backward compatibility.
    
    (cherry picked from commit 94f9d147ed804a2caae35893cac0d205b2155476)

--- lib/gui/epositiongauge.h
+++ lib/gui/epositiongauge.h
@@ -26,6 +26,8 @@ public:
 	void enableSeekPointer(int enable);
 	void setSeekPosition(const pts_t &pos);
 
+	void setHidePointerOnZeroLength(int hide);
+
 #ifndef SWIG
 protected:
 	int event(int event, void *data=0, void *data2=0);
@@ -62,6 +64,8 @@ private:
 
 	int m_have_foreground_color;
 	gRGB m_foreground_color;
+
+	int m_hidePointerOnZeroLength;
 #endif
 };
 
