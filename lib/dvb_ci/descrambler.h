5a058a7e35ab363eff6cbf0ac0dbda1c5a35b118
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2026-09-18 13:24:05 +0200

    [descrambler.h] Revert data type argument descrambler_init() back to integer
    Revert:
    https://github.com/Hains/enigma2/commit/0017647825bcb4e656b0b99b660b69634c83635e#diff-0d41511784e9dc1f889abf397e9f0fafeb1dc55c5bd86147caa489bfcb8d1e7dL6
    
     ../../sources/enigma2-3.14+git/lib/dvb_ci/dvbci_ccmgr.cpp:801:70: error: invalid conversion from 'int' to 'eDVBCISlot*' [-fpermissive]
    |   801 |                 m_descrambler_fd = descrambler_init(m_slot->getSlotID(), m_slot->getCADemuxID() + (m_slot->getIsCA0Excluded() ? 1 : 0));
    |       |                                                     ~~~~~~~~~~~~~~~~~^~
    |       |                                                                      |
    |       |                                                                      int

--- lib/dvb_ci/descrambler.h
+++ lib/dvb_ci/descrambler.h
@@ -17,7 +17,7 @@ struct ca_pid
 #define CA_SET_PID _IOW('o', 135, struct ca_pid)
 #endif
 
-int descrambler_init(eDVBCISlot *slot, uint8_t ca_demux_id);
+int descrambler_init(int slot, uint8_t ca_demux_id);
 void descrambler_deinit(int desc_fd);
 int descrambler_set_key(int& desc_fd, eDVBCISlot *slot, int parity, unsigned char *data);
 int descrambler_set_pid(int desc_fd, eDVBCISlot *slot, int enable, int pid);
